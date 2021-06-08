e0a26bee10d1ddff05dbf06d2e828d9f5d8ddda5
John D. Corbett <corbett@pictographer.com>
2021-06-08 17:25:13 -0400

    Basic double entry demo
    Co-Authored-By: Nathan McCarty<nathan@mccarty.io>

---
+++ src/demos/dentry.rs
@@ -0,0 +1,238 @@
+/// `BlockContents` implementation for the double entry bookkeeping demo
+pub mod block;
+
+use crate::message::Message;
+use crate::networking::w_network::WNetwork;
+use crate::{HotStuff, HotStuffConfig, PubKey, H_256};
+use blake3::Hasher;
+use block::{DEntryBlock, State, Transaction};
+use rand::Rng;
+use serde::{de::DeserializeOwned, Serialize};
+use std::collections::BTreeMap;
+use threshold_crypto as tc;
+
+/// Attempts to create a network connection with a random port
+pub async fn try_network<
+    T: Clone + Serialize + DeserializeOwned + Send + Sync + std::fmt::Debug + 'static,
+>(
+    key: PubKey,
+) -> (WNetwork<T>, u16) {
+    // TODO: Actually attempt to open the port and find a new one if it doens't work
+    let port = rand::thread_rng().gen_range(2000, 5000);
+    (
+        WNetwork::new_from_strings(key, vec![], port, None)
+            .await
+            .expect("Failed to create network"),
+        port,
+    )
+}
+
+/// Generates the `SecretKeySet` for this BFT instance
+pub fn gen_keys(threshold: usize) -> tc::SecretKeySet {
+    tc::SecretKeySet::random(threshold, &mut rand::thread_rng())
+}
+
+/// Turns a `PublicKeySet` into a set of `HotStuff` `PubKey`s
+pub fn set_to_keys(total: usize, set: &tc::PublicKeySet) -> Vec<PubKey> {
+    (0..total)
+        .map(|x| PubKey {
+            set: set.clone(),
+            node: set.public_key_share(x),
+            nonce: x as u64,
+        })
+        .collect()
+}
+
+/// Attempts to create a hotstuff instance
+pub async fn try_hotstuff(
+    keys: &tc::SecretKeySet,
+    total: usize,
+    threshold: usize,
+    node_number: usize,
+) -> (
+    HotStuff<DEntryBlock, H_256>,
+    PubKey,
+    u16,
+    WNetwork<Message<DEntryBlock, Transaction, H_256>>,
+) {
+    // TODO !corbett how do all the genesis blocks get initialized?
+    assert_eq!(
+        *Hasher::new().finalize().as_bytes(),
+        *Hasher::new().finalize().as_bytes()
+    );
+    let genesis = DEntryBlock {
+        previous_block: (*Hasher::new().finalize().as_bytes()).into(),
+        transactions: vec![],
+    };
+    let pub_key_set = keys.public_keys();
+    let tc_pub_key = pub_key_set.public_key_share(node_number);
+    let pub_key = PubKey {
+        set: pub_key_set.clone(),
+        node: tc_pub_key,
+        nonce: node_number as u64,
+    };
+    let config = HotStuffConfig {
+        total_nodes: total as u32,
+        thershold: threshold as u32,
+        max_transactions: 100,
+        known_nodes: set_to_keys(total, &pub_key_set),
+        next_view_timeout: 500,
+        timeout_ratio: (11,10),
+    };
+    let (networking, port) = try_network(pub_key.clone()).await;
+    let hotstuff = HotStuff::new(
+        genesis,
+        &keys,
+        node_number as u64,
+        config,
+        State {
+            balances: BTreeMap::new(),
+        },
+        networking.clone(),
+    );
+    (hotstuff, pub_key, port, networking)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use super::block::{Addition, Subtraction};
+    use crate::utility::test_util::setup_logging;
+    use async_std::task::spawn;
+    use futures::channel::oneshot;
+    use futures::future::join_all;
+
+    #[async_std::test]
+    async fn hotstuff_dentry_demo() {
+        setup_logging();
+        let keys = gen_keys(3);
+        // Create the hotstuffs and spawn their tasks
+        let hotstuffs: Vec<(HotStuff<DEntryBlock, H_256>, PubKey, u16, WNetwork<_>)> =
+            join_all((0..5).map(|x| try_hotstuff(&keys, 5, 4, x))).await;
+        // Boot up all the low level networking implementations
+        for (_, _, _, network) in &hotstuffs {
+            let (x, sync) = oneshot::channel();
+            match network.generate_task(x) {
+                Some(task) => {
+                    spawn(task);
+                    sync.await.expect("sync.await failed");
+                }
+                None => {
+                    println!("generate_task(x) returned None");
+                    panic!();
+                }
+            }
+        }
+        // Connect the hotstuffs
+        for (i, (_, key, port, _)) in hotstuffs.iter().enumerate() {
+            let socket = format!("localhost:{}", port);
+            // Loop through all the other hotstuffs and connect it to this one
+            for (_, key_2, port_2, network_2) in &hotstuffs[i..] {
+                println!("Connecting {} to {}", port_2, port);
+                if key != key_2 {
+                    network_2
+                        .connect_to(key.clone(), &socket)
+                        .await
+                        .expect("Unable to connect to node");
+                }
+            }
+        }
+        // Boot up all the high level implementations
+        for (hotstuff, _, _, _) in &hotstuffs {
+            hotstuff.spawn_networking_tasks().await;
+        }
+        // Wait for all nodes to connect to each other
+        println!("Waiting for nodes to fully connect");
+        for (_, _, _, w) in &hotstuffs {
+            while w.connection_table_size().await < 4 {
+                async_std::task::sleep(std::time::Duration::from_millis(10)).await;
+            }
+            while w.nodes_table_size().await < 4 {
+                async_std::task::sleep(std::time::Duration::from_millis(10)).await;
+            }
+        }
+        println!("Nodes should be connected");
+        println!(
+            "Current states: {:?}",
+            join_all(hotstuffs.iter().map(|(h, _, _, _)| h.get_state())).await
+        );
+        // Propose a new transaction
+        println!("Proposing first transaction");
+        hotstuffs[0]
+            .0
+            .publish_transaction_async(Transaction {
+                add: Addition {
+                    account: "Alice".to_string(),
+                    amount: 0,
+                },
+                sub: Subtraction {
+                    account: "Bob".to_string(),
+                    amount: 0,
+                },
+            })
+            .await
+            .unwrap();
+        // issuing new views
+        println!("Issuing new view messages");
+        join_all(hotstuffs.iter().map(|(h, _, _, _)| h.next_view(0, None))).await;
+        // Running a round of consensus
+        println!("Running round 1");
+        join_all(hotstuffs.iter().map(|(h, _, _, _)| h.run_round(1, None))).await;
+        println!(
+            "Current states: {:?}",
+            join_all(hotstuffs.iter().map(|(h, _, _, _)| h.get_state())).await
+        );
+        // Propose a new transaction
+        println!("Proposing second transaction");
+        hotstuffs[1]
+            .0
+            .publish_transaction_async(Transaction {
+                add: Addition {
+                    account: "Alice".to_string(),
+                    amount: 0,
+                },
+                sub: Subtraction {
+                    account: "Bob".to_string(),
+                    amount: 0,
+                },
+            })
+            .await
+            .unwrap();
+        // issuing new views
+        println!("Issuing new view messages");
+        join_all(hotstuffs.iter().map(|(h, _, _, _)| h.next_view(1, None))).await;
+        // Running a round of consensus
+        println!("Running round 2");
+        join_all(hotstuffs.iter().map(|(h, _, _, _)| h.run_round(2, None))).await;
+        println!(
+            "Current states: {:?}",
+            join_all(hotstuffs.iter().map(|(h, _, _, _)| h.get_state())).await
+        );
+        // Propose a new transaction
+        println!("Proposing third transaction");
+        hotstuffs[0]
+            .0
+            .publish_transaction_async(Transaction {
+                add: Addition {
+                    account: "Alice".to_string(),
+                    amount: 0,
+                },
+                sub: Subtraction {
+                    account: "Bob".to_string(),
+                    amount: 0,
+                },
+            })
+            .await
+            .unwrap();
+        // issuing new views
+        println!("Issuing new view messages");
+        join_all(hotstuffs.iter().map(|(h, _, _, _)| h.next_view(2, None))).await;
+        // Running a round of consensus
+        println!("Running round 3");
+        join_all(hotstuffs.iter().map(|(h, _, _, _)| h.run_round(3, None))).await;
+        println!(
+            "Current states: {:?}",
+            join_all(hotstuffs.iter().map(|(h, _, _, _)| h.get_state())).await
+        );
+    }
+}
---
+++ src/demos/dentry/block.rs
@@ -0,0 +1,278 @@
+use blake3::Hasher;
+use serde::{Deserialize, Serialize};
+use snafu::{ensure, OptionExt, Snafu};
+use std::collections::BTreeMap;
+use tracing::error;
+
+use crate::{BlockContents, BlockHash, H_256};
+
+/// An account identifier
+pub type Account = String;
+
+/// An account balance
+pub type Balance = i64;
+
+/// Records a reduction in an account balance
+#[derive(PartialEq, Eq, Hash, Serialize, Deserialize, Clone, Debug)]
+pub struct Subtraction {
+    /// An account identifier
+    pub account: Account,
+    /// An account balance
+    pub amount: Balance,
+}
+
+/// Records an increase in an account balance
+#[derive(PartialEq, Eq, Hash, Serialize, Deserialize, Clone, Debug)]
+pub struct Addition {
+    /// An account identifier
+    pub account: Account,
+    /// An account balance
+    pub amount: Balance,
+}
+
+/// The error type for the dentry demo
+#[derive(Snafu, Debug)]
+pub enum DEntryError {
+    /// The subtraction and addition amounts for this transaction were not equal
+    InconsistentTransaction,
+    /// No such input account exists
+    NoSuchInputAccount,
+    /// No such output account exists
+    NoSuchOutputAccount,
+    /// Tried to move more money than was in the account
+    InsufficentBalance,
+    /// Previous state commitment does not match
+    PreviousStateMismatch,
+}
+
+/// The transaction for the dentry demo
+#[derive(PartialEq, Eq, Hash, Serialize, Deserialize, Clone, Debug)]
+pub struct Transaction {
+    /// An increment to an account balance
+    pub add: Addition,
+    /// A decrement to an account balance
+    pub sub: Subtraction,
+}
+
+impl Transaction {
+    /// Ensures that this transaction is at least consistent with itself
+    pub fn validate_independence(&self) -> bool {
+        // Ensure that we are adding to one account exactly as much as we are subtracting from
+        // another
+        self.add.amount <= self.sub.amount // TODO why not strict equality?
+    }
+}
+
+/// The state for the dentry demo
+#[derive(Clone, Debug)]
+pub struct State {
+    /// Key/value store of accounts and balances
+    pub balances: BTreeMap<Account, Balance>,
+}
+
+impl State {
+    fn hash_state(&self) -> BlockHash<H_256> {
+        // BTreeMap sorts so this will be in a consistent order
+        let mut hasher = Hasher::new();
+        for (account, balance) in &self.balances {
+            // account first
+            hasher.update(account.as_bytes());
+            // then balance
+            hasher.update(&balance.to_be_bytes());
+        }
+        let x = *hasher.finalize().as_bytes();
+        x.into()
+    }
+}
+
+/// The block for the dentry demo
+#[derive(PartialEq, Eq, Default, Hash, Serialize, Deserialize, Clone, Debug)]
+pub struct DEntryBlock {
+    /// Block state commitment
+    pub previous_block: BlockHash<H_256>,
+    /// Transaction vector
+    pub transactions: Vec<Transaction>,
+}
+
+impl BlockContents<H_256> for DEntryBlock {
+    type State = State;
+
+    type Transaction = Transaction;
+
+    type Error = DEntryError;
+
+    fn add_transaction(
+        &self,
+        state: &Self::State,
+        tx: &Self::Transaction,
+    ) -> std::result::Result<Self, Self::Error> {
+        // first, make sure that the transaction is internally valid
+        if tx.validate_independence() {
+            // Now add up all the existing transactions from this block involving the subtraction,
+            // we don't want to allow an account balance to go below zero
+            let total_so_far: i64 = self
+                .transactions
+                .iter()
+                .filter(|x| x.sub.account == tx.sub.account)
+                .map(|x| x.sub.amount)
+                .sum::<i64>()
+                + tx.sub.amount;
+            // Look up the current balance for the account, and make sure we aren't subtracting more
+            // than they currently have, returning an error if the account does not exist
+            let current_balance: i64 = *state
+                .balances
+                .get(&tx.sub.account)
+                .context(NoSuchInputAccount)?;
+            // Make sure that the total we are trying to transact in this block is less than or
+            // equal to the current account balance, otherwise return an error
+            if total_so_far <= current_balance {
+                // Make a copy of ourselves to modify (ideally we would be using persistent data
+                // structures, so this would be low cost)
+                let mut new_block = self.clone();
+                // Insert our transaction and return
+                new_block.transactions.push(tx.clone());
+                Ok(new_block)
+            } else {
+                Err(DEntryError::InsufficentBalance)
+            }
+        } else {
+            Err(DEntryError::InconsistentTransaction)
+        }
+    }
+
+    // Note: validate_block is actually somewhat redundant, its meant to be a quick and dirty check
+    // for clarity, the logic is duplicated with append_to
+    fn validate_block(&self, state: &Self::State) -> bool {
+        // A valid block is one in which every transaction is internally consistent, and results in
+        // nobody having a negative balance
+        //
+        // We will check this, in this case, by making a trial copy of our balances map, making
+        // trial modifications to it, and then asserting that no balances are negative
+        let mut trial_balances = state.balances.clone();
+        for tx in &self.transactions {
+            // This is a macro from SNAFU that returns an Err if the condition is not satisfied
+            //
+            // We first check that the transaction is internally consistent, then apply the change
+            // to our trial map
+            if !tx.validate_independence() {
+                error!("validate_independence failed");
+                return false;
+            }
+            // Find the input account, and subtract the transfer balance from it, failing if it
+            // doesn't exist
+            if let Some(input_account) = trial_balances.get_mut(&tx.sub.account) {
+                *input_account = *input_account - tx.sub.amount;
+            } else {
+                error!("no such input account");
+                return false;
+            }
+            // Find the output account, and add the transfer balance to it, failing if it doesn't
+            // exist
+            if let Some(output_account) = trial_balances.get_mut(&tx.add.account) {
+                *output_account = *output_account + tx.add.amount;
+            } else {
+                error!("no such output account");
+                return false;
+            }
+        }
+        // Loop through our account map and make sure nobody is negative
+        for (_account, balance) in &trial_balances {
+            if *balance < 0 {
+                error!("negative balance");
+                return false;
+            }
+        }
+        // This block has now passed all our tests, and thus has not done anything bad, so the block
+        // is valid if its previous state hash matches that of the previous state
+        let result = &self.previous_block == &state.hash_state();
+        if !result {
+            error!(
+                "hash failure. previous_block: {:?} hash_state: {:?}",
+                self.previous_block,
+                state.hash_state()
+            );
+        }
+        return result;
+    }
+
+    fn append_to(&self, state: &Self::State) -> std::result::Result<Self::State, Self::Error> {
+        // A valid block is one in which every transaction is internally consistent, and results in
+        // nobody having a negative balance
+        //
+        // We will check this, in this case, by making a trial copy of our balances map, making
+        // trial modifications to it, and then asserting that no balances are negative
+        let mut trial_balances = state.balances.clone();
+        for tx in &self.transactions {
+            // This is a macro from SNAFU that returns an Err if the condition is not satisfied
+            //
+            // We first check that the transaction is internally consistent, then apply the change
+            // to our trial map
+            ensure!(tx.validate_independence(), InconsistentTransaction);
+            // Find the input account, and subtract the transfer balance from it, failing if it
+            // doesn't exist
+            if let Some(input_account) = trial_balances.get_mut(&tx.sub.account) {
+                *input_account = *input_account - tx.sub.amount;
+            } else {
+                return Err(DEntryError::NoSuchInputAccount);
+            }
+            // Find the output account, and add the transfer balance to it, failing if it doesn't
+            // exist
+            if let Some(output_account) = trial_balances.get_mut(&tx.add.account) {
+                *output_account = *output_account + tx.add.amount;
+            } else {
+                return Err(DEntryError::NoSuchOutputAccount);
+            }
+        }
+        // Loop through our account map and make sure nobody is negative
+        for (_account, balance) in &trial_balances {
+            if *balance < 0 {
+                return Err(DEntryError::InsufficentBalance);
+            }
+        }
+        // Make sure our previous state commitment matches the provided state
+        if self.previous_block == state.hash_state() {
+            // This block has now passed all our tests, and thus has not done anything bad
+            Ok(State {
+                balances: trial_balances,
+            })
+        } else {
+            Err(DEntryError::PreviousStateMismatch)
+        }
+    }
+
+    // Note: this is really used for indexing the block in storage
+    fn hash(&self) -> BlockHash<H_256> {
+        let mut hasher = Hasher::new();
+        hasher.update(&self.previous_block.as_ref());
+        self.transactions.iter().for_each(|tx| {
+            hasher.update(&Self::hash_transaction(tx).as_ref());
+            ()
+        });
+        let x = *hasher.finalize().as_bytes();
+        x.into()
+    }
+
+    // Note: This is used for indexing the transaction in storage
+    fn hash_transaction(tx: &Self::Transaction) -> BlockHash<H_256> {
+        assert!(tx.validate_independence());
+        let mut hasher = Hasher::new();
+        hasher.update(&tx.add.account.as_bytes());
+        hasher.update(&tx.add.amount.to_be_bytes());
+        hasher.update(&tx.sub.account.as_bytes());
+        hasher.update(&tx.sub.amount.to_be_bytes());
+        let x = *hasher.finalize().as_bytes();
+        x.into()
+    }
+
+    fn hash_bytes(bytes: &[u8]) -> BlockHash<H_256> {
+        let x = *blake3::hash(bytes).as_bytes();
+        x.into()
+    }
+
+    fn next_block(state: &Self::State) -> Self {
+        DEntryBlock {
+            previous_block: state.hash_state(),
+            transactions: Vec::new(),
+        }
+    }
+}
