a321758a43c404e8f12daad2167c3a9872766ac0
Justin Restivo <justin@restivo.me>
2022-01-06 16:22:36 -0500

    feat: add nix repl

---
+++ repl.nix
@@ -0,0 +1,9 @@
+# usage: nix repl repl.nix
+let
+  flake = builtins.getFlake (toString ./.);
+in
+  {
+    inherit flake;
+    self = flake.inputs.self;
+    pkgs = flake.inputs.nixpkgs.legacyPackages;
+  }
