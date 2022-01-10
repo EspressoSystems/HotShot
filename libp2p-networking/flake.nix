6ace4a919568f9da223608ea1d5835ecee108310
Nathan McCarty <nathan@mccarty.io>
2022-01-10 14:05:13 -0500

    Initial Commit

---
+++ libp2p-networking/flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "PhaseLock consensus library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    crate2nix = {
+      url = "github:balsoft/crate2nix/balsoft/fix-broken-ifd";
+      flake = false;
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-compat, utils, crate2nix, fenix }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        fenixPackage = fenix.packages.${system}.stable.withComponents [ "cargo" "clippy" "rust-src" "rustc" "rustfmt"];
+        rustOverlay = final: prev:
+          {
+            inherit fenixPackage;
+            rustc = fenixPackage;
+            cargo = fenixPackage;
+            rust-src = fenixPackage;
+          };
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rustOverlay
+          ];
+        };
+
+        # DON'T FORGET TO PUT YOUR PACKAGE NAME HERE, REMOVING `throw`
+        crateName = "networking-demo";
+
+        inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
+          generatedCargoNix;
+
+        project = import
+          (generatedCargoNix {
+            name = crateName;
+            src = ./.;
+          })
+          {
+            inherit pkgs;
+            defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+              # Crate dependency overrides go here
+            };
+          };
+
+      in
+      {
+        packages.${crateName} = project.rootCrate.build;
+        packages.tests.${crateName} = project.rootCrate.build.override {
+          runTests = true;
+        };
+
+        defaultPackage = self.packages.${system}.${crateName};
+
+        devShell = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.packages.${system};
+          buildInputs =
+            with pkgs; [ cargo-audit nixpkgs-fmt git-chglog fenix.packages.${system}.rust-analyzer fenixPackage ];
+        };
+      });
+}
