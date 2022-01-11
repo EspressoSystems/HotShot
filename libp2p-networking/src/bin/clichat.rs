e55d9946a92b62a0f2a8cc33a7e5592184f59e58
Nathan McCarty <nathan@mccarty.io>
2022-01-11 15:49:34 -0500

    Really basic boiler plate

--- libp2p-networking/src/bin/clichat.rs
+++ libp2p-networking/src/bin/clichat.rs
@@ -1,6 +1,26 @@
+use std::collections::VecDeque;
+
 use color_eyre::eyre::{eyre, Result};
+
+#[derive(Debug, Clone)]
+pub struct Message {
+    sender: String,
+    content: String,
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+    // Setup a ring buffer to hold messages, 25 of them should do for the demo
+    let mut buffer: VecDeque<Message> = VecDeque::new();
+    // Put a few dummy messages in there so we can display something
+    buffer.push_back(Message {
+        sender: "Nathan".to_string(),
+        content: "Hello".to_string(),
+    });
+    buffer.push_back(Message {
+        sender: "Justin".to_string(),
+        content: "hi!".to_string(),
+    });
+
     Err(eyre!("Not implemented yet"))
 }
