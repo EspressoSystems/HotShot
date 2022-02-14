ec1ce9a780731515c0287488517d5d73a36144aa
Nathan McCarty <nathan@mccarty.io>
2022-02-14 13:44:48 -0500

    style: Set clippy in types

--- phaselock-types/src/lib.rs
+++ phaselock-types/src/lib.rs
@@ -1,8 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+//! Types and Traits for the `PhaseLock` consensus module
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    rust_2018_idioms,
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    clippy::panic
+)]
