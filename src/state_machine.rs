26753a4a50996150554093dbc7f257e5412837aa
Victor Koenders <gpg@trangar.com>
2022-04-05 19:10:37 +0200

    Made the follower node save the leaf after a consensus round

--- src/state_machine.rs
+++ src/state_machine.rs
@@ -346,10 +346,10 @@ impl<I: NodeImplementation<N> + Send + Sync + 'static, const N: usize> Sequentia
                     pl.inner
                         .storage
                         .update(|mut m| {
-                            // let new_leaf = new_leaf.clone();
+                            let new_leaf = new_leaf.clone();
                             let new_state = new_state.clone();
                             async move {
-                                // m.insert_leaf(new_leaf).await?;
+                                m.insert_leaf(new_leaf).await?;
                                 m.insert_state(new_state, the_hash).await?;
                                 Ok(())
                             }
@@ -357,7 +357,7 @@ impl<I: NodeImplementation<N> + Send + Sync + 'static, const N: usize> Sequentia
                         .await
                         .context(StorageSnafu)?;
 
-                    // debug!(?new_leaf, ?the_hash, "Leaf created and added to store");
+                    debug!(?new_leaf, ?the_hash, "Leaf created and added to store");
                     debug!(?new_state, "New state inserted");
 
                     // Broadcast out the leaf
@@ -793,11 +793,12 @@ impl<I: NodeImplementation<N> + 'static + Send + Sync, const N: usize> Sequentia
                 Poll::Ready(Err(PhaseLockError::Continue))
             }
             BeforePrepare(prepare) => {
-                debug!("Preapre message recieved from leader!!!");
+                debug!("Prepare message recieved from leader!!!");
                 let leaf = prepare.leaf.clone();
                 let leaf_hash = leaf.hash();
                 let pl = phaselock.clone();
                 let high_qc = prepare.high_qc.clone();
+                let suggested_state = prepare.state.clone();
                 let ctx = context.clone();
                 let fut = async move {
                     debug!(?leaf, "Looking for state for leaf");
@@ -852,11 +853,23 @@ impl<I: NodeImplementation<N> + 'static + Send + Sync, const N: usize> Sequentia
                         })?;
                         // Insert new state into storage
                         debug!(?new_state, "New state inserted");
+                        if suggested_state != new_state {
+                            // the state that the leader send does not match with what we calculated
+                            error!(?new_state, ?suggested_state, "Suggested state does not match actual state.");
+                            return Err(PhaseLockError::InconsistentBlock {
+                                stage: Stage::Prepare
+                            });
+                        }
                         pl.inner
                             .storage
-                            .update(
-                                |mut m| async move { m.insert_state(new_state, leaf_hash).await },
-                            )
+                            .update(|mut m| {
+                                let leaf = leaf.clone();
+                                async move {
+                                    m.insert_state(new_state, leaf_hash).await?;
+                                    m.insert_leaf(leaf).await?;
+                                    Ok(())
+                                }
+                            })
                             .await
                             .context(StorageSnafu)?;
 
