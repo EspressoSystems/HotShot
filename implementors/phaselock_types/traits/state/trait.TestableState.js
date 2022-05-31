796451ed460dfac4bd78c709d1520c04838b64f2
DieracDelta <DieracDelta@users.noreply.github.com>
2022-05-31 17:09:24 +0000

    deploy: 9ecbeff579daf41e93b8c35af2ec19782541c885

---
+++ implementors/phaselock_types/traits/state/trait.TestableState.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["phaselock"] = [{"text":"impl TestableState&lt;H_256&gt; for <a class=\"struct\" href=\"phaselock/demos/dentry/struct.State.html\" title=\"struct phaselock::demos::dentry::State\">State</a>","synthetic":false,"types":["phaselock::demos::dentry::State"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
