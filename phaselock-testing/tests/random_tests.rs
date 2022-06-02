9547355c6b0187ba84958351a484f729443a9b9f
Justin Restivo <justin@restivo.me>
2022-06-02 10:52:44 -0400

    fix: missing import

--- phaselock-testing/tests/random_tests.rs
+++ phaselock-testing/tests/random_tests.rs
@@ -7,7 +7,7 @@ use common::{get_tolerance, AppliedTestRunner, TestRoundResult, TestTransaction}
 use phaselock::traits::Storage;
 use phaselock::{
     demos::dentry::{DEntryBlock, State},
-    traits::implementations::{AtomicStorage, Libp2pNetwork, MemoryNetwork, MemoryStorage},
+    traits::implementations::{AtomicStorage, Libp2pNetwork, MemoryNetwork, MemoryStorage, WNetwork},
 };
 use phaselock_testing::{ConsensusRoundError, Round};
 use phaselock_types::data::ViewNumber;
