0d0cd9421d3aef5c44b62ec7d79454221d461dc3
Justin Restivo <justin@restivo.me>
2022-06-13 08:46:18 -0400

    feat: split libp2p into stress test and normal test

--- phaselock-testing/tests/libp2p.rs
+++ phaselock-testing/tests/libp2p.rs
@@ -24,6 +24,37 @@ async fn libp2p_network() {
         round_start_delay: 25,
         timeout_ratio: (1, 1),
         start_delay: 25,
+        total_nodes: 10,
+        start_nodes: 10,
+        num_succeeds: 15,
+        txn_ids: Right(1),
+        ..GeneralTestDescriptionBuilder::default()
+    };
+
+    description
+        .build::<Libp2pNetwork<
+            Message<
+                DEntryBlock,
+                <DEntryBlock as BlockContents<H_256>>::Transaction,
+                DemoState,
+                H_256,
+            >,
+        >, MemoryStorage<DEntryBlock, DemoState, H_256>, DEntryBlock, DemoState>()
+        .execute()
+        .await
+        .unwrap();
+}
+
+// stress test for libp2p
+#[async_std::test]
+#[instrument]
+#[ignore]
+async fn test_stress_libp2p_network() {
+    let description = GeneralTestDescriptionBuilder {
+        next_view_timeout: 600,
+        round_start_delay: 25,
+        timeout_ratio: (1, 1),
+        start_delay: 25,
         total_nodes: 15,
         start_nodes: 15,
         num_succeeds: 100,
