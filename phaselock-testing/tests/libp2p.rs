ca74cd83064c4f52a699d30502b36861d56efa3c
Justin Restivo <justin@restivo.me>
2022-06-14 13:21:21 -0400

    Libp2p-networking fix CI (#228)
    * feat: libp2p 0.45.0 -> 0.45.1
    
    * fix: libp2p add more time to tests

--- phaselock-testing/tests/libp2p.rs
+++ phaselock-testing/tests/libp2p.rs
@@ -21,14 +21,14 @@ use tracing::instrument;
 #[instrument]
 async fn libp2p_network() {
     let description = GeneralTestDescriptionBuilder {
-        next_view_timeout: 600,
         round_start_delay: 25,
         timeout_ratio: (1, 1),
-        start_delay: 25,
         total_nodes: 10,
         start_nodes: 10,
         num_succeeds: 15,
         txn_ids: Right(1),
+        next_view_timeout: 2000,
+        start_delay: 1000,
         ..GeneralTestDescriptionBuilder::default()
     };
 
@@ -54,10 +54,10 @@ async fn libp2p_network() {
 #[ignore]
 async fn test_stress_libp2p_network() {
     let description = GeneralTestDescriptionBuilder {
-        next_view_timeout: 600,
+        next_view_timeout: 2000,
         round_start_delay: 25,
         timeout_ratio: (1, 1),
-        start_delay: 25,
+        start_delay: 1000,
         total_nodes: 15,
         start_nodes: 15,
         num_succeeds: 100,
