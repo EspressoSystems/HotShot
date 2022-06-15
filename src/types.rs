3f69eac8a9857b8ed21d962d2308ea3c3c9a0f43
victor koenders <gpg@trangar.com>
2022-06-15 09:39:19 +0200

    Release 0.1.0 Re-exported ed25519 in the phaselock crate Re-exported traits::signature_key::SignatureKey

--- src/types.rs
+++ src/types.rs
@@ -7,3 +7,4 @@ pub use handle::PhaseLockHandle;
 
 pub(crate) use phaselock_types::error::PhaseLockError;
 pub use phaselock_types::message::{Commit, Decide, Message, NewView, PreCommit, Prepare, Vote};
+pub use phaselock_types::traits::signature_key::{ed25519, SignatureKey};
