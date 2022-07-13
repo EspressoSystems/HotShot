17a1dcc354c7c86fca000100ad644f50d8ef244a
Trangar <gpg@trangar.com>
2022-07-13 17:38:10 +0200

    Added .vscode/launch.json which should make it easier for people to debug our code (#304)
    * Added .vscode/launch.json which should make it easier for people to debug our code
    
    * Added documentation on how to debug
    
    * feat: debug shell with lldb
    
    * docs: document debugShell
    
    Co-authored-by: Justin Restivo <justin@restivo.me>

--- flake.nix
+++ flake.nix
@@ -147,6 +147,13 @@
              buildInputs = with pkgs; [ cargo-flamegraph fd cargo-llvm-cov fenixStable ripgrep ] ++ buildDeps ++ lib.optionals stdenv.isLinux [ heapstack_pkgs.heaptrack pkgs.valgrind ];
           };
 
+          # usage: brings in debugging tools including:
+          # - lldb: a debugger to be used with vscode
+          debugShell = pkgs.mkShell {
+            buildInputs =
+              with pkgs; [ fenixStable lldb ] ++ buildDeps;
+          };
+
         };
         # TODO uncomment when fetching dependencies is unborked
         # packages = pkgsAndChecksAttrSet.packages;
