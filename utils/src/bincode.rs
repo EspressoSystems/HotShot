0b1c3e63c4410d9d0c80c6933e1c42323b07a7ea
Justin Restivo <justin@restivo.me>
2022-07-19 10:43:17 -0400

    feat: explicit bincode options

--- utils/src/bincode.rs
+++ utils/src/bincode.rs
@@ -1,11 +1,31 @@
-#![allow(clippy::must_use_candidate, clippy::module_name_repetitions)]
-use bincode::DefaultOptions;
+#![allow(
+    clippy::must_use_candidate,
+    clippy::module_name_repetitions,
+    clippy::type_complexity
+)]
+use bincode::{
+    config::{
+        LittleEndian, RejectTrailing, VarintEncoding, WithOtherEndian, WithOtherIntEncoding,
+        WithOtherLimit, WithOtherTrailing,
+    },
+    DefaultOptions, Options,
+};
 
 /// For the wire format, we use bincode with the following options:
-///   - Limit of 16KiB per message
+///   - No upper size limit
 ///   - Litte endian encoding
 ///   - Varint encoding
 ///   - Reject trailing bytes
-pub fn bincode_opts() -> DefaultOptions {
+pub fn bincode_opts() -> WithOtherTrailing<
+    WithOtherIntEncoding<
+        WithOtherEndian<WithOtherLimit<DefaultOptions, bincode::config::Infinite>, LittleEndian>,
+        VarintEncoding,
+    >,
+    RejectTrailing,
+> {
     bincode::DefaultOptions::new()
+        .with_no_limit()
+        .with_little_endian()
+        .with_varint_encoding()
+        .reject_trailing_bytes()
 }
