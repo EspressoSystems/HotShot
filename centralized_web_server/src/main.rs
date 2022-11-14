f56ffe3923b478e9cfe7e639c815a2b2137dec12
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2022-11-14 15:08:14 -0500

    bug: ReadState is not implemented for WebServerState

--- centralized_web_server/src/main.rs
+++ centralized_web_server/src/main.rs
@@ -1,27 +1,76 @@
 // use tide_disco::{http::StatusCode, Api, App, Error, RequestError};
 // use std::io;
 
+use std::collections::HashMap;
+
+use clap::Args;
 use futures::FutureExt;
+use std::path::PathBuf;
+use tide_disco::api::ApiError;
 use tide_disco::error::ServerError;
+use tide_disco::method::ReadState;
 use tide_disco::Api;
 use tide_disco::App;
+use async_lock::RwLock;
 
-type State = ();
+// type State = WebServerState;
 type Error = ServerError;
+
+#[derive(Clone, Default)]
+struct WebServerState {
+    proposals: HashMap<u64, Vec<u8>>,
+    votes: HashMap<u64, Vec<Vec<u8>>>,
+}
+
+impl WebServerState {
+    fn new() -> Result<Self, ServerError> {
+        Ok(WebServerState::default())
+    }
+}
+
+#[derive(Args, Default)]
+pub struct Options {
+    #[arg(long = "availability-api-path", env = "ESPRESSO_AVAILABILITY_API_PATH")]
+    pub api_path: Option<PathBuf>,
+}
+
+fn define_api<State>(options: &Options) -> Result<Api<State, Error>, ApiError>
+where
+    State: 'static + Send + Sync + ReadState,
+    for<'a> &'a <State as ReadState>::State: Send + Sync,
+{
+    let mut api = match &options.api_path {
+        Some(path) => Api::<State, Error>::from_file(path)?,
+        None => {
+            let toml = toml::from_str(include_str!("../api.toml")).map_err(|err| {
+                ApiError::CannotReadToml {
+                    reason: err.to_string(),
+                }
+            })?;
+            Api::<State, Error>::new(toml)?
+        }
+    };
+    api.get("proposal", |req, state| {
+        async move { Ok("Hello, world!") }.boxed()
+    })?;
+    Ok(api)
+}
 
 #[async_std::main]
 async fn main() -> () {
     // let mut app = tide_disco::new();
     // app.at("/orders/shoes").post(order_shoes);
     // app.listen("127.0.0.1:8080").await?;
-    let spec =
-        toml::from_slice(&std::fs::read("./centralized_web_server/api.toml").unwrap()).unwrap();
-    let mut api = Api::<State, Error>::new(spec).unwrap();
-    let mut app = App::<State, Error>::with_state(());
-    api.get("proposal", |req, state| {
-        async move { Ok("Hello, world!") }.boxed()
-    })
-    .unwrap();
+    // let spec =
+    //     toml::from_slice(&std::fs::read("./centralized_web_server/api.toml").unwrap()).unwrap();
+    let options = Options::default();
+    let mut api = define_api(&options).unwrap();
+    let mut app = App::<WebServerState, Error>::with_state(RwLock<WebServerState>::new(WebServerState::default()));
+    // api.get("proposal", |req, state| {
+    //     async move { Ok("Hello, world!") }.boxed()
+    // })
+    // .unwrap();
+ 
 
     app.register_module("api", api);
     app.serve("http://localhost:8080").await;
