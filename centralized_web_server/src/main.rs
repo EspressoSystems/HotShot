9368c1e7b7226990744d28574dc008442c0e10ac
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2022-11-15 10:40:03 -0500

    bug: put_vote method not found in State as ReadState

--- centralized_web_server/src/main.rs
+++ centralized_web_server/src/main.rs
@@ -3,34 +3,41 @@
 
 use std::collections::HashMap;
 
+use async_lock::RwLock;
 use clap::Args;
 use futures::FutureExt;
-use tide_disco::method::WriteState;
 use std::path::PathBuf;
 use tide_disco::api::ApiError;
 use tide_disco::error::ServerError;
 use tide_disco::method::ReadState;
+use tide_disco::method::WriteState;
 use tide_disco::Api;
 use tide_disco::App;
-use async_lock::RwLock;
 
-// type State = WebServerState;
+type State = RwLock<WebServerState>;
 type Error = ServerError;
 
 #[derive(Clone, Default)]
 struct WebServerState {
     proposals: HashMap<u64, Vec<u8>>,
-    votes: HashMap<u64, Vec<Vec<u8>>>,
+    votes: HashMap<u128, Vec<Vec<u8>>>,
 }
 
 impl WebServerState {
     fn new() -> Result<Self, ServerError> {
         Ok(WebServerState::default())
     }
+
+    fn put_vote(&self, view_number: u128, vote: Vec<u8>) -> Result<&Self, ServerError> {
+        // TODO use entry here so we can modify votes vector in place
+        // self.votes.insert(view_number, vote);
+        Ok(self)
+    }
 }
 
 #[derive(Args, Default)]
 pub struct Options {
+    // TODO update below to be relevant to the centralized server
     #[arg(long = "availability-api-path", env = "ESPRESSO_AVAILABILITY_API_PATH")]
     pub api_path: Option<PathBuf>,
 }
@@ -52,11 +59,32 @@ where
         }
     };
     api.get("getproposal", |req, state| {
-        async move { Ok("Hello, world!") }.boxed()
+        async move {
+            let view_number: u128 = req.integer_param("view_number").unwrap();
+            Ok(format!(
+                "You requested the proposal for view {:?}",
+                view_number
+            ))
+        }
+        .boxed()
     })?
-    .post("postproposal",  |req, state| {
-        async move { Ok(()) }.boxed()
-    })?;
+    .post("vote", |req, state| {
+        async move {
+            let view_number: u128 = req.integer_param("view_number").unwrap();
+            // using body_bytes because we don't want to deserialize.  body_auto or body_json deserailizes
+            let vote = req.body_bytes();
+            // Add vote to state
+            // *state.votes.get(view_number);
+            (*state).put_vote(view_number, vote);
+
+            Ok(format!(
+                "You voted for view {:?}.  \n This is the vote you sent: {:?}",
+                view_number, vote
+            ))
+        }
+        .boxed()
+    })?
+    .post("postproposal", |req, state| async move { Ok(()) }.boxed())?;
     Ok(api)
 }
 
@@ -69,12 +97,11 @@ async fn main() -> () {
     //     toml::from_slice(&std::fs::read("./centralized_web_server/api.toml").unwrap()).unwrap();
     let options = Options::default();
     let mut api = define_api(&options).unwrap();
-    let mut app = App::<RwLock<WebServerState>, Error>::with_state(RwLock::new(WebServerState::default()));
+    let mut app = App::<RwLock<WebServerState>, Error>::with_state(State::default());
     // api.get("proposal", |req, state| {
     //     async move { Ok("Hello, world!") }.boxed()
     // })
     // .unwrap();
- 
 
     app.register_module("api", api);
     app.serve("http://0.0.0.0:8080").await;
