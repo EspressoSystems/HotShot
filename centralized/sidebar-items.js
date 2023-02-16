fde8a96332e02a15fd7c3eaaec83061a256e2a31
shenkeyao <shenkeyao@users.noreply.github.com>
2023-02-16 17:21:31 +0000

    deploy: 70f08777615affde9812232a046ad81bf51b13c2

--- centralized/sidebar-items.js
+++ centralized/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["main",""]],"mod":[["infra",""]],"type":[["ThisConfig",""],["ThisElection",""],["ThisLeaf",""],["ThisNetwork",""],["ThisNode",""],["ThisProposal",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["main",""]],"mod":[["infra",""]],"type":[["ThisConfig",""],["ThisElection",""],["ThisLeaf",""],["ThisNetwork",""],["ThisNode",""],["ThisProposal",""],["ThisVote",""]]};
