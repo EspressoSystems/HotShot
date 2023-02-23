36602119b0979c990f76c7f7a41186d3a8d67b10
DieracDelta <DieracDelta@users.noreply.github.com>
2023-02-23 00:51:05 +0000

    deploy: 2db172e763242ce5efa66122cab43601d9bd6986

--- source-files.js
+++ source-files.js
@@ -6,7 +6,7 @@ var sourcesIndex = JSON.parse('{\
 "hotshot":["",[["demos",[],["sdemo.rs","vdemo.rs"]],["tasks",[],["mod.rs"]],["traits",[["election",[],["static_committee.rs","vrf.rs"]],["networking",[],["centralized_server_network.rs","libp2p_network.rs","memory_network.rs"]],["storage",[],["memory_storage.rs"]]],["election.rs","networking.rs","node_implementation.rs","storage.rs"]],["types",[],["event.rs","handle.rs"]]],["certificate.rs","demos.rs","documentation.rs","lib.rs","traits.rs","types.rs"]],\
 "hotshot_centralized_server":["",[],["client.rs","clients.rs","config.rs","lib.rs","runs.rs"]],\
 "hotshot_consensus":["",[],["da_member.rs","leader.rs","lib.rs","next_leader.rs","replica.rs","sequencing_leader.rs","sequencing_replica.rs","traits.rs","utils.rs"]],\
-"hotshot_testing":["",[["impls",[],["mod.rs"]]],["launcher.rs","lib.rs","network_reliability.rs"]],\
+"hotshot_testing":["",[["macros",[],["mod.rs"]]],["launcher.rs","lib.rs","network_reliability.rs","test_description.rs","test_types.rs"]],\
 "hotshot_types":["",[["traits",[["signature_key",[["ed25519",[],["ed25519_priv.rs","ed25519_pub.rs"]]],["ed25519.rs"]]],["block_contents.rs","election.rs","metrics.rs","network.rs","node_implementation.rs","signature_key.rs","state.rs","storage.rs"]]],["certificate.rs","constants.rs","data.rs","error.rs","event.rs","lib.rs","message.rs","traits.rs","vote.rs"]],\
 "hotshot_utils":["",[],["bincode.rs","lib.rs"]],\
 "libp2p":["",[["infra",[],["mod.rs"]]],["types.rs","validator.rs"]],\
