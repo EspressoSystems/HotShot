7a6a41517779287e857d1ecb9e3786099babbe4f
Keyao Shen <shenkeyao@gmail.com>
2023-02-27 09:52:55 -0800

    Fix fmt

--- centralized_web_server/src/config.rs
+++ centralized_web_server/src/config.rs
@@ -1,23 +1,23 @@
 pub const DEFAULT_WEB_SERVER_PORT: u16 = 9000;
 
 pub fn get_proposal_route(view_number: u64) -> String {
-    format!("api/proposal/{}", view_number)
+    format!("api/proposal/{view_number}")
 }
 
 pub fn post_proposal_route(view_number: u64) -> String {
-    format!("api/proposal/{}", view_number)
+    format!("api/proposal/{view_number}")
 }
 
 pub fn get_vote_route(view_number: u64, index: u64) -> String {
-    format!("api/votes/{}/{}", view_number, index)
+    format!("api/votes/{view_number}/{index}")
 }
 
 pub fn post_vote_route(view_number: u64) -> String {
-    format!("api/votes/{}", view_number)
+    format!("api/votes/{view_number}")
 }
 
 pub fn get_transactions_route(index: u64) -> String {
-    format!("api/transactions/{}", index)
+    format!("api/transactions/{index}")
 }
 
 pub fn post_transactions_route() -> String {
