646f7f4f4a1193db5555df43937c5f08f1a5d881
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2023-04-13 09:14:56 -0400

    Update in place yes/no tests

--- consensus/src/replica.rs
+++ consensus/src/replica.rs
@@ -215,35 +215,35 @@ where
 
                                 // Generate and send vote
                                 // TODO ED Will remove once actual tests are in place
-                                // let message = if self.id % 4 == 0 {
-                                //     self.exchange.create_no_message(
-                                //         leaf.justify_qc.commit(),
-                                //         leaf_commitment,
-                                //         self.cur_view,
-                                //         vote_token,
-                                //     )
-                                // } else if self.id % 5 == 0 {
-                                //     self.exchange.create_timeout_message(
-                                //         leaf.justify_qc.clone(),
-                                //         self.cur_view,
-                                //         vote_token,
-                                //     )
-                                // }
-                                // else {
-                                //     self.exchange.create_yes_message(
-                                //         leaf.justify_qc.commit(),
-                                //         leaf_commitment,
-                                //         self.cur_view,
-                                //         vote_token,
-                                //     )
-                                // };
+                                let message = if self.id % 2 == 0 {
+                                    self.exchange.create_no_message(
+                                        leaf.justify_qc.commit(),
+                                        leaf_commitment,
+                                        self.cur_view,
+                                        vote_token,
+                                    )
+                                } else if self.id % 5 == 0 {
+                                    self.exchange.create_timeout_message(
+                                        leaf.justify_qc.clone(),
+                                        self.cur_view,
+                                        vote_token,
+                                    )
+                                }
+                                else {
+                                    self.exchange.create_yes_message(
+                                        leaf.justify_qc.commit(),
+                                        leaf_commitment,
+                                        self.cur_view,
+                                        vote_token,
+                                    )
+                                };
 
-                                let message = self.exchange.create_yes_message(
-                                    leaf.justify_qc.commit(),
-                                    leaf_commitment,
-                                    self.cur_view,
-                                    vote_token,
-                                );
+                                // let message = self.exchange.create_yes_message(
+                                //     leaf.justify_qc.commit(),
+                                //     leaf_commitment,
+                                //     self.cur_view,
+                                //     vote_token,
+                                // );
 
                                 let next_leader = self.exchange.get_leader(self.cur_view + 1);
 
