c71d014f7922b9940b6efb8c900c31c4d2ad91ee
Justin Restivo <justin@restivo.me>
2023-05-01 11:37:49 -0400

    feat: bump libp2p

--- examples/infra/mod.rs
+++ examples/infra/mod.rs
@@ -399,7 +399,7 @@ pub fn libp2p_generate_indexed_identity(seed: [u8; 32], index: u64) -> Keypair {
     hasher.update(&seed);
     hasher.update(&index.to_le_bytes());
     let new_seed = *hasher.finalize().as_bytes();
-    let sk_bytes = SecretKey::from_bytes(new_seed).unwrap();
+    let sk_bytes = SecretKey::try_from_bytes(new_seed).unwrap();
     let ed_kp = <EdKeypair as From<SecretKey>>::from(sk_bytes);
     #[allow(deprecated)]
     Keypair::Ed25519(ed_kp)
