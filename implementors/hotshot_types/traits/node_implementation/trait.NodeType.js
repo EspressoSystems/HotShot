947fe7001767c9e208a8db7798e2aa9e080d7401
DieracDelta <DieracDelta@users.noreply.github.com>
2023-05-08 13:06:29 +0000

    deploy: 30323f1d5826f2ea28444e05d12e38d3d22530a5

--- implementors/hotshot_types/traits/node_implementation/trait.NodeType.js
+++ implementors/hotshot_types/traits/node_implementation/trait.NodeType.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "hotshot":[["impl NodeType for <a class=\"struct\" href=\"hotshot/demos/vdemo/struct.VDemoTypes.html\" title=\"struct hotshot::demos::vdemo::VDemoTypes\">VDemoTypes</a>"]],
-"hotshot_testing":[["impl NodeType for <a class=\"struct\" href=\"hotshot_testing/test_types/struct.VrfTestTypes.html\" title=\"struct hotshot_testing::test_types::VrfTestTypes\">VrfTestTypes</a>"],["impl NodeType for <a class=\"struct\" href=\"hotshot_testing/test_types/struct.StaticCommitteeTestTypes.html\" title=\"struct hotshot_testing::test_types::StaticCommitteeTestTypes\">StaticCommitteeTestTypes</a>"]]
+"hotshot_testing":[["impl NodeType for <a class=\"struct\" href=\"hotshot_testing/test_types/struct.StaticCommitteeTestTypes.html\" title=\"struct hotshot_testing::test_types::StaticCommitteeTestTypes\">StaticCommitteeTestTypes</a>"],["impl NodeType for <a class=\"struct\" href=\"hotshot_testing/test_types/struct.VrfTestTypes.html\" title=\"struct hotshot_testing::test_types::VrfTestTypes\">VrfTestTypes</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
