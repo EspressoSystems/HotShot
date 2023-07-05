61c5fa2deb937f4169cea10a076d264477f66dde
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2023-07-05 16:19:19 -0400

    Add wireframe of orchestrator testing

--- testing/tests/orchestrator.rs
+++ testing/tests/orchestrator.rs
@@ -1,9 +1,17 @@
+use hotshot::traits::election::static_committee::StaticElectionConfig;
+use hotshot::types::ed25519::Ed25519Pub;
+use nll::nll_todo;
+use hotshot_orchestrator::run_orchestrator;
+
+use hotshot_orchestrator;
 
 #[cfg_attr(
     feature = "tokio-executor",
     tokio::test(flavor = "multi_thread", worker_threads = 2)
 )]
 #[cfg_attr(feature = "async-std-executor", async_std::test)]
-async fn test_orchestrator () {
+
+async fn test_orchestrator() {
+    run_orchestrator::<Ed25519Pub, StaticElectionConfig>(todo!(), todo!(), todo!()).await;
     assert!(true)
 }
