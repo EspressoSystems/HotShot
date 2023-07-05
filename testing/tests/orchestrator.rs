1466bfb4901e85b43f3911900a9b3a974c112bcd
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2023-07-05 16:14:19 -0400

    Add orchestrator test file to avoid circular dependency issues

---
+++ testing/tests/orchestrator.rs
@@ -0,0 +1,9 @@
+
+#[cfg_attr(
+    feature = "tokio-executor",
+    tokio::test(flavor = "multi_thread", worker_threads = 2)
+)]
+#[cfg_attr(feature = "async-std-executor", async_std::test)]
+async fn test_orchestrator () {
+    assert!(true)
+}
