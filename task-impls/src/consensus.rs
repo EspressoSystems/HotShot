7a0f512b9fe25f2578640ffbbf67b5f1ed5fa85e
Brendon Fish <bfish713@gmail.com>
2023-07-05 14:29:10 -0400

    async spawn

--- task-impls/src/consensus.rs
+++ task-impls/src/consensus.rs
@@ -554,7 +554,7 @@ where
                                     .register_state(state)
                                     .register_event_handler(handle_event);
                             let id = builder.get_task_id().unwrap();
-                            let _task = VoteCollectionTypes::build(builder).launch();
+                            let _task = async_spawn(VoteCollectionTypes::build(builder).launch() );
                             self.vote_collector = Some((vote.current_view, id));
                         }
                     }
