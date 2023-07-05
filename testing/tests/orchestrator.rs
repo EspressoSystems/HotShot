b32aeb2a1a6ef081035eb9733530e6e53d2b30fd
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2023-07-05 16:25:01 -0400

    Add network config constructor

--- testing/tests/orchestrator.rs
+++ testing/tests/orchestrator.rs
@@ -1,9 +1,7 @@
 use hotshot::traits::election::static_committee::StaticElectionConfig;
 use hotshot::types::ed25519::Ed25519Pub;
-use nll::nll_todo;
 use hotshot_orchestrator::run_orchestrator;
-
-use hotshot_orchestrator;
+use hotshot_orchestrator::config::NetworkConfig;
 
 #[cfg_attr(
     feature = "tokio-executor",
@@ -12,6 +10,7 @@ use hotshot_orchestrator;
 #[cfg_attr(feature = "async-std-executor", async_std::test)]
 
 async fn test_orchestrator() {
+    let config = NetworkConfig::<Ed25519Pub, StaticElectionConfig>::default();
     run_orchestrator::<Ed25519Pub, StaticElectionConfig>(todo!(), todo!(), todo!()).await;
     assert!(true)
 }
