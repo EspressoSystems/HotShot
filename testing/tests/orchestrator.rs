7668a85fb4e37c176bf3383c34a42d2cb605280a
Rohan Ahluwalia <rohanahluwalia@Rohans-MacBook-Pro.local>
2023-07-11 09:09:15 -0700

    returning hmtl output at endpoints

--- testing/tests/orchestrator.rs
+++ testing/tests/orchestrator.rs
@@ -2,6 +2,11 @@ use hotshot::traits::election::static_committee::StaticElectionConfig;
 use hotshot::types::ed25519::Ed25519Pub;
 use hotshot_orchestrator::run_orchestrator;
 use hotshot_orchestrator::config::NetworkConfig;
+use portpicker::pick_unused_port;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use surf_disco::error::ClientError;
+
+use hotshot_orchestrator::StatisticsStruct;
 
 #[cfg_attr(
     feature = "tokio-executor",
@@ -11,6 +16,27 @@ use hotshot_orchestrator::config::NetworkConfig;
 
 async fn test_orchestrator() {
     let config = NetworkConfig::<Ed25519Pub, StaticElectionConfig>::default();
-    run_orchestrator::<Ed25519Pub, StaticElectionConfig>(todo!(), todo!(), todo!()).await;
+    let port = 7777; //pick_unused_port().unwrap();
+    let ipaddr = IpAddr::V4(Ipv4Addr::new(0,0,0,0));
+    let base_url = format!("0.0.0.0:{port}");
+    run_orchestrator::<Ed25519Pub, StaticElectionConfig>(config,ipaddr,port).await;
+
+    let base_url = format!("http://{base_url}").parse().unwrap();
+    let client = surf_disco::Client::<ClientError>::new(base_url);
+    assert!(client.connect(None).await);
+
+    // just need to test the stats (not really needed for another endpoint)
+    let stat_data = StatisticsStruct {
+        stat_runduration: vec![10],
+        stat_viewtime: vec![20],
+        stat_throughput: vec![30],
+    };
+    client
+        .post::<()>("results")
+        .body_json(&stat_data)
+        .unwrap()
+        .send()
+        .await
+        .unwrap();
     assert!(true)
 }
