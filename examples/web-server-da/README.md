9aed871e6e65e1243316f825499b76f7164d50af
kmchicoine <kaley@espressosys.com>
2023-07-19 15:12:22 -0700

    add multi-web-server script

--- examples/web-server-da/README.md
+++ examples/web-server-da/README.md
@@ -1,13 +1,26 @@
 Commands to run da examples: 
-cargo run --example web-server --profile=release-lto --features="full-ci" <PORT_FOR_VALIDATING_CONSENSUS> 
+1a)Start web servers by either running 3 servers:
+cargo run --example web-server --profile=release-lto --features="full-ci" <PORT_FOR_CDN> 
 cargo run --example web-server --profile=release-lto --features="full-ci" <PORT_FOR_DA> 
 cargo run --example web-server --profile=release-lto --features="full-ci" <PORT_FOR_VIEW_SYNC> 
+
+1b)Or use multi-web-server to spin up all three:
+cargo run --example multi-web-server --profile=release-lto --features="full-ci" <PORT_FOR_CDN> <PORT_FOR_DA> <PORT_FOR_VIEW_SYNC> 
+
+2) Start orchestrator:
 cargo run --example web-server-da-orchestrator --features="full-ci,channel-async-std" --profile=release-lto -- <ORCHESTRATOR_ADDR> <ORCHESTRATOR_PORT> <ORCHESTRATOR_CONFIG_FILE> 
+
+3) Start validator:
 cargo run --profile=release-lto --example web-server-da-validator --features="full-ci" <ORCHESTRATOR_ADDR> <ORCHESTRATOR_PORT>
 
 I.e. 
 cargo run --example web-server --profile=release-lto --features="full-ci" 9000 
 cargo run --example web-server --profile=release-lto --features="full-ci" 9001 
 cargo run --example web-server --profile=release-lto --features="full-ci" 9002
+cargo run --example web-server-da-orchestrator --features="full-ci,channel-async-std" --profile=release-lto -- 0.0.0.0 4444 ./orchestrator/default-run-config.toml 
+cargo run --profile=release-lto --example web-server-da-validator --features="full-ci" 0.0.0.0 4444
+
+OR: 
+cargo run --example multi-web-server --profile=release-lto --features="full-ci" 9000 9001 9002
 cargo run --example web-server-da-orchestrator --features="full-ci,channel-async-std" --profile=release-lto -- 0.0.0.0 4444 ./orchestrator/default-run-config.toml 
 cargo run --profile=release-lto --example web-server-da-validator --features="full-ci" 0.0.0.0 4444
---
+++ examples/web-server-da/multi-web-server.rs
@@ -0,0 +1,64 @@
+use hotshot::demos::vdemo::VDemoTypes;
+use std::sync::Arc;
+
+use async_compatibility_layer::channel::oneshot;
+use async_compatibility_layer::art::async_spawn;
+use clap::Parser;
+use tracing::error;
+
+#[derive(Parser, Debug)]
+struct MultiWebServerArgs {
+    cdn_port: u16,
+    da_port: u16,
+    view_sync_port: u16,
+}
+#[async_std::main]
+async fn main() {
+    //KALEY TODO
+    // This doesn't work yet fyi
+    let args = MultiWebServerArgs::parse();
+    let (server_shutdown_sender_cdn, server_shutdown_cdn) = oneshot();
+    let (server_shutdown_sender_da, server_shutdown_da) = oneshot();
+    let (server_shutdown_sender_view_sync, server_shutdown_view_sync) = oneshot();
+    let _sender = Arc::new(server_shutdown_sender_cdn);
+    let _sender = Arc::new(server_shutdown_sender_da);
+    let _sender = Arc::new(server_shutdown_sender_view_sync);
+
+    let cdn_server = async_spawn(async move {
+        if let Err(e) = hotshot_web_server::run_web_server::<
+            <VDemoTypes as hotshot_types::traits::node_implementation::NodeType>::SignatureKey,
+        >(Some(server_shutdown_cdn), args.cdn_port)
+        .await {
+            error!(
+                "Problem starting cdn web server: {:?}",
+                e
+            );
+        }
+        error!("cdn");
+    });
+    let da_server = async_spawn(async move {
+        if let Err(e) = hotshot_web_server::run_web_server::<
+            <VDemoTypes as hotshot_types::traits::node_implementation::NodeType>::SignatureKey,
+        >(Some(server_shutdown_da), args.da_port)
+        .await {
+            error!(
+                "Problem starting da web server: {:?}",
+                e
+            );
+        }
+        error!("da");
+    });
+    let vs_server = async_spawn(async move {
+        if let Err(e) = hotshot_web_server::run_web_server::<
+            <VDemoTypes as hotshot_types::traits::node_implementation::NodeType>::SignatureKey,
+        >(Some(server_shutdown_view_sync), args.view_sync_port)
+        .await {
+            error!(
+                "Problem starting view sync web server: {:?}",
+                e
+            );
+        }
+        error!("vs");
+    });
+    let _result = futures::future::join_all(vec![cdn_server, da_server, vs_server]).await;
+}
