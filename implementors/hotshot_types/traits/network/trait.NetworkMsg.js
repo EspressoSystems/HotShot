78e9ecbcacba1d6c52f924e12033889d9dc477ed
philippecamacho <philippecamacho@users.noreply.github.com>
2023-07-20 04:05:22 +0000

    deploy: 987338b2e1b903ccbde7bcc0790624f89e76c0a9

--- implementors/hotshot_types/traits/network/trait.NetworkMsg.js
+++ implementors/hotshot_types/traits/network/trait.NetworkMsg.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hotshot":[["impl&lt;M: NetworkMsg&gt; NetworkMsg for <a class=\"struct\" href=\"hotshot/traits/networking/web_server_network/struct.SendMsg.html\" title=\"struct hotshot::traits::networking::web_server_network::SendMsg\">SendMsg</a>&lt;M&gt;"],["impl&lt;M: NetworkMsg&gt; NetworkMsg for <a class=\"struct\" href=\"hotshot/traits/networking/web_server_network/struct.RecvMsg.html\" title=\"struct hotshot::traits::networking::web_server_network::RecvMsg\">RecvMsg</a>&lt;M&gt;"]],
+"hotshot":[["impl&lt;M: NetworkMsg&gt; NetworkMsg for <a class=\"struct\" href=\"hotshot/traits/networking/web_server_network/struct.RecvMsg.html\" title=\"struct hotshot::traits::networking::web_server_network::RecvMsg\">RecvMsg</a>&lt;M&gt;"],["impl&lt;M: NetworkMsg&gt; NetworkMsg for <a class=\"struct\" href=\"hotshot/traits/networking/web_server_network/struct.SendMsg.html\" title=\"struct hotshot::traits::networking::web_server_network::SendMsg\">SendMsg</a>&lt;M&gt;"]],
 "hotshot_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
