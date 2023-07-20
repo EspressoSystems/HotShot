c7283bedb3cf150492012654d10c0e4e30938e7c
Rohan Ahluwalia <rohanahluwalia@Rohans-MacBook-Pro.local>
2023-07-20 12:13:49 -0700

    finished orchestrator with multiple tests

--- testing/tests/orchestrator.rs
+++ testing/tests/orchestrator.rs
@@ -4,7 +4,7 @@ use hotshot::types::ed25519::Ed25519Pub;
 use hotshot_orchestrator::config::NetworkConfig;
 use hotshot_orchestrator::run_orchestrator;
 use portpicker::pick_unused_port;
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::net::{IpAddr, Ipv4Addr};
 use surf_disco::error::ClientError;
 
 use hotshot_orchestrator::StatisticsStruct;
@@ -22,11 +22,10 @@ async fn test_simple_orchestrator() {
     let base_url = format!("127.0.0.1:{port}");
 
     async_spawn(async move {
-        run_orchestrator::<Ed25519Pub, StaticElectionConfig>(config, ipaddr, port).await;
+        let _ = run_orchestrator::<Ed25519Pub, StaticElectionConfig>(config, ipaddr, port).await;
     });
 
     let base_url = format!("http://{base_url}").parse().unwrap();
-    println!("base url: {}", base_url);
     let client = surf_disco::Client::<ClientError>::new(base_url);
     assert!(client.connect(None).await);
 
@@ -44,5 +43,54 @@ async fn test_simple_orchestrator() {
         .send()
         .await
         .unwrap();
-    assert!(true)
+    assert!(true);
+}
+
+#[cfg_attr(
+    feature = "tokio-executor",
+    tokio::test(flavor = "multi_thread", worker_threads = 2)
+)]
+#[cfg_attr(feature = "async-std-executor", async_std::test)]
+async fn test_orchestrator_multiplenodes(){
+    let config = NetworkConfig::<Ed25519Pub, StaticElectionConfig>::default();
+    let port = pick_unused_port().unwrap();
+    let ipaddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
+    let base_url = format!("127.0.0.1:{port}");
+
+    async_spawn(async move {
+        let _ = run_orchestrator::<Ed25519Pub, StaticElectionConfig>(config, ipaddr, port).await;
+    });
+
+    let base_url = format!("http://{base_url}").parse().unwrap();
+    let client = surf_disco::Client::<ClientError>::new(base_url);
+    assert!(client.connect(None).await);
+
+
+    let stat_data = StatisticsStruct {
+        stat_runduration: vec![10,15],
+        stat_throughput: vec![10,20,30],
+        stat_viewtime: vec![10],
+    };
+
+    let stat_data2 = StatisticsStruct {
+        stat_runduration: vec![10],
+        stat_throughput: vec![10],
+        stat_viewtime: vec![10],
+    };
+
+    client
+        .post::<()>("api/results/1")
+        .body_json(&stat_data)
+        .unwrap()
+        .send()
+        .await
+        .unwrap();
+    client
+        .post::<()>("api/results/1")
+        .body_json(&stat_data2)
+        .unwrap()
+        .send()
+        .await
+        .unwrap();
+    assert!(true);
 }
