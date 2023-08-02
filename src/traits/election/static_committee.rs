d1ac805ecd2f25447a9ae4f7422dc18d2cb9e842
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2023-08-01 20:35:59 -0400

    Updating so only 10 nodes are leaders

--- src/traits/election/static_committee.rs
+++ src/traits/election/static_committee.rs
@@ -109,7 +109,8 @@ where
     }
     /// Index the vector of public keys with the current view number
     fn get_leader(&self, view_number: TYPES::Time) -> PUBKEY {
-        let index = (*view_number % self.nodes.len() as u64) as usize;
+        // Testing benchmarking theory that leader is bottleneck
+        let index = (*view_number % 10 as u64) as usize;
         self.nodes[index].clone()
     }
 
