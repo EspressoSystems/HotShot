ccdbcf91fcd0d6b2f734c2f757ac6cecd1121231
elliedavidson <elliedavidson@users.noreply.github.com>
2023-08-07 20:41:14 +0000

    deploy: 5b102e7f1c483d6b7ee9edded0eac944735d7ce2

--- hotshot_web_server/sidebar-items.js
+++ hotshot_web_server/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["MAX_TXNS","MAX_VIEWS"],"fn":["define_api","run_web_server"],"mod":["config"],"struct":["Options","WebServerState"],"trait":["WebServerDataSource"],"type":["Error","State"]};
+window.SIDEBAR_ITEMS = {"constant":["MAX_TXNS","MAX_VIEWS","TX_BATCH_SIZE"],"fn":["define_api","run_web_server"],"mod":["config"],"struct":["Options","WebServerState"],"trait":["WebServerDataSource"],"type":["Error","State"]};
