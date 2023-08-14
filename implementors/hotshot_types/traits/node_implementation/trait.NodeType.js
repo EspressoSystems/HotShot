6b503814556b722d5448b03e9abee0355bae5210
DieracDelta <DieracDelta@users.noreply.github.com>
2023-08-14 17:18:46 +0000

    deploy: 15208acc85011c4ee934abddee11a7c1260ecd01

--- implementors/hotshot_types/traits/node_implementation/trait.NodeType.js
+++ implementors/hotshot_types/traits/node_implementation/trait.NodeType.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hotshot":[["impl NodeType for <a class=\"struct\" href=\"hotshot/demos/sdemo/struct.SDemoTypes.html\" title=\"struct hotshot::demos::sdemo::SDemoTypes\">SDemoTypes</a>"],["impl NodeType for <a class=\"struct\" href=\"hotshot/demos/vdemo/struct.VDemoTypes.html\" title=\"struct hotshot::demos::vdemo::VDemoTypes\">VDemoTypes</a>"]],
+"hotshot":[["impl NodeType for <a class=\"struct\" href=\"hotshot/demos/vdemo/struct.VDemoTypes.html\" title=\"struct hotshot::demos::vdemo::VDemoTypes\">VDemoTypes</a>"],["impl NodeType for <a class=\"struct\" href=\"hotshot/demos/sdemo/struct.SDemoTypes.html\" title=\"struct hotshot::demos::sdemo::SDemoTypes\">SDemoTypes</a>"]],
 "hotshot_testing":[["impl NodeType for <a class=\"struct\" href=\"hotshot_testing/node_types/struct.SequencingTestTypes.html\" title=\"struct hotshot_testing::node_types::SequencingTestTypes\">SequencingTestTypes</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
