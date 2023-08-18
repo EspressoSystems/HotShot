4bf416291820e46ba7f5d7372d2f82142ffb2319
Justin Restivo <justin@restivo.me>
2023-08-18 16:29:06 -0400

    feat: improve testing harness

--- testing/tests/libp2p.rs
+++ testing/tests/libp2p.rs
@@ -1,6 +1,10 @@
+use std::time::Duration;
+
 use hotshot_testing::{
+    completion_task::{CompletionTaskDescription, TimeBasedCompletionTaskDescription},
     node_types::{SequencingLibp2pImpl, SequencingTestTypes},
-    test_builder::TestMetadata,
+    overall_safety_task::OverallSafetyPropertiesDescription,
+    test_builder::{TestMetadata, TimingData},
 };
 use tracing::instrument;
 
@@ -14,7 +18,22 @@ use tracing::instrument;
 async fn libp2p_network() {
     async_compatibility_layer::logging::setup_logging();
     async_compatibility_layer::logging::setup_backtrace();
-    let metadata = TestMetadata::default_multiple_rounds();
+    let metadata = TestMetadata {
+        timing_data: TimingData {
+            start_delay: 12000000,
+            ..TimingData::default()
+        },
+        overall_safety_properties: OverallSafetyPropertiesDescription {
+            check_leaf: true,
+            ..Default::default()
+        },
+        completion_task_description: CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
+            TimeBasedCompletionTaskDescription {
+                duration: Duration::new(240, 0),
+            },
+        ),
+        ..TestMetadata::default_multiple_rounds()
+    };
 
     metadata
         .gen_launcher::<SequencingTestTypes, SequencingLibp2pImpl>()
