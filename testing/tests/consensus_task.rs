84de724d4b5e07066a05474881d6098474fa1aef
Brendon Fish <bfish713@gmail.com>
2023-08-18 09:25:08 -0400

    use add_consensus_task

--- testing/tests/consensus_task.rs
+++ testing/tests/consensus_task.rs
@@ -1,18 +1,17 @@
-use futures::FutureExt;
-use hotshot::types::SystemContextHandle;
-use hotshot::{certificate::QuorumCertificate, traits::TestableNodeImplementation, SystemContext};
-
 use async_compatibility_layer::art::async_spawn;
 use commit::Committable;
 use either::Right;
+use futures::FutureExt;
+use hotshot::traits::NodeImplementation;
+use hotshot::types::SystemContextHandle;
+use hotshot::{certificate::QuorumCertificate, traits::TestableNodeImplementation, SystemContext};
 
 use hotshot::rand::SeedableRng;
 use hotshot::traits::election::static_committee::GeneralStaticCommittee;
 
+use hotshot::tasks::add_consensus_task;
 use hotshot::traits::election::static_committee::StaticElectionConfig;
-
 use hotshot::traits::election::vrf::JfPubKey;
-
 use hotshot::traits::implementations::MemoryStorage;
 use hotshot::traits::Block;
 use hotshot::types::SignatureKey;
@@ -25,6 +24,7 @@ use hotshot_task::task::HotShotTaskTypes;
 use hotshot_task::task::{HandleEvent, HotShotTaskCompleted};
 use hotshot_task::task_impls::TaskBuilder;
 use hotshot_task::task_launcher::TaskRunner;
+use hotshot_task_impls::consensus::consensus_event_filter;
 use hotshot_task_impls::consensus::{self, ConsensusTaskTypes};
 use hotshot_types::message::Proposal;
 
@@ -71,203 +71,8 @@ use std::collections::HashMap;
 
 use std::marker::PhantomData;
 use std::sync::Arc;
-// TODO (Keyao) `pub` was added to avoid the `function never used` warning, but we can remove it
-// once we have unit tests using this function.
-async fn build_consensus_task<
-    TYPES: NodeType<
-        ConsensusType = SequencingConsensus,
-        ElectionConfigType = StaticElectionConfig,
-        SignatureKey = JfPubKey<BLSSignatureScheme>,
-        Time = ViewNumber,
-    >,
-    I: TestableNodeImplementation<
-        TYPES::ConsensusType,
-        TYPES,
-        Leaf = SequencingLeaf<TYPES>,
-        ConsensusMessage = SequencingMessage<TYPES, I>,
-        Storage = MemoryStorage<SequencingTestTypes, SequencingLeaf<SequencingTestTypes>>,
-    >,
->(
-    task_runner: TaskRunner,
-    event_stream: ChannelStream<SequencingHotShotEvent<TYPES, I>>,
-    handle: SystemContextHandle<TYPES, I>,
-) -> TaskRunner
-where
-    I::Exchanges: SequencingExchangesType<
-        TYPES,
-        Message<TYPES, I>,
-        Networks = (
-            StaticMemoryQuorumComm,
-            StaticMemoryViewSyncComm,
-            StaticMemoryDAComm,
-        ),
-        ElectionConfigs = (StaticElectionConfig, StaticElectionConfig),
-    >,
-    SequencingQuorumEx<TYPES, I>: ConsensusExchange<
-        TYPES,
-        Message<TYPES, I>,
-        Proposal = QuorumProposal<TYPES, SequencingLeaf<TYPES>>,
-        Certificate = QuorumCertificate<TYPES, SequencingLeaf<TYPES>>,
-        Commitment = SequencingLeaf<TYPES>,
-        Membership = StaticMembership,
-        Networking = StaticMemoryQuorumComm,
-    >,
-    CommitteeEx<TYPES, I>: ConsensusExchange<
-        TYPES,
-        Message<TYPES, I>,
-        Proposal = DAProposal<TYPES>,
-        Certificate = DACertificate<TYPES>,
-        Commitment = TYPES::BlockType,
-        Membership = StaticMembership,
-        Networking = StaticMemoryDAComm,
-    >,
-    ViewSyncEx<TYPES, I>: ConsensusExchange<
-        TYPES,
-        Message<TYPES, I>,
-        Proposal = ViewSyncCertificate<TYPES>,
-        Certificate = ViewSyncCertificate<TYPES>,
-        Commitment = ViewSyncData<TYPES>,
-        Membership = StaticMembership,
-        Networking = StaticMemoryViewSyncComm,
-    >,
-    // Why do we need this?
-    GeneralStaticCommittee<
-        SequencingTestTypes,
-        SequencingLeaf<SequencingTestTypes>,
-        JfPubKey<BLSSignatureScheme>,
-    >: Membership<TYPES>,
-{
-    let consensus = handle.get_consensus();
-    let c_api: HotShotSequencingConsensusApi<TYPES, I> = HotShotSequencingConsensusApi {
-        inner: handle.hotshot.inner.clone(),
-    };
 
-    let committee_exchange = c_api.inner.exchanges.committee_exchange().clone();
-
-    let registry = task_runner.registry.clone();
-    #[allow(deprecated)]
-    let consensus_state =
-        SequencingConsensusTaskState::<TYPES, I, HotShotSequencingConsensusApi<TYPES, I>> {
-            registry: registry.clone(),
-            consensus,
-            timeout: 1000,
-            cur_view: TYPES::Time::new(0),
-            block: TYPES::BlockType::new(),
-            quorum_exchange: c_api.inner.exchanges.quorum_exchange().clone().into(),
-            api: c_api.clone(),
-            committee_exchange: committee_exchange.clone().into(),
-            _pd: PhantomData,
-            vote_collector: None,
-            timeout_task: async_spawn(async move {}),
-            event_stream: event_stream.clone(),
-            output_event_stream: ChannelStream::new(),
-            certs: HashMap::new(),
-            current_proposal: None,
-            id: 1,
-            qc: None,
-        };
-    let consensus_event_handler = HandleEvent(Arc::new(
-        move |event,
-              mut state: SequencingConsensusTaskState<
-            TYPES,
-            I,
-            HotShotSequencingConsensusApi<TYPES, I>,
-        >| {
-            async move {
-                if let SequencingHotShotEvent::Shutdown = event {
-                    (Some(HotShotTaskCompleted::ShutDown), state)
-                } else {
-                    state.handle_event(event).await;
-                    (None, state)
-                }
-            }
-            .boxed()
-        },
-    ));
-    let consensus_name = "Consensus Task";
-    let consensus_event_filter = FilterEvent::default();
-
-    let consensus_task_builder = TaskBuilder::<
-        ConsensusTaskTypes<TYPES, I, HotShotSequencingConsensusApi<TYPES, I>>,
-    >::new(consensus_name.to_string())
-    .register_event_stream(event_stream.clone(), consensus_event_filter)
-    .await
-    .register_registry(&mut registry.clone())
-    .await
-    .register_state(consensus_state)
-    .register_event_handler(consensus_event_handler);
-    // impossible for unwrap to fail
-    // we *just* registered
-    let consensus_task_id = consensus_task_builder.get_task_id().unwrap();
-    let consensus_task = ConsensusTaskTypes::build(consensus_task_builder).launch();
-
-    task_runner.add_task(
-        consensus_task_id,
-        consensus_name.to_string(),
-        consensus_task,
-    )
-}
-
-async fn build_consensus_api<
-    TYPES: NodeType<
-        ConsensusType = SequencingConsensus,
-        ElectionConfigType = StaticElectionConfig,
-        SignatureKey = JfPubKey<BLSSignatureScheme>,
-        Time = ViewNumber,
-    >,
-    I: TestableNodeImplementation<
-        TYPES::ConsensusType,
-        TYPES,
-        Leaf = SequencingLeaf<TYPES>,
-        ConsensusMessage = SequencingMessage<TYPES, I>,
-        Storage = MemoryStorage<SequencingTestTypes, SequencingLeaf<SequencingTestTypes>>,
-    >,
->() -> SystemContextHandle<TYPES, I>
-where
-    I::Exchanges: SequencingExchangesType<
-        TYPES,
-        Message<TYPES, I>,
-        Networks = (
-            StaticMemoryQuorumComm,
-            StaticMemoryViewSyncComm,
-            StaticMemoryDAComm,
-        ),
-        ElectionConfigs = (StaticElectionConfig, StaticElectionConfig),
-    >,
-    SequencingQuorumEx<TYPES, I>: ConsensusExchange<
-        TYPES,
-        Message<TYPES, I>,
-        Proposal = QuorumProposal<TYPES, SequencingLeaf<TYPES>>,
-        Certificate = QuorumCertificate<TYPES, SequencingLeaf<TYPES>>,
-        Commitment = SequencingLeaf<TYPES>,
-        Membership = StaticMembership,
-        Networking = StaticMemoryQuorumComm,
-    >,
-    CommitteeEx<TYPES, I>: ConsensusExchange<
-        TYPES,
-        Message<TYPES, I>,
-        Proposal = DAProposal<TYPES>,
-        Certificate = DACertificate<TYPES>,
-        Commitment = TYPES::BlockType,
-        Membership = StaticMembership,
-        Networking = StaticMemoryDAComm,
-    >,
-    ViewSyncEx<TYPES, I>: ConsensusExchange<
-        TYPES,
-        Message<TYPES, I>,
-        Proposal = ViewSyncCertificate<TYPES>,
-        Certificate = ViewSyncCertificate<TYPES>,
-        Commitment = ViewSyncData<TYPES>,
-        Membership = StaticMembership,
-        Networking = StaticMemoryViewSyncComm,
-    >,
-    // Why do we need this?
-    GeneralStaticCommittee<
-        SequencingTestTypes,
-        SequencingLeaf<SequencingTestTypes>,
-        JfPubKey<BLSSignatureScheme>,
-    >: Membership<TYPES>,
-{
+async fn build_consensus_api() -> SystemContextHandle<SequencingTestTypes, SequencingMemoryImpl> {
     let builder = TestMetadata::default_multiple_rounds();
 
     let launcher = builder.gen_launcher::<SequencingTestTypes, SequencingMemoryImpl>();
@@ -280,35 +85,45 @@ where
     let view_sync_network = (launcher.resource_generator.view_sync_network)(network_generator);
     let storage = (launcher.resource_generator.storage)(node_id);
     let config = launcher.resource_generator.config.clone();
-    let initializer =
-        HotShotInitializer::<TYPES, I::Leaf>::from_genesis(I::block_genesis()).unwrap();
+    let initializer = HotShotInitializer::<
+        SequencingTestTypes,
+        <SequencingMemoryImpl as NodeImplementation<SequencingTestTypes>>::Leaf,
+    >::from_genesis(<SequencingMemoryImpl as TestableNodeImplementation<
+        SequencingConsensus,
+        SequencingTestTypes,
+    >>::block_genesis())
+    .unwrap();
 
     let known_nodes = config.known_nodes.clone();
-    let private_key = I::generate_test_key(node_id);
-    let public_key = TYPES::SignatureKey::from_private(&private_key);
+    let private_key = <SequencingMemoryImpl as TestableNodeImplementation<
+        SequencingConsensus,
+        SequencingTestTypes,
+    >>::generate_test_key(node_id);
+    let public_key = <SequencingTestTypes as NodeType>::SignatureKey::from_private(&private_key);
     let ek =
         jf_primitives::aead::KeyPair::generate(&mut rand_chacha::ChaChaRng::from_seed([0u8; 32]));
     let quorum_election_config = config.election_config.clone().unwrap_or_else(|| {
-        <QuorumEx<TYPES,I> as ConsensusExchange<
-                TYPES,
-                Message<TYPES, I>,
-            >>::Membership::default_election_config(config.total_nodes.get() as u64)
+        <QuorumEx<SequencingTestTypes, SequencingMemoryImpl> as ConsensusExchange<
+            SequencingTestTypes,
+            Message<SequencingTestTypes, SequencingMemoryImpl>,
+        >>::Membership::default_election_config(config.total_nodes.get() as u64)
     });
 
     let committee_election_config = config.election_config.clone().unwrap_or_else(|| {
-        <CommitteeEx<TYPES,I> as ConsensusExchange<
-                TYPES,
-                Message<TYPES, I>,
-            >>::Membership::default_election_config(config.total_nodes.get() as u64)
+        <CommitteeEx<SequencingTestTypes, SequencingMemoryImpl> as ConsensusExchange<
+            SequencingTestTypes,
+            Message<SequencingTestTypes, SequencingMemoryImpl>,
+        >>::Membership::default_election_config(config.total_nodes.get() as u64)
     });
-    let exchanges = I::Exchanges::create(
-        known_nodes.clone(),
-        (quorum_election_config, committee_election_config),
-        (quorum_network, view_sync_network, committee_network),
-        public_key.clone(),
-        private_key.clone(),
-        ek.clone(),
-    );
+    let exchanges =
+        <SequencingMemoryImpl as NodeImplementation<SequencingTestTypes>>::Exchanges::create(
+            known_nodes.clone(),
+            (quorum_election_config, committee_election_config),
+            (quorum_network, view_sync_network, committee_network),
+            public_key.clone(),
+            private_key.clone(),
+            ek.clone(),
+        );
     SystemContext::init(
         public_key,
         private_key,
@@ -392,11 +207,7 @@ async fn test_consensus_task() {
     async_compatibility_layer::logging::setup_logging();
     async_compatibility_layer::logging::setup_backtrace();
 
-    let handle = build_consensus_api::<
-        hotshot_testing::node_types::SequencingTestTypes,
-        hotshot_testing::node_types::SequencingMemoryImpl,
-    >()
-    .await;
+    let handle = build_consensus_api().await;
 
     let mut input = Vec::new();
     let mut output = HashMap::new();
@@ -413,8 +224,9 @@ async fn test_consensus_task() {
     output.insert(SequencingHotShotEvent::ViewChange(ViewNumber::new(2)), 2);
     output.insert(SequencingHotShotEvent::Shutdown, 1);
 
-    let build_fn =
-        |task_runner, event_stream| build_consensus_task(task_runner, event_stream, handle);
+    let build_fn = |task_runner, event_stream| {
+        add_consensus_task(task_runner, event_stream, ChannelStream::new(), handle)
+    };
 
     run_harness(input, output, build_fn).await;
 }
