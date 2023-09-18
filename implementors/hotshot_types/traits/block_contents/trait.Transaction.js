21f7ab1957dc9df22234e3be83365a62f9667a60
DieracDelta <DieracDelta@users.noreply.github.com>
2023-09-18 00:17:44 +0000

    deploy: 8ced68ce3bbba592d1989b5edbea14539d907ce5

--- implementors/hotshot_types/traits/block_contents/trait.Transaction.js
+++ implementors/hotshot_types/traits/block_contents/trait.Transaction.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hotshot":[["impl Transaction for <a class=\"struct\" href=\"hotshot/demos/sdemo/struct.SDemoTransaction.html\" title=\"struct hotshot::demos::sdemo::SDemoTransaction\">SDemoTransaction</a>"]],
+"hotshot":[["impl <a class=\"trait\" href=\"hotshot_types/traits/block_contents/trait.Transaction.html\" title=\"trait hotshot_types::traits::block_contents::Transaction\">Transaction</a> for <a class=\"struct\" href=\"hotshot/demos/sdemo/struct.SDemoTransaction.html\" title=\"struct hotshot::demos::sdemo::SDemoTransaction\">SDemoTransaction</a>"]],
 "hotshot_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
