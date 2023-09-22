ff698d842eb2823cfa73729d42954625b21de63e
rob-maron <rob-maron@users.noreply.github.com>
2023-09-22 13:21:02 +0000

    deploy: e8d34a2748eb153c339e725b2cf455b20136fc44

--- implementors/hotshot_types/traits/state/trait.TestableState.js
+++ implementors/hotshot_types/traits/state/trait.TestableState.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hotshot":[["impl TestableState for <a class=\"struct\" href=\"hotshot/demo/struct.SDemoState.html\" title=\"struct hotshot::demo::SDemoState\">SDemoState</a>"]],
+"hotshot":[["impl <a class=\"trait\" href=\"hotshot_types/traits/state/trait.TestableState.html\" title=\"trait hotshot_types::traits::state::TestableState\">TestableState</a> for <a class=\"struct\" href=\"hotshot/demo/struct.SDemoState.html\" title=\"struct hotshot::demo::SDemoState\">SDemoState</a>"]],
 "hotshot_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
