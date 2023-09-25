fc6d0f17c1ed028f027eb62876716613ee6514ca
DieracDelta <DieracDelta@users.noreply.github.com>
2023-09-25 00:18:13 +0000

    deploy: e8d34a2748eb153c339e725b2cf455b20136fc44

--- implementors/hotshot_types/traits/state/trait.TestableBlock.js
+++ implementors/hotshot_types/traits/state/trait.TestableBlock.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hotshot":[["impl <a class=\"trait\" href=\"hotshot_types/traits/state/trait.TestableBlock.html\" title=\"trait hotshot_types::traits::state::TestableBlock\">TestableBlock</a> for <a class=\"struct\" href=\"hotshot/block_impl/struct.VIDBlockPayload.html\" title=\"struct hotshot::block_impl::VIDBlockPayload\">VIDBlockPayload</a>"]],
+"hotshot":[["impl TestableBlock for <a class=\"struct\" href=\"hotshot/block_impl/struct.VIDBlockPayload.html\" title=\"struct hotshot::block_impl::VIDBlockPayload\">VIDBlockPayload</a>"]],
 "hotshot_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
