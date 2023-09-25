631976d803c264f37eaf8be33807acd6e9c5eea2
ggutoski <ggutoski@users.noreply.github.com>
2023-09-25 22:33:42 +0000

    deploy: e329b5b50c764788778ccc323e14a0814b7b22d2

--- implementors/hotshot_types/traits/block_contents/trait.Transaction.js
+++ implementors/hotshot_types/traits/block_contents/trait.Transaction.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hotshot":[["impl <a class=\"trait\" href=\"hotshot_types/traits/block_contents/trait.Transaction.html\" title=\"trait hotshot_types::traits::block_contents::Transaction\">Transaction</a> for <a class=\"struct\" href=\"hotshot/block_impl/struct.VIDTransaction.html\" title=\"struct hotshot::block_impl::VIDTransaction\">VIDTransaction</a>"]],
+"hotshot":[["impl Transaction for <a class=\"struct\" href=\"hotshot/block_impl/struct.VIDTransaction.html\" title=\"struct hotshot::block_impl::VIDTransaction\">VIDTransaction</a>"]],
 "hotshot_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
