8dcd8e553639d40d9af513e133435e5e95f6e4fb
rob-maron <rob-maron@users.noreply.github.com>
2023-10-03 15:38:52 +0000

    deploy: 42d19bad389dab76c2ba073a6ffbd5daf5f5b08c

--- implementors/hotshot_types/traits/node_implementation/trait.NodeType.js
+++ implementors/hotshot_types/traits/node_implementation/trait.NodeType.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hotshot":[["impl <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeType.html\" title=\"trait hotshot_types::traits::node_implementation::NodeType\">NodeType</a> for <a class=\"struct\" href=\"hotshot/demo/struct.DemoTypes.html\" title=\"struct hotshot::demo::DemoTypes\">DemoTypes</a>"]],
-"hotshot_testing":[["impl <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeType.html\" title=\"trait hotshot_types::traits::node_implementation::NodeType\">NodeType</a> for <a class=\"struct\" href=\"hotshot_testing/node_types/struct.SequencingTestTypes.html\" title=\"struct hotshot_testing::node_types::SequencingTestTypes\">SequencingTestTypes</a>"]]
+"hotshot":[["impl NodeType for <a class=\"struct\" href=\"hotshot/demo/struct.DemoTypes.html\" title=\"struct hotshot::demo::DemoTypes\">DemoTypes</a>"]],
+"hotshot_testing":[["impl NodeType for <a class=\"struct\" href=\"hotshot_testing/node_types/struct.SequencingTestTypes.html\" title=\"struct hotshot_testing::node_types::SequencingTestTypes\">SequencingTestTypes</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
