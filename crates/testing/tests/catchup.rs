a3a38e8311d3be17de2eafd8805601620f7f48a7
Keyao Shen <shenkeyao@gmail.com>
2023-11-28 16:44:09 -0800

    Fix build

--- crates/testing/tests/catchup.rs
+++ crates/testing/tests/catchup.rs
@@ -115,64 +115,6 @@ async fn test_catchup_web() {
         .await;
 }
 
-#[cfg(test)]
-#[cfg_attr(
-    async_executor_impl = "tokio",
-    tokio::test(flavor = "multi_thread", worker_threads = 2)
-)]
-#[cfg_attr(async_executor_impl = "async-std", async_std::test)]
-async fn test_catchup_web() {
-    use std::time::Duration;
-
-    use hotshot_testing::{
-        completion_task::{CompletionTaskDescription, TimeBasedCompletionTaskDescription},
-        node_types::{SequencingTestTypes, SequencingWebImpl},
-        overall_safety_task::OverallSafetyPropertiesDescription,
-        spinning_task::{ChangeNode, SpinningTaskDescription, UpDown},
-        test_builder::{TestMetadata, TimingData},
-    };
-
-    async_compatibility_layer::logging::setup_logging();
-    async_compatibility_layer::logging::setup_backtrace();
-    let timing_data = TimingData {
-        next_view_timeout: 1000,
-        ..Default::default()
-    };
-    let mut metadata = TestMetadata::default();
-    let catchup_nodes = vec![ChangeNode {
-        idx: 18,
-        updown: UpDown::Up,
-    }];
-
-    metadata.timing_data = timing_data;
-    metadata.start_nodes = 20;
-    metadata.total_nodes = 20;
-
-    metadata.spinning_properties = SpinningTaskDescription {
-        node_changes: vec![(Duration::from_millis(2500), catchup_nodes)],
-    };
-
-    metadata.completion_task_description =
-        CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
-            TimeBasedCompletionTaskDescription {
-                duration: Duration::from_millis(100000),
-            },
-        );
-    metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
-        check_leaf: true,
-        ..Default::default()
-    };
-
-    // only alow for the view which the catchup node hasn't started to fail
-    metadata.overall_safety_properties.num_failed_views = 1;
-
-    metadata
-        .gen_launcher::<SequencingTestTypes, SequencingWebImpl>()
-        .launch()
-        .run_test()
-        .await;
-}
-
 /// Test that one node catches up and has sucessful views after coming back
 #[cfg(test)]
 #[cfg_attr(
