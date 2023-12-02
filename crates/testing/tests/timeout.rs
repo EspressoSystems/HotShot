0f642fe20c3fd6a50945a0f5312671d16f7b01bb
rob-maron <132852777+rob-maron@users.noreply.github.com>
2023-12-02 18:31:47 -0500

    [Stability] Fix DHT timeout  (#2150)
    * separate out crypto tests
    
    * spinning task to be function of view
    
    * change number of rounds, fmt
    
    * ignore libp2p timeout test
    
    * dht fixes
    
    * down later
    
    * test
    
    * Revert "test"
    
    This reverts commit dc24ecb842494d7e2bc0cf66ee10283cf0ae547d.
    
    * Revert "down later"
    
    This reverts commit 9e0e25724d43ea8aa28959a8c75933881fae029d.
    
    * Revert "dht fixes"
    
    This reverts commit 2c7de9db65b2fe98790ddda0515d9a8024ababdd.
    
    * push
    
    * decrease dht timeout
    
    * change delays

--- crates/testing/tests/timeout.rs
+++ crates/testing/tests/timeout.rs
@@ -69,7 +69,6 @@ async fn test_timeout_web() {
     tokio::test(flavor = "multi_thread", worker_threads = 2)
 )]
 #[cfg_attr(async_executor_impl = "async-std", async_std::test)]
-#[ignore]
 async fn test_timeout_libp2p() {
     use std::time::Duration;
 
@@ -87,16 +86,19 @@ async fn test_timeout_libp2p() {
     async_compatibility_layer::logging::setup_backtrace();
     let timing_data = TimingData {
         next_view_timeout: 2000,
+        start_delay: 2000,
+        round_start_delay: 1000,
         ..Default::default()
     };
 
     let mut metadata = TestMetadata {
         total_nodes: 10,
         start_nodes: 10,
+        num_bootstrap_nodes: 10,
         ..Default::default()
     };
     let dead_nodes = vec![ChangeNode {
-        idx: 5,
+        idx: 9,
         updown: UpDown::Down,
     }];
 
@@ -108,7 +110,7 @@ async fn test_timeout_libp2p() {
     };
 
     metadata.spinning_properties = SpinningTaskDescription {
-        node_changes: vec![(2, dead_nodes)],
+        node_changes: vec![(5, dead_nodes)],
     };
 
     metadata.completion_task_description =
