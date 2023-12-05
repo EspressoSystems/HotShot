095d6ed1d4ba5a8260449e0dbc515bd91dfc5862
Keyao Shen <shenkeyao@gmail.com>
2023-12-05 11:49:43 -0800

    Make Committable a subtrait of BlockHeader

--- crates/types/src/traits/block_contents.rs
+++ crates/types/src/traits/block_contents.rs
@@ -95,7 +95,7 @@ pub fn vid_commitment(encoded_transactions: &Vec<u8>) -> <VidScheme as VidScheme
 
 /// Header of a block, which commits to a [`BlockPayload`].
 pub trait BlockHeader:
-    Serialize + Clone + Debug + Hash + PartialEq + Eq + Send + Sync + DeserializeOwned
+    Serialize + Clone + Debug + Hash + PartialEq + Eq + Send + Sync + DeserializeOwned + Committable
 {
     /// Block payload associated with the commitment.
     type Payload: BlockPayload;
