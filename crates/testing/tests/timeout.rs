9b74691dcfcb70bee9ef15f2c801de988f77c32c
Rob <rob@espressosys.com>
2023-12-13 12:23:27 -0500

    change to 10 seconds

--- crates/testing/tests/timeout.rs
+++ crates/testing/tests/timeout.rs
@@ -85,9 +85,9 @@ async fn test_timeout_libp2p() {
     async_compatibility_layer::logging::setup_logging();
     async_compatibility_layer::logging::setup_backtrace();
     let timing_data = TimingData {
-        next_view_timeout: 7000,
-        start_delay: 7000,
-        round_start_delay: 7000,
+        next_view_timeout: 10000,
+        start_delay: 10000,
+        round_start_delay: 10000,
         ..Default::default()
     };
 
