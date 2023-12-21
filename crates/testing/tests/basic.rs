f9d9289646d9c339fe88fb58fd9ba5dadf1879c3
Brendon Fish <bfish713@gmail.com>
2023-12-21 14:10:23 -0500

    send dac after skipped view

--- crates/testing/tests/basic.rs
+++ crates/testing/tests/basic.rs
@@ -110,8 +110,7 @@ async fn test_with_failures_half_f() {
         node_changes: vec![(5, dead_nodes)],
     };
 
-    // TODO: this should only have 3 failures for each down leader, investigate why it fails additional views
-    metadata.overall_safety_properties.num_failed_views = 8;
+    metadata.overall_safety_properties.num_failed_views = 3;
     // Make sure we keep commiting rounds after the bad leaders, but not the full 50 because of the numerous timeouts
     metadata.overall_safety_properties.num_successful_views = 22;
     metadata
@@ -176,6 +175,54 @@ async fn test_with_failures_f() {
     metadata.spinning_properties = SpinningTaskDescription {
         node_changes: vec![(5, dead_nodes)],
     };
+    metadata
+        .gen_launcher::<TestTypes, MemoryImpl>(0)
+        .launch()
+        .run_test()
+        .await;
+}
+
+/// Test that a good leader can succeed in the view directly after view sync
+#[cfg(test)]
+#[cfg_attr(
+    async_executor_impl = "tokio",
+    tokio::test(flavor = "multi_thread", worker_threads = 2)
+)]
+#[cfg_attr(async_executor_impl = "async-std", async_std::test)]
+async fn test_with_failures_2() {
+    use hotshot_testing::{
+        node_types::{MemoryImpl, TestTypes},
+        spinning_task::{ChangeNode, SpinningTaskDescription, UpDown},
+        test_builder::TestMetadata,
+    };
+
+    async_compatibility_layer::logging::setup_logging();
+    async_compatibility_layer::logging::setup_backtrace();
+    let mut metadata = TestMetadata::default_more_nodes();
+    // The first 14 (i.e., 20 - f) nodes are in the DA committee and we may shutdown the
+    // remaining 6 (i.e., f) nodes. We could remove this restriction after fixing the
+    // following issue.
+    // TODO: Update message broadcasting to avoid hanging
+    // <https://github.com/EspressoSystems/HotShot/issues/1567>
+    let dead_nodes = vec![
+        ChangeNode {
+            idx: 17,
+            updown: UpDown::Down,
+        },
+        ChangeNode {
+            idx: 18,
+            updown: UpDown::Down,
+        },
+    ];
+
+    metadata.spinning_properties = SpinningTaskDescription {
+        node_changes: vec![(5, dead_nodes)],
+    };
+
+    // 2 nodes fail triggering view sync, expect no other timeouts
+    metadata.overall_safety_properties.num_failed_views = 2;
+    // Make sure we keep commiting rounds after the bad leaders, but not the full 50 because of the numerous timeouts
+    metadata.overall_safety_properties.num_successful_views = 25;
     metadata
         .gen_launcher::<TestTypes, MemoryImpl>(0)
         .launch()
