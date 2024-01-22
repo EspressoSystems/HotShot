417d3d3c5eb174ee62c8fc8c5c88eb769738e24e
mrain <mrain@users.noreply.github.com>
2024-01-22 17:00:14 +0000

    deploy: 390f3c98f287e1b3e56b877088e275a79780918f

--- trait.impl/tagged_base64/trait.Tagged.js
+++ trait.impl/tagged_base64/trait.Tagged.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
-"hotshot_stake_table":[["impl Tagged for <a class=\"struct\" href=\"hotshot_stake_table/mt_based/internal/struct.MerkleCommitment.html\" title=\"struct hotshot_stake_table::mt_based::internal::MerkleCommitment\">MerkleCommitment</a>"]]
+"hotshot_stake_table":[["impl Tagged for <a class=\"struct\" href=\"hotshot_stake_table/mt_based/internal/struct.MerkleCommitment.html\" title=\"struct hotshot_stake_table::mt_based::internal::MerkleCommitment\">MerkleCommitment</a>"]],
+"hotshot_types":[["impl&lt;F: PrimeField&gt; Tagged for <a class=\"struct\" href=\"hotshot_types/light_client/struct.LightClientState.html\" title=\"struct hotshot_types::light_client::LightClientState\">LightClientState</a>&lt;F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
