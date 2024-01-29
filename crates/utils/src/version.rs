d05a81b2881add29ab7839de6458fa9033c3498f
ss-es <155648797+ss-es@users.noreply.github.com>
2024-01-29 01:00:48 -0500

    fixed read_version panic, added tests

--- crates/utils/src/version.rs
+++ crates/utils/src/version.rs
@@ -3,19 +3,32 @@
 use hotshot_constants::Version;
 
 /// Read the version number from a message (passed a byte vector),
-/// returning None if there are not enough bytes.
+/// returning `None` is there are not enough bytes.
 #[must_use]
 #[allow(clippy::module_name_repetitions)]
 pub fn read_version(message: &[u8]) -> Option<Version> {
-    let maybe_bytes_major: Result<[u8; 2], _> = message[0..2].try_into();
-    let maybe_bytes_minor: Result<[u8; 2], _> = message[2..4].try_into();
+    let bytes_major = message.get(0..2)?.try_into().ok()?;
+    let bytes_minor = message.get(2..4)?.try_into().ok()?;
+    let major = u16::from_le_bytes(bytes_major);
+    let minor = u16::from_le_bytes(bytes_minor);
 
-    match (maybe_bytes_major, maybe_bytes_minor) {
-        (Ok(bytes_major), Ok(bytes_minor)) => {
-            let major = u16::from_le_bytes(bytes_major);
-            let minor = u16::from_le_bytes(bytes_minor);
-            Some(Version { major, minor })
-        }
-        _ => None,
+    Some(Version { major, minor })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_version_test() {
+        let bytes: [u8; 6] = [0, 0, 1, 0, 4, 9];
+        let version = Version { major: 0, minor: 1 };
+        assert_eq!(read_version(&bytes), None);
+    }
+
+    #[test]
+    fn read_version_insufficient_bytes_test() {
+        let bytes: [u8; 3] = [0, 0, 0];
+        assert_eq!(read_version(&bytes), None);
     }
 }
