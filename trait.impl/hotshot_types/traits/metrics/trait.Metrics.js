5be12c6a8956350967eacf719089cecb668ab26f
DieracDelta <DieracDelta@users.noreply.github.com>
2024-02-01 21:41:40 +0000

    deploy: caa8cb80f78643027da32b22d5eda8a99928f8c1

--- trait.impl/hotshot_types/traits/metrics/trait.Metrics.js
+++ trait.impl/hotshot_types/traits/metrics/trait.Metrics.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hotshot":[["impl Metrics for <a class=\"struct\" href=\"hotshot/traits/networking/struct.NetworkingMetrics.html\" title=\"struct hotshot::traits::networking::NetworkingMetrics\">NetworkingMetrics</a>"]],
+"hotshot":[["impl <a class=\"trait\" href=\"hotshot_types/traits/metrics/trait.Metrics.html\" title=\"trait hotshot_types::traits::metrics::Metrics\">Metrics</a> for <a class=\"struct\" href=\"hotshot/traits/networking/struct.NetworkingMetrics.html\" title=\"struct hotshot::traits::networking::NetworkingMetrics\">NetworkingMetrics</a>"]],
 "hotshot_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
