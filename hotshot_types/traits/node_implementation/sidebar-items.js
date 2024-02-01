6a09be2941fcd32af8c115e558e21dee8c6fc314
bfish713 <bfish713@users.noreply.github.com>
2024-02-01 15:42:45 +0000

    deploy: e457776d3cfaa510fd3b63d96c23164cc7dc4d8d

--- hotshot_types/traits/node_implementation/sidebar-items.js
+++ hotshot_types/traits/node_implementation/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["ChannelMaps","SendToTasks","ViewQueue"],"trait":["ConsensusTime","NodeImplementation","NodeType","TestableNodeImplementation"]};
+window.SIDEBAR_ITEMS = {"trait":["ConsensusTime","NodeImplementation","NodeType","TestableNodeImplementation"]};
