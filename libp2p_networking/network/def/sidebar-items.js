230f5eb016907c13045f9b1c93281a1a160d62d1
elliedavidson <elliedavidson@users.noreply.github.com>
2024-02-05 20:23:45 +0000

    deploy: d5ad8ad59554b6059f149ba0e6cf46d2898ae11e

--- libp2p_networking/network/def/sidebar-items.js
+++ libp2p_networking/network/def/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["NUM_REPLICATED_TO_TRUST","_DERIVE_core_fmt_Debug_FOR_NetworkDef"],"struct":["NetworkDef"]};
+window.SIDEBAR_ITEMS = {"constant":["_DERIVE_core_fmt_Debug_FOR_NetworkDef"],"struct":["NetworkDef"]};
