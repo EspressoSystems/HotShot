e4784c2edaa84c944069c4971791fd1ef71425ad
bfish713 <bfish713@users.noreply.github.com>
2024-02-12 15:20:00 +0000

    deploy: f0e4ccca02282b15b544dc95f87fb3fc86b0781a

--- trait.impl/hotshot_types/traits/states/trait.InstanceState.js
+++ trait.impl/hotshot_types/traits/states/trait.InstanceState.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hotshot_testing":[["impl InstanceState for <a class=\"struct\" href=\"hotshot_testing/state_types/struct.TestInstanceState.html\" title=\"struct hotshot_testing::state_types::TestInstanceState\">TestInstanceState</a>"]]
+"hotshot_testing":[["impl <a class=\"trait\" href=\"hotshot_types/traits/states/trait.InstanceState.html\" title=\"trait hotshot_types::traits::states::InstanceState\">InstanceState</a> for <a class=\"struct\" href=\"hotshot_testing/state_types/struct.TestInstanceState.html\" title=\"struct hotshot_testing::state_types::TestInstanceState\">TestInstanceState</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
