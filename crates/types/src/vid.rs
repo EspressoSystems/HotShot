07ebe8392f2f6b0fca7bd30d3b08905f9028308e
Gus Gutoski <ggutoski@users.noreply.github.com>
2024-02-26 20:19:24 -0500

    switch to newtype to hide impl details of VidScheme

--- crates/types/src/vid.rs
+++ crates/types/src/vid.rs
@@ -1,30 +1,30 @@
 //! A constructor for [`VidScheme`].
 
 use ark_bls12_381::Bls12_381;
-use ark_ec::pairing::Pairing;
 use jf_primitives::{
-    merkle_tree::hasher::HasherDigest,
     pcs::{prelude::UnivariateKzgPCS, PolynomialCommitmentScheme},
     vid::{
         advz::{
             payload_prover::{LargeRangeProof, SmallRangeProof},
             Advz,
         },
-        payload_prover::PayloadProver,
+        payload_prover::{PayloadProver, Statement},
         precomputable::Precomputable,
+        VidDisperse, VidResult, VidScheme,
     },
 };
-use serde::{de::DeserializeOwned, Serialize};
+use serde::{Deserialize, Serialize};
 use sha2::Sha256;
-use std::fmt::Debug;
+use std::{fmt::Debug, ops::Range};
 
 /// VID scheme constructor.
 ///
 /// We prefer a return type of the form `impl VidScheme`.
 /// But it's currently impossible to name an impl Trait return type:
 /// [Naming impl trait in return types - Impl trait initiative](https://rust-lang.github.io/impl-trait-initiative/explainer/rpit_names.html)
+/// For example: we cannot name any assoc types for `VidScheme` such as `Commit`, etc.
 /// So instead we return a newtype that impls `VidScheme` via delegation.
-pub fn vid_scheme(num_storage_nodes: usize) -> impl VidSchemeTrait {
+pub fn vid_scheme(num_storage_nodes: usize) -> VidSchemeType {
     // TODO use a proper SRS
     // https://github.com/EspressoSystems/HotShot/issues/1686
     let srs = crate::data::test_srs(num_storage_nodes);
@@ -38,46 +38,184 @@ pub fn vid_scheme(num_storage_nodes: usize) -> impl VidSchemeTrait {
     let multiplicity = 1;
 
     // TODO panic, return `Result`, or make `new` infallible upstream (eg. by panicking)?
-    AdvzVidScheme::new(chunk_size, num_storage_nodes, multiplicity, srs).unwrap_or_else(|err| panic!("advz construction failure:\n\t(num_storage nodes,chunk_size,multiplicity)=({num_storage_nodes},{chunk_size},{multiplicity})\n\terror: : {err}"))
+    VidSchemeType(Advz::new(chunk_size, num_storage_nodes, multiplicity, srs).unwrap_or_else(|err| panic!("advz construction failure:\n\t(num_storage nodes,chunk_size,multiplicity)=({num_storage_nodes},{chunk_size},{multiplicity})\n\terror: : {err}")))
 }
 
 // TODO can't name the return type of `vid_scheme`:
 // https://rust-lang.github.io/impl-trait-initiative/explainer/rpit_names.html
 // pub type VidCommitment = <<vid_scheme as FnOnce(usize)>::Output as VidScheme>::Commit;
 
-// Private type alias for `Advz`.
-// Set curve/pairing and hash function here.
-type AdvzVidScheme = Advz<Bls12_381, Sha256>;
+type E = Bls12_381;
+type H = Sha256;
 
-/// wrapper trait workaround for nested impl Trait
-pub trait VidSchemeTrait:
-    PayloadProver<Self::NamespaceProof> + PayloadProver<Self::TransactionProof> + Precomputable
-{
-    /// Namespace proof
-    type NamespaceProof: Clone + Debug + Eq + PartialEq + Serialize + DeserializeOwned;
-    /// Transaction proof
-    type TransactionProof: Clone + Debug + Eq + PartialEq + Serialize + DeserializeOwned;
+/// TODO doc
+pub struct VidSchemeType(Advz<E, H>);
+
+/// TODO doc
+///
+/// # Type complexity
+///
+/// Jellyfish's `LargeRangeProof` type has a prime field generic parameter `F`.
+/// This `F` is determined by the pairing parameter for `Advz` currently returned by `test_vid_factory()`.
+/// Jellyfish needs a more ergonomic way for downstream users to refer to this type.
+///
+/// There is a `KzgEval` type alias in jellyfish that helps a little, but it's currently private:
+/// https://github.com/EspressoSystems/jellyfish/issues/423
+/// If it were public then we could instead use
+/// `LargeRangeProof<KzgEval<E>>`
+/// but that's still pretty crufty.
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct NamespaceProofType(
+    LargeRangeProof<<UnivariateKzgPCS<E> as PolynomialCommitmentScheme>::Evaluation>,
+);
+
+/// TODO doc
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct TransactionProofType(
+    SmallRangeProof<<UnivariateKzgPCS<E> as PolynomialCommitmentScheme>::Proof>,
+);
+
+// BOILERPLATE FROM HERE TO THE END
+
+impl VidScheme for VidSchemeType {
+    type Commit = <Advz<E, H> as VidScheme>::Commit;
+    type Share = <Advz<E, H> as VidScheme>::Share;
+    type Common = <Advz<E, H> as VidScheme>::Common;
+
+    fn commit_only<B>(&self, payload: B) -> VidResult<Self::Commit>
+    where
+        B: AsRef<[u8]>,
+    {
+        self.0.commit_only(payload)
+    }
+
+    fn disperse<B>(&self, payload: B) -> VidResult<VidDisperse<Self>>
+    where
+        B: AsRef<[u8]>,
+    {
+        self.0.disperse(payload).map(|res| VidDisperse {
+            shares: res.shares,
+            common: res.common,
+            commit: res.commit,
+        })
+    }
+
+    fn verify_share(
+        &self,
+        share: &Self::Share,
+        common: &Self::Common,
+        commit: &Self::Commit,
+    ) -> VidResult<Result<(), ()>> {
+        self.0.verify_share(share, common, commit)
+    }
+
+    fn recover_payload(&self, shares: &[Self::Share], common: &Self::Common) -> VidResult<Vec<u8>> {
+        self.0.recover_payload(shares, common)
+    }
+
+    fn is_consistent(commit: &Self::Commit, common: &Self::Common) -> VidResult<()> {
+        <Advz<E, H> as VidScheme>::is_consistent(commit, common)
+    }
+
+    fn get_payload_byte_len(common: &Self::Common) -> usize {
+        <Advz<E, H> as VidScheme>::get_payload_byte_len(common)
+    }
+
+    fn get_num_storage_nodes(common: &Self::Common) -> usize {
+        <Advz<E, H> as VidScheme>::get_num_storage_nodes(common)
+    }
+
+    fn get_multiplicity(common: &Self::Common) -> usize {
+        <Advz<E, H> as VidScheme>::get_multiplicity(common)
+    }
 }
 
-impl<E, H> VidSchemeTrait for Advz<E, H>
-where
-    E: Pairing,
-    H: HasherDigest,
-{
-    /// # Type complexity
-    ///
-    /// Jellyfish's `LargeRangeProof` type has a prime field generic parameter `F`.
-    /// This `F` is determined by the pairing parameter for `Advz` currently returned by `test_vid_factory()`.
-    /// Jellyfish needs a more ergonomic way for downstream users to refer to this type.
-    ///
-    /// There is a `KzgEval` type alias in jellyfish that helps a little, but it's currently private:
-    /// https://github.com/EspressoSystems/jellyfish/issues/423
-    /// If it were public then we could instead use
-    /// `LargeRangeProof<KzgEval<E>>`
-    /// but that's still pretty crufty.
-    type NamespaceProof =
-        LargeRangeProof<<UnivariateKzgPCS<E> as PolynomialCommitmentScheme>::Evaluation>;
+impl PayloadProver<NamespaceProofType> for VidSchemeType {
+    fn payload_proof<B>(&self, payload: B, range: Range<usize>) -> VidResult<NamespaceProofType>
+    where
+        B: AsRef<[u8]>,
+    {
+        self.0
+            .payload_proof(payload, range)
+            .map(|res| NamespaceProofType(res))
+    }
 
-    type TransactionProof =
-        SmallRangeProof<<UnivariateKzgPCS<E> as PolynomialCommitmentScheme>::Proof>;
+    fn payload_verify(
+        &self,
+        stmt: Statement<'_, Self>,
+        proof: &NamespaceProofType,
+    ) -> VidResult<Result<(), ()>> {
+        self.0.payload_verify(
+            Statement {
+                payload_subslice: stmt.payload_subslice,
+                range: stmt.range,
+                commit: stmt.commit,
+                common: stmt.common,
+            },
+            &proof.0,
+        )
+    }
 }
+
+impl PayloadProver<TransactionProofType> for VidSchemeType {
+    fn payload_proof<B>(&self, payload: B, range: Range<usize>) -> VidResult<TransactionProofType>
+    where
+        B: AsRef<[u8]>,
+    {
+        self.0
+            .payload_proof(payload, range)
+            .map(|res| TransactionProofType(res))
+    }
+
+    fn payload_verify(
+        &self,
+        stmt: Statement<'_, Self>,
+        proof: &TransactionProofType,
+    ) -> VidResult<Result<(), ()>> {
+        self.0.payload_verify(
+            Statement {
+                payload_subslice: stmt.payload_subslice,
+                range: stmt.range,
+                commit: stmt.commit,
+                common: stmt.common,
+            },
+            &proof.0,
+        )
+    }
+}
+
+impl Precomputable for VidSchemeType {
+    type PrecomputeData = <Advz<E, H> as Precomputable>::PrecomputeData;
+
+    fn commit_only_precompute<B>(
+        &self,
+        payload: B,
+    ) -> VidResult<(Self::Commit, Self::PrecomputeData)>
+    where
+        B: AsRef<[u8]>,
+    {
+        self.0.commit_only_precompute(payload)
+    }
+
+    fn disperse_precompute<B>(
+        &self,
+        payload: B,
+        data: &Self::PrecomputeData,
+    ) -> VidResult<VidDisperse<Self>>
+    where
+        B: AsRef<[u8]>,
+    {
+        self.0
+            .disperse_precompute(payload, data)
+            .map(|res| VidDisperse {
+                shares: res.shares,
+                common: res.common,
+                commit: res.commit,
+            })
+    }
+}
+
+// Foreign trait rules prevent us from doing:
+// `impl From<VidDisperse<VidSchemeType>> for VidDisperse<Advz<E, H>>`
+// and similarly for `Statement`
