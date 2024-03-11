af4bf361cae83899067fd979b59ed836743d7111
bfish713 <bfish713@users.noreply.github.com>
2024-03-11 17:34:17 +0000

    deploy: 3b0ddfcb0b1d701ebc6f7736967443f3e84b05e2

--- hotshot/traits/networking/combined_network/sidebar-items.js
+++ hotshot/traits/networking/combined_network/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["calculate_hash_of"],"struct":["Cache","CombinedNetworks","UnderlyingCombinedNetworks"],"type":["DelayedTasksLockedMap"]};
+window.SIDEBAR_ITEMS = {"fn":["calculate_hash_of"],"struct":["CombinedNetworks","UnderlyingCombinedNetworks"],"type":["DelayedTasksLockedMap"]};
