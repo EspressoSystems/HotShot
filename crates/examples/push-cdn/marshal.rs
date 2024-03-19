c32c5a40a3cc2e34c2d4a28d832bac3195ca041a
Rob <rob@espressosys.com>
2024-03-19 11:50:01 -0400

    fmt

--- crates/examples/push-cdn/marshal.rs
+++ crates/examples/push-cdn/marshal.rs
@@ -1,6 +1,6 @@
 //! The following is the main `Marshal` binary, which just instantiates and runs
 //! a `Marshal` object with the HotShot types.
-//! 
+//!
 use anyhow::{Context, Result};
 use cdn_broker::reexports::connection::protocols::Quic;
 use cdn_marshal::{ConfigBuilder, Marshal};
