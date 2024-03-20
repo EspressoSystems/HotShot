2ef6e6c0b51b1817b937d30b22ba2255f8efb633
Brendon Fish <bfish713@gmail.com>
2024-03-19 23:23:17 -0400

    fix lint

--- crates/task-impls/src/network.rs
+++ crates/task-impls/src/network.rs
@@ -6,7 +6,7 @@ use async_broadcast::Sender;
 use async_compatibility_layer::art::async_spawn;
 use async_lock::RwLock;
 use either::Either::{self, Left, Right};
-use hotshot_types::{constants::VERSION_0_1, event::HotShotAction, traits::storage::Storage};
+use hotshot_types::{event::HotShotAction, traits::storage::Storage};
 use std::sync::Arc;
 
 use hotshot_task::task::{Task, TaskState};
