8eac9c675cf2b147c821df552b8d5a49dfe851cf
QuentinI <QuentinI@users.noreply.github.com>
2024-03-25 03:04:44 +0000

    deploy: ddde95714a4c0030bc1422e245cb7cc4e0e330e2

--- hotshot_testing/block_builder/sidebar-items.js
+++ hotshot_testing/block_builder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["run_random_builder"],"struct":["BlockEntry","RandomBuilderOptions","RandomBuilderSource"]};
+window.SIDEBAR_ITEMS = {"fn":["build_block","make_simple_builder","run_random_builder"],"struct":["BlockEntry","RandomBuilderOptions","RandomBuilderSource","SimpleBuilderSource","SimpleBuilderTask"]};
