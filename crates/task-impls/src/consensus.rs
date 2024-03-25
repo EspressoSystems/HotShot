db3fcfafccdc909845de47e2dd241662c3849b77
Brendon Fish <bfish713@gmail.com>
2024-03-25 10:11:29 -0400

    [Catchup] Store High QC When it is Updated (#2807)
    * add more action types
    
    * integrate new storage type
    
    * fix build, tie up stragglers, modifying tasks next
    
    * merge latest stable tabs
    
    * merge latest tags and fix method signatures
    
    * working towards adding action to storage
    
    * Store high qc whenever we update it
    
    * fix lint
    
    * adding high_qc to initializer and tests
    
    * typos and if let instead of match
    
    * better comment
    
    * adding state to storage
    
    * add state to initializer
    
    * split high qc and undecided state storage
    
    * fix new clippy lints
    
    ---------
    
    Co-authored-by: Jarred Parr <jparr721@gmail.com>

--- crates/task-impls/src/consensus.rs
+++ crates/task-impls/src/consensus.rs
@@ -513,6 +513,19 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES>, A: ConsensusApi<TYPES, I> + 
                     }
                 };
 
+                if justify_qc.get_view_number() > consensus.high_qc.view_number {
+                    if let Err(e) = self
+                        .storage
+                        .write()
+                        .await
+                        .update_high_qc(justify_qc.clone())
+                        .await
+                    {
+                        warn!("Failed to store High QC not voting. Error: {:?}", e);
+                        return;
+                    }
+                }
+
                 let mut consensus = RwLockUpgradableReadGuard::upgrade(consensus).await;
 
                 if justify_qc.get_view_number() > consensus.high_qc.view_number {
@@ -550,6 +563,19 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES>, A: ConsensusApi<TYPES, I> + 
                         },
                     );
                     consensus.saved_leaves.insert(leaf.commit(), leaf.clone());
+
+                    if let Err(e) = self
+                        .storage
+                        .write()
+                        .await
+                        .update_undecided_state(
+                            consensus.saved_leaves.clone(),
+                            consensus.validated_state_map.clone(),
+                        )
+                        .await
+                    {
+                        warn!("Couldn't store undecided state.  Error: {:?}", e);
+                    }
 
                     // If we are missing the parent from storage, the safety check will fail.  But we can
                     // still vote if the liveness check succeeds.
@@ -782,6 +808,20 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES>, A: ConsensusApi<TYPES, I> + 
                     },
                 );
                 consensus.saved_leaves.insert(leaf.commit(), leaf.clone());
+
+                if let Err(e) = self
+                    .storage
+                    .write()
+                    .await
+                    .update_undecided_state(
+                        consensus.saved_leaves.clone(),
+                        consensus.validated_state_map.clone(),
+                    )
+                    .await
+                {
+                    warn!("Couldn't store undecided state.  Error: {:?}", e);
+                }
+
                 if new_commit_reached {
                     consensus.locked_view = new_locked_view;
                 }
@@ -969,6 +1009,10 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES>, A: ConsensusApi<TYPES, I> + 
                     }
                 }
                 if let either::Left(qc) = cert {
+                    if let Err(e) = self.storage.write().await.update_high_qc(qc.clone()).await {
+                        warn!("Failed to store High QC of QC we formed. Error: {:?}", e);
+                    }
+
                     let mut consensus = self.consensus.write().await;
                     consensus.high_qc = qc.clone();
 
