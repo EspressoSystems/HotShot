1ad151a31660e4ca768a18d34e2859bd0e6f7ba9
Rob <rob@espressosys.com>
2024-03-31 21:39:13 -0400

    broker build for async_std

--- crates/examples/push-cdn/broker.rs
+++ crates/examples/push-cdn/broker.rs
@@ -55,7 +55,8 @@ struct Args {
     key_seed: u64,
 }
 
-#[tokio::main]
+#[cfg_attr(async_executor_impl = "tokio", tokio::main(flavor = "multi_thread"))]
+#[cfg_attr(async_executor_impl = "async-std", async_std::main)]
 async fn main() -> Result<()> {
     // Parse command line arguments
     let args = Args::parse();
