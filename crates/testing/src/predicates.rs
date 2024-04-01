26c9c344cfe0f52ffdab941d2fe8cf6f076d2da2
ss-es <155648797+ss-es@users.noreply.github.com>
2024-04-01 16:10:18 -0400

    Ensure blank blocks between versions (#2765)

--- crates/testing/src/predicates.rs
+++ crates/testing/src/predicates.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
 use hotshot_task_impls::{
-    consensus::ConsensusTaskState, events::HotShotEvent, events::HotShotEvent::*,
+    consensus::{null_block, ConsensusTaskState},
+    events::HotShotEvent,
+    events::HotShotEvent::*,
 };
-use hotshot_types::traits::node_implementation::NodeType;
+use hotshot_types::traits::{block_contents::BlockHeader, node_implementation::NodeType};
 
 use hotshot::types::SystemContextHandle;
 
@@ -139,6 +141,27 @@ where
 {
     let info = "QuorumProposalSend".to_string();
     let function = |e: &Arc<HotShotEvent<TYPES>>| matches!(e.as_ref(), QuorumProposalSend(_, _));
+
+    Predicate {
+        function: Box::new(function),
+        info,
+    }
+}
+
+pub fn quorum_proposal_send_with_null_block<TYPES>(
+    num_storage_nodes: usize,
+) -> Predicate<Arc<HotShotEvent<TYPES>>>
+where
+    TYPES: NodeType,
+{
+    let info = "QuorumProposalSend with null block payload".to_string();
+    let function = move |e: &Arc<HotShotEvent<TYPES>>| match e.as_ref() {
+        QuorumProposalSend(proposal, _) => {
+            Some(proposal.data.block_header.payload_commitment())
+                == null_block::commitment(num_storage_nodes)
+        }
+        _ => false,
+    };
 
     Predicate {
         function: Box::new(function),
