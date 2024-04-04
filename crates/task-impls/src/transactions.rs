952547c4f7c4ef647b5a21ad8b2bd5c76e593b58
ss-es <155648797+ss-es@users.noreply.github.com>
2024-04-04 15:15:53 -0400

    test

--- crates/task-impls/src/transactions.rs
+++ crates/task-impls/src/transactions.rs
@@ -214,7 +214,6 @@ impl<
             if num_txns >= self.api.min_transactions() {
                 return latest_block;
             }
-            async_sleep(Duration::from_millis(10)).await;
         }
         latest_block
     }
