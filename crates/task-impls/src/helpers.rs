1acac5b70e585aaa2231dab5b15dff19d9d6b6be
Brendon Fish <bfish713@gmail.com>
2024-04-06 07:04:26 -0400

    Only calculate VID shares when Asked (#2902)
    * Only calculate VID shares when asked
    
    * fix comment
    
    * change back type

--- crates/task-impls/src/helpers.rs
+++ crates/task-impls/src/helpers.rs
@@ -1,8 +1,16 @@
+use std::sync::Arc;
+
 use async_broadcast::{SendError, Sender};
 #[cfg(async_executor_impl = "async-std")]
-use async_std::task::JoinHandle;
+use async_std::task::{spawn_blocking, JoinHandle};
+use hotshot_types::{
+    data::VidDisperse,
+    traits::{election::Membership, node_implementation::NodeType},
+    vid::vid_scheme,
+};
+use jf_primitives::vid::VidScheme;
 #[cfg(async_executor_impl = "tokio")]
-use tokio::task::JoinHandle;
+use tokio::task::{spawn_blocking, JoinHandle};
 
 /// Cancel a task
 pub async fn cancel_task<T>(task: JoinHandle<T>) {
@@ -29,4 +37,25 @@ pub async fn broadcast_event<E: Clone + std::fmt::Debug>(event: E, sender: &Send
             );
         }
     }
+}
+/// Calculate the vid disperse information from the payload given a view and membership
+///
+/// # Panics
+/// Panics if the VID calculation fails, this should not happen.
+pub async fn calculate_vid_disperse<TYPES: NodeType>(
+    txns: Vec<u8>,
+    membership: Arc<TYPES::Membership>,
+    view: TYPES::Time,
+) -> VidDisperse<TYPES> {
+    let num_nodes = membership.total_nodes();
+    let vid_disperse = spawn_blocking(move || {
+        #[allow(clippy::panic)]
+        vid_scheme(num_nodes).disperse(&txns).unwrap_or_else(|err|panic!("VID disperse failure:\n\t(num_storage nodes,payload_byte_len)=({num_nodes},{})\n\terror: : {err}", txns.len()))
+    })
+    .await;
+    #[cfg(async_executor_impl = "tokio")]
+    // Unwrap here will just propagate any panic from the spawned task, it's not a new place we can panic.
+    let vid_disperse = vid_disperse.unwrap();
+
+    VidDisperse::from_membership(view, vid_disperse, membership)
 }
