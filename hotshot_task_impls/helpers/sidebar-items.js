4b8d765da1181f0b65eb18ed8bd5f0b9e7436d01
ss-es <ss-es@users.noreply.github.com>
2024-04-09 06:27:57 +0000

    deploy: 642e6c164329d0f5dd76033c4e16297e369ceab7

--- hotshot_task_impls/helpers/sidebar-items.js
+++ hotshot_task_impls/helpers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["broadcast_event","calculate_vid_disperse","cancel_task"]};
+window.SIDEBAR_ITEMS = {"fn":["broadcast_event","calculate_vid_disperse","cancel_task"],"trait":["AnyhowTracing"]};
