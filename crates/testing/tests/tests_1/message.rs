7a670fe94b1dab3e2c23c34f6089bedc7f50c534
ss-es <155648797+ss-es@users.noreply.github.com>
2024-04-15 11:05:37 -0400

    undo revert

--- crates/testing/tests/tests_1/message.rs
+++ crates/testing/tests/tests_1/message.rs
@@ -42,7 +42,6 @@ fn version_number_at_start_of_serialization() {
         vote_commitment: data.commit(),
         view_number,
         signatures: None,
-        is_genesis: false,
         _pd: PhantomData,
     };
     let message = Message {
