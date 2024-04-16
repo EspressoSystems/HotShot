59c01f1d7add916d1b55ac96a60f8c6e90eacb33
QuentinI <QuentinI@users.noreply.github.com>
2024-04-16 15:48:38 +0000

    deploy: 6f7120a16d76f1f1d1172724d58ad431456048c7

---
+++ trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hotshot_orchestrator":[["impl ValueEnum for <a class=\"enum\" href=\"hotshot_orchestrator/config/enum.BuilderType.html\" title=\"enum hotshot_orchestrator::config::BuilderType\">BuilderType</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
