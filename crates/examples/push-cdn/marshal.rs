4f9c275421936c65c6943a305406aa4280f35a1c
rob-maron <132852777+rob-maron@users.noreply.github.com>
2024-04-22 10:41:52 -0400

    Update the Push CDN to 0.2.1 (#2997)
    * update the push CDN to 0.2.0
    
    * lint
    
    * short param for key seed
    
    * fix argument name
    
    * update to 0.2.1

--- crates/examples/push-cdn/marshal.rs
+++ crates/examples/push-cdn/marshal.rs
@@ -1,38 +1,40 @@
 //! The following is the main `Marshal` binary, which just instantiates and runs
 //! a `Marshal` object.
 
-use anyhow::{Context, Result};
-use cdn_marshal::{ConfigBuilder, Marshal};
+use anyhow::Result;
+use cdn_marshal::{Config, Marshal};
 use clap::Parser;
 use hotshot::traits::implementations::ProductionDef;
 use hotshot_example_types::node_types::TestTypes;
+
+// TODO: forall, add logging where we need it
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 /// The main component of the push CDN.
 struct Args {
     /// The discovery client endpoint (including scheme) to connect to
-    /// With the local discovery feature, this is a file path.
-    /// With the remote (redis) discovery feature, this is a redis URL (e.g. `redis://127.0.0.1:6789`).
-
     #[arg(short, long)]
     discovery_endpoint: String,
-
-    /// Whether or not metric collection and serving is enabled
-    #[arg(long, default_value_t = false)]
-    metrics_enabled: bool,
-
-    /// The IP to bind to for externalizing metrics
-    #[arg(long, default_value = "127.0.0.1")]
-    metrics_ip: String,
-
-    /// The port to bind to for externalizing metrics
-    #[arg(long, default_value_t = 9090)]
-    metrics_port: u16,
 
     /// The port to bind to for connections (from users)
     #[arg(short, long, default_value_t = 1737)]
     bind_port: u16,
+
+    /// The endpoint to bind to for externalizing metrics (in `IP:port` form). If not provided,
+    /// metrics are not exposed.
+    #[arg(short, long)]
+    metrics_bind_endpoint: Option<String>,
+
+    /// The path to the CA certificate
+    /// If not provided, a local, pinned CA is used
+    #[arg(long)]
+    ca_cert_path: Option<String>,
+
+    /// The path to the CA key
+    /// If not provided, a local, pinned CA is used
+    #[arg(long)]
+    ca_key_path: Option<String>,
 }
 
 #[cfg_attr(async_executor_impl = "tokio", tokio::main)]
@@ -42,17 +44,20 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     // Initialize tracing
-    tracing_subscriber::fmt::init();
+    if std::env::var("RUST_LOG_FORMAT") == Ok("json".to_string()) {
+        tracing_subscriber::fmt().json().init();
+    } else {
+        tracing_subscriber::fmt().init();
+    }
 
     // Create a new `Config`
-    let config = ConfigBuilder::default()
-        .bind_address(format!("0.0.0.0:{}", args.bind_port))
-        .metrics_enabled(args.metrics_enabled)
-        .metrics_ip(args.metrics_ip)
-        .metrics_port(args.metrics_port)
-        .discovery_endpoint(args.discovery_endpoint)
-        .build()
-        .with_context(|| "failed to build Marshal config")?;
+    let config = Config {
+        discovery_endpoint: args.discovery_endpoint,
+        bind_endpoint: format!("0.0.0.0:{}", args.bind_port),
+        metrics_bind_endpoint: args.metrics_bind_endpoint,
+        ca_cert_path: args.ca_cert_path,
+        ca_key_path: args.ca_key_path,
+    };
 
     // Create new `Marshal` from the config
     let marshal = Marshal::<ProductionDef<TestTypes>>::new(config).await?;
