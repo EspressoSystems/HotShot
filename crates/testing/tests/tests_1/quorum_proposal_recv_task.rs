4090725ddb348ac31f381575b757aa14f8e6001a
Jarred Parr <jparr721@gmail.com>
2024-04-26 15:02:22 -0400

    fix compiler error

--- crates/testing/tests/tests_1/quorum_proposal_recv_task.rs
+++ crates/testing/tests/tests_1/quorum_proposal_recv_task.rs
@@ -6,6 +6,7 @@ use hotshot_task_impls::{
 #[cfg_attr(async_executor_impl = "tokio", tokio::test(flavor = "multi_thread"))]
 #[cfg_attr(async_executor_impl = "async-std", async_std::test)]
 async fn test_quorum_proposal_recv_task() {
+    use hotshot::tasks::task_state::CreateTaskState;
     use hotshot_example_types::node_types::{MemoryImpl, TestTypes};
     use hotshot_testing::task_helpers::build_system_handle;
 
@@ -13,5 +14,5 @@ async fn test_quorum_proposal_recv_task() {
     async_compatibility_layer::logging::setup_backtrace();
 
     let handle = build_system_handle(2).await.0;
-    let state = QuorumProposalRecvTaskState::<TestTypes, MemoryImpl>;
+    let state = QuorumProposalRecvTaskState::<TestTypes, MemoryImpl>::create_from(&handle);
 }
