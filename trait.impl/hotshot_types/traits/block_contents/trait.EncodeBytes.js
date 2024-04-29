14155e485434ec28606236bd2ca49567dbf80bac
ss-es <ss-es@users.noreply.github.com>
2024-04-29 22:50:44 +0000

    deploy: c6e67ce2863a994ee361bf12542c1872e09e36cc

--- trait.impl/hotshot_types/traits/block_contents/trait.EncodeBytes.js
+++ trait.impl/hotshot_types/traits/block_contents/trait.EncodeBytes.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hotshot_example_types":[["impl EncodeBytes for <a class=\"struct\" href=\"hotshot_example_types/block_types/struct.TestMetadata.html\" title=\"struct hotshot_example_types::block_types::TestMetadata\">TestMetadata</a>"]]
+"hotshot_example_types":[["impl <a class=\"trait\" href=\"hotshot_types/traits/block_contents/trait.EncodeBytes.html\" title=\"trait hotshot_types::traits::block_contents::EncodeBytes\">EncodeBytes</a> for <a class=\"struct\" href=\"hotshot_example_types/block_types/struct.TestMetadata.html\" title=\"struct hotshot_example_types::block_types::TestMetadata\">TestMetadata</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
