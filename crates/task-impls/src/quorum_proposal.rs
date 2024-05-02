1a42ae0ad14d1b4674014a1b1bf3bddab4a7632b
Keyao Shen <shenkeyao@gmail.com>
2024-05-02 17:27:33 +0800

    Move vote_if_able to helper

--- crates/task-impls/src/quorum_proposal.rs
+++ crates/task-impls/src/quorum_proposal.rs
@@ -32,7 +32,7 @@ use tokio::task::JoinHandle;
 use tracing::{debug, error, info, instrument, warn};
 
 #[cfg(feature = "dependency-tasks")]
-use crate::consensus::proposal_helpers::handle_quorum_proposal_validated;
+use crate::consensus::helpers::handle_quorum_proposal_validated;
 use crate::{
     events::HotShotEvent,
     helpers::{broadcast_event, cancel_task},
