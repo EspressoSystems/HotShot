939348092c9042b43eeccfef9c017a54ea77770f
rob-maron <rob-maron@users.noreply.github.com>
2024-05-07 14:34:18 +0000

    deploy: a13a62c8da7407633adb584083f60ebe9bbc8046

--- trait.impl/orchestrator/infra/trait.RunDA.js
+++ trait.impl/orchestrator/infra/trait.RunDA.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"orchestrator_libp2p":[]
+"orchestrator":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
