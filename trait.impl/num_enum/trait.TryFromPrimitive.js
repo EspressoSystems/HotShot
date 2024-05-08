4400ede3e9ee9e9374121fa305c0c004480c0884
rob-maron <rob-maron@users.noreply.github.com>
2024-05-08 13:40:47 +0000

    deploy: dc841a742f077778598b06db9b64d4f3954a51c2

---
+++ trait.impl/num_enum/trait.TryFromPrimitive.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hotshot":[["impl TryFromPrimitive for <a class=\"enum\" href=\"hotshot/traits/networking/push_cdn_network/enum.Topic.html\" title=\"enum hotshot::traits::networking::push_cdn_network::Topic\">Topic</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
