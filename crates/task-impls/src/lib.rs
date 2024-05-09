d9e0a6bb31d321a943435bf8221451a5fe4ad63c
Jarred Parr <jparr721@gmail.com>
2024-05-09 14:27:13 -0600

    init consensus 2

--- crates/task-impls/src/lib.rs
+++ crates/task-impls/src/lib.rs
@@ -5,6 +5,7 @@
 pub mod consensus;
 
 /// The task which implements the core state logic of consensus.
+#[cfg(feature = "dependency-tasks")]
 pub mod consensus2;
 
 /// The task which handles the logic for the quorum vote.
