f416ac4a302fe87248dbdd437492ea02a9edf2bd
dailinsubjam <dlsubjam@gmail.com>
2024-05-10 07:15:59 +0000

    adding access to remote server in script

--- scripts/aws_ecs_benchmarks_cdn.sh
+++ scripts/aws_ecs_benchmarks_cdn.sh
@@ -34,54 +34,67 @@ just async_std example cdn-broker -- -d redis://localhost:6379 \
     --private-bind-endpoint 0.0.0.0:1741 \
     --private-advertise-endpoint local_ip:1741 &
 # server2: broker
-# just async_std example cdn-broker -- -d ${keydb_address} \
-#     --public-bind-endpoint 0.0.0.0:1740 \
-#     --public-advertise-endpoint local_ip:1740 \
-#     --private-bind-endpoint 0.0.0.0:1741 \
-#     --private-advertise-endpoint local_ip:1741 &
+# make sure you're able to access the remote host from current host
+REMOTE_USER="sishan"
+REMOTE_HOST="3.135.239.251"
+COMMAND="just async_std example cdn-broker -- -d ${keydb_address} \
+    --public-bind-endpoint 0.0.0.0:1740 \
+    --public-advertise-endpoint local_ip:1740 \
+    --private-bind-endpoint 0.0.0.0:1741 \
+    --private-advertise-endpoint local_ip:1741 &"
+ssh $REMOTE_USER@$REMOTE_HOST "$COMMAND; exit"
+
+# Function to round up to the nearest integer
+round_up() {
+  echo "scale=0;($1+0.5)/1" | bc
+}
 
 # for a single run
 # total_nodes, da_committee_size, transactions_per_round, transaction_size = 100, 10, 1, 4096
 # for iteration of assignment
 # see `aws_ecs_nginx_benchmarks.sh` for an example
-for total_nodes in 10 # 50 100
+for total_nodes in 10 50 100
 do
-    for da_committee_size in 5 #10 50 100
+    for da_committee_size in 5 10 50 100
     do
         if [ $da_committee_size -le $total_nodes ]
         then
-            for transactions_per_round in 1 # 10 50 100
+            for transactions_per_round in 1 10 50 100
             do
-                for transaction_size in 1000000 # 512 4096 
+                for transaction_size in 512 4096 1000000
                 do
-                    for fixed_leader_for_gpuvid in 1 # 5 10
+                    for fixed_leader_for_gpuvid in 1 5 10
                     do
                         if [ $fixed_leader_for_gpuvid -le $da_committee_size ]
                         then
-                            rounds=100
+                            for rounds in 50 100
+                            do
+                                # start orchestrator
+                                just async_std example_fixed_leader orchestrator -- --config_file ./crates/orchestrator/run-config.toml \
+                                                                                --orchestrator_url http://0.0.0.0:4444 \
+                                                                                --total_nodes ${total_nodes} \
+                                                                                --da_committee_size ${da_committee_size} \
+                                                                                --transactions_per_round ${transactions_per_round} \
+                                                                                --transaction_size ${transaction_size} \
+                                                                                --rounds ${rounds} \
+                                                                                --fixed_leader_for_gpuvid ${fixed_leader_for_gpuvid} \
+                                                                                --cdn_marshal_address ${cdn_marshal_address} \
+                                                                                --commit_sha cdn_no_gpu &
+                                sleep 30
 
-                            # start orchestrator
-                            just async_std example_fixed_leader orchestrator -- --config_file ./crates/orchestrator/run-config.toml \
-                                                                            --orchestrator_url http://0.0.0.0:4444 \
-                                                                            --total_nodes ${total_nodes} \
-                                                                            --da_committee_size ${da_committee_size} \
-                                                                            --transactions_per_round ${transactions_per_round} \
-                                                                            --transaction_size ${transaction_size} \
-                                                                            --rounds ${rounds} \
-                                                                            --fixed_leader_for_gpuvid ${fixed_leader_for_gpuvid} \
-                                                                            --cdn_marshal_address ${cdn_marshal_address} \
-                                                                            --commit_sha cdn_no_gpu &
-                            sleep 30
+                                # start validators
+                                ecs scale --region us-east-2 hotshot hotshot_centralized ${total_nodes} --timeout -1
+                                base=100
+                                mul=$(echo "l($transaction_size)/l($base)" | bc -l)
+                                mul=$(round_up $mul)
+                                sleep $(( ($rounds + $total_nodes) * $transactions_per_round * $mul ))
 
-                            # start validators
-                            ecs scale --region us-east-2 hotshot hotshot_centralized ${total_nodes} --timeout -1
-                            sleep $(( ($rounds + $total_nodes) * $transactions_per_round * 5 ))
-
-                            # kill them
-                            ecs scale --region us-east-2 hotshot hotshot_centralized 0 --timeout -1
-                            sleep 1m
-                            for pid in $(ps -ef | grep "orchestrator" | awk '{print $2}'); do kill -9 $pid; done
-                            sleep 10
+                                # kill them
+                                ecs scale --region us-east-2 hotshot hotshot_centralized 0 --timeout -1
+                                sleep $(( $total_nodes / 2 ))
+                                for pid in $(ps -ef | grep "orchestrator" | awk '{print $2}'); do kill -9 $pid; done
+                                sleep 10
+                            done
                         fi
                     done
                 done
@@ -91,6 +104,8 @@ do
 done
 
 # shut down all related threads
+COMMAND_SHUTDOWN="for pid in $(ps -ef | grep "cdn-broker" | awk '{print $2}'); do kill -9 $pid; done"
 for pid in $(ps -ef | grep "cdn-broker" | awk '{print $2}'); do kill -9 $pid; done
+ssh $REMOTE_USER@$REMOTE_HOST "$COMMAND_SHUTDOWN; exit"
 for pid in $(ps -ef | grep "cdn-marshal" | awk '{print $2}'); do kill -9 $pid; done
 # for pid in $(ps -ef | grep "keydb-server" | awk '{print $2}'); do sudo kill -9 $pid; done
