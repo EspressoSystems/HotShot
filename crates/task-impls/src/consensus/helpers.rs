e3daca7aa80adae25806b3640dcad9a875643868
Jarred Parr <jparr721@gmail.com>
2024-05-14 11:49:36 -0600

    remove error logs, actually propose correctly for view 1

--- crates/task-impls/src/consensus/helpers.rs
+++ crates/task-impls/src/consensus/helpers.rs
@@ -311,20 +311,20 @@ pub fn validate_proposal_view_and_certs<TYPES: NodeType>(
 /// Gets the parent leaf and state from the parent of a proposal, returning an [`anyhow::Error`] if not.
 pub(crate) async fn get_parent_leaf_and_state<TYPES: NodeType>(
     cur_view: TYPES::Time,
-    view: TYPES::Time,
+    view_number: TYPES::Time,
     quorum_membership: Arc<TYPES::Membership>,
     public_key: TYPES::SignatureKey,
     consensus: Arc<RwLock<Consensus<TYPES>>>,
 ) -> Result<(Leaf<TYPES>, Arc<<TYPES as NodeType>::ValidatedState>)> {
     ensure!(
-        quorum_membership.get_leader(view) == public_key,
-        "Somehow we formed a QC but are not the leader for the next view {view:?}",
+        quorum_membership.get_leader(view_number) == public_key,
+        "Somehow we formed a QC but are not the leader for the next view {view_number:?}",
     );
 
-    let consensus = consensus.read().await;
-    let parent_view_number = &consensus.high_qc().get_view_number();
-    let parent_view = consensus.validated_state_map().get(parent_view_number).context(
-        format!("Couldn't find parent view in state map, waiting for replica to see proposal; parent_view_number: {}", **parent_view_number)
+    let consensus_reader = consensus.read().await;
+    let parent_view_number = consensus_reader.high_qc().get_view_number();
+    let parent_view = consensus_reader.validated_state_map().get(&parent_view_number).context(
+        format!("Couldn't find parent view in state map, waiting for replica to see proposal; parent_view_number: {}", *parent_view_number)
     )?;
 
     // Leaf hash in view inner does not match high qc hash - Why?
@@ -332,21 +332,12 @@ pub(crate) async fn get_parent_leaf_and_state<TYPES: NodeType>(
         format!("Parent of high QC points to a view without a proposal; parent_view_number: {parent_view_number:?}, parent_view {parent_view:?}")
     )?;
 
-    if leaf_commitment != consensus.high_qc().get_data().leaf_commit {
-        // NOTE: This happens on the genesis block
-        debug!(
-            "They don't equal: {:?}   {:?}",
-            leaf_commitment,
-            consensus.high_qc().get_data().leaf_commit
-        );
-    }
-
-    let leaf = consensus
+    let leaf = consensus_reader
         .saved_leaves()
         .get(&leaf_commitment)
         .context("Failed to find high QC of parent")?;
 
-    let reached_decided = leaf.get_view_number() == consensus.last_decided_view();
+    let reached_decided = leaf.get_view_number() == consensus_reader.last_decided_view();
     let parent_leaf = leaf.clone();
     let original_parent_hash = parent_leaf.commit();
     let mut next_parent_hash = original_parent_hash;
@@ -354,8 +345,8 @@ pub(crate) async fn get_parent_leaf_and_state<TYPES: NodeType>(
     // Walk back until we find a decide
     if !reached_decided {
         debug!("We have not reached decide from view {:?}", cur_view);
-        while let Some(next_parent_leaf) = consensus.saved_leaves().get(&next_parent_hash) {
-            if next_parent_leaf.get_view_number() <= consensus.last_decided_view() {
+        while let Some(next_parent_leaf) = consensus_reader.saved_leaves().get(&next_parent_hash) {
+            if next_parent_leaf.get_view_number() <= consensus_reader.last_decided_view() {
                 break;
             }
             next_parent_hash = next_parent_leaf.get_parent_commitment();
