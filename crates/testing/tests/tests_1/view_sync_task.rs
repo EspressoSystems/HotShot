023376c52e2bda2ec84ca36119049e426daf035d
Jarred Parr <jparr721@gmail.com>
2024-05-27 10:08:24 -0600

    revert name

--- crates/testing/tests/tests_1/view_sync_task.rs
+++ crates/testing/tests/tests_1/view_sync_task.rs
@@ -12,7 +12,7 @@ use hotshot_types::{
 #[cfg(test)]
 #[cfg_attr(async_executor_impl = "tokio", tokio::test(flavor = "multi_thread"))]
 #[cfg_attr(async_executor_impl = "async-std", async_std::test)]
-async fn test_view_sync_task_old() {
+async fn test_view_sync_task() {
     async_compatibility_layer::logging::setup_logging();
     async_compatibility_layer::logging::setup_backtrace();
 
