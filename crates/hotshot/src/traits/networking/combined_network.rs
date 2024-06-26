50498457315f79da566edea03e9cc59b2abf88c6
Rob <rob@espressosys.com>
2024-06-26 13:59:54 -0400

    remove coimbined fail counter increment

--- crates/hotshot/src/traits/networking/combined_network.rs
+++ crates/hotshot/src/traits/networking/combined_network.rs
@@ -199,7 +199,6 @@ impl<TYPES: NodeType> CombinedNetworks<TYPES> {
                 // The task hasn't been cancelled, the primary probably failed.
                 // Increment the primary fail counter and send the message.
                 debug!("Sending on secondary after delay, message possibly has not reached recipient on primary");
-                primary_fail_counter.fetch_add(1, Ordering::Relaxed);
                 secondary_future.await
             });
             Ok(())
