664ec2efd7c472d0bae77c862e3232df35878329
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2024-07-18 09:31:25 -0400

    Minor change to AuctionResults trait to allow multiple URLs

--- crates/types/src/traits/auction_results_provider.rs
+++ crates/types/src/traits/auction_results_provider.rs
@@ -1,18 +1,19 @@
 //! This module defines the interaction layer with the Solver via the [`AuctionResultsProvider`] trait,
 //! which handles connecting to, and fetching the allocation results from, the Solver.
 
+use std::collections::HashSet;
 use anyhow::Result;
 use async_trait::async_trait;
 use url::Url;
 
 use super::node_implementation::NodeType;
 
-/// This trait guarantees that a particular type has a url associated with it. This trait
-/// essentially ensures that the results returned by the [`AuctionResultsProvider`] trait includes a URL
-/// for the builder that HotShot must request from.
-pub trait HasUrl {
+/// This trait guarantees that a particular type has urls that can be extracted from it. This trait
+/// essentially ensures that the results returned by the [`AuctionResultsProvider`] trait includes a
+/// list of urls for the builders that HotShot must request from.
+pub trait HasUrls {
     /// Returns the builer url associated with the datatype
-    fn url(&self) -> Url;
+    fn urls(&self) -> HashSet<Url>;
 }
 
 /// The AuctionResultsProvider trait is the sole source of Solver-originated state and interaction,
@@ -23,12 +24,12 @@ pub trait HasUrl {
 pub trait AuctionResultsProvider<TYPES: NodeType>: Send + Sync + Clone {
     /// The AuctionSolverResult is a type that holds the data associated with a particular solver
     /// run, for a particular view.
-    type AuctionResult: HasUrl;
+    type AuctionResult: HasUrls;
 
     /// Fetches the auction result for a view. Does not cache the result,
     /// subsequent calls will invoke additional wasted calls.
     async fn fetch_auction_result(
         &self,
         view_number: TYPES::Time,
-    ) -> Result<Vec<Self::AuctionResult>>;
+    ) -> Result<Self::AuctionResult>;
 }
