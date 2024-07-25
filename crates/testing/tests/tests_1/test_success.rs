11bec9da06b38b4cb358eb7b55fe5efb613eaf82
ss-es <155648797+ss-es@users.noreply.github.com>
2024-07-25 11:40:29 -0400

    add byzantine test for sending multiple bad proposals

--- crates/testing/tests/tests_1/test_success.rs
+++ crates/testing/tests/tests_1/test_success.rs
@@ -11,7 +11,7 @@ use hotshot_testing::{
     test_builder::TestDescription,
 };
 #[cfg(async_executor_impl = "async-std")]
-use {hotshot::tasks::DoubleProposeVote, hotshot_testing::test_builder::Behaviour, std::rc::Rc};
+use {hotshot::tasks::{DoubleProposeVote, BadProposalViewDos}, hotshot_testing::test_builder::Behaviour, std::rc::Rc};
 
 cross_tests!(
     TestName: test_success,
@@ -33,13 +33,38 @@ cross_tests!(
 
 #[cfg(async_executor_impl = "async-std")]
 cross_tests!(
-    TestName: twins_test_success,
+    TestName: double_propose_vote,
     Impls: [MemoryImpl],
     Types: [TestTypes],
     Ignore: false,
     Metadata: {
         let behaviour = Rc::new(|node_id| { match node_id {
           1 => Behaviour::Byzantine(Box::new(DoubleProposeVote)),
+          _ => Behaviour::Standard,
+          } });
+
+        TestDescription {
+            // allow more time to pass in CI
+            completion_task_description: CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
+                                             TimeBasedCompletionTaskDescription {
+                                                 duration: Duration::from_secs(60),
+                                             },
+                                         ),
+            behaviour,
+            ..TestDescription::default()
+        }
+    },
+);
+
+#[cfg(async_executor_impl = "async-std")]
+cross_tests!(
+    TestName: multiple_bad_proposals,
+    Impls: [MemoryImpl],
+    Types: [TestTypes],
+    Ignore: false,
+    Metadata: {
+        let behaviour = Rc::new(|node_id| { match node_id {
+          4 => Behaviour::Byzantine(Box::new(BadProposalViewDos { multiplier: 100, increment: 1 })),
           _ => Behaviour::Standard,
           } });
 
