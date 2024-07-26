1383d7a35e00624ec40d98dba7b0603c420b2d5b
Lukasz Rzasik <lukasz.rzasik@gmail.com>
2024-07-26 15:42:55 +0200

    Remove byzantine code for now

--- crates/task-impls/src/quorum_proposal_recv/mod.rs
+++ crates/task-impls/src/quorum_proposal_recv/mod.rs
@@ -124,24 +124,24 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES>> QuorumProposalRecvTaskState<
                 proposal.data.view_number()
             );
 
-            tracing::warn!(
-                "lrzasik: check if send bad proposals, id: {:?}, latest proposed view number: {:?}",
-                self.id,
-                *proposal.data.view_number()
-            );
-            if self.id == 2 && *proposal.data.view_number() > 100 {
-                tracing::warn!("lrzasik: sending bad proposals");
-                let mut bad_proposal = proposal.clone();
-                for i in 0..5 {
-                    bad_proposal.data.view_number += 1;
-                    broadcast_event(
-                        HotShotEvent::QuorumProposalSend(bad_proposal.clone(), sender.clone())
-                            .into(),
-                        &event_stream,
-                    )
-                    .await;
-                }
-            }
+            // tracing::warn!(
+            //     "lrzasik: check if send bad proposals, id: {:?}, latest proposed view number: {:?}",
+            //     self.id,
+            //     *proposal.data.view_number()
+            // );
+            // if self.id == 2 && *proposal.data.view_number() > 100 {
+            //     tracing::warn!("lrzasik: sending bad proposals");
+            //     let mut bad_proposal = proposal.clone();
+            //     for i in 0..5 {
+            //         bad_proposal.data.view_number += 1;
+            //         broadcast_event(
+            //             HotShotEvent::QuorumProposalSend(bad_proposal.clone(), sender.clone())
+            //                 .into(),
+            //             &event_stream,
+            //         )
+            //         .await;
+            //     }
+            // }
 
             match handle_quorum_proposal_recv(proposal, sender, &event_stream, self).await {
                 Ok(QuorumProposalValidity::Fully) => {
