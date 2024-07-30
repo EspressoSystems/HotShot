51ffac3e416456468ba8c1890a70e60d5adb8d31
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2024-07-30 11:15:38 -0400

    change docker image

--- scripts/benchmark_scripts/aws_ecs_benchmarks_cdn.sh
+++ scripts/benchmark_scripts/aws_ecs_benchmarks_cdn.sh
@@ -30,11 +30,11 @@ ulimit -n 65536
 # for pid in $(ps -ef | grep "validator" | awk '{print $2}'); do kill -9 $pid; done
 
 # docker build and push
-docker build . -f ./docker/validator-cdn-local.Dockerfile -t ghcr.io/espressosystems/hotshot/bench:ed
-docker push ghcr.io/espressosystems/hotshot/bench:ed
+docker build . -f ./docker/validator-cdn-local.Dockerfile -t ghcr.io/espressosystems/hotshot/validator-push-cdn:ed
+docker push ghcr.io/espressosystems/hotshot/validator-push-cdn:ed
 
 # ecs deploy
-ecs deploy --region us-east-2 hotshot hotshot_libp2p -i libp2p ghcr.io/espressosystems/hotshot/bench:ed
+ecs deploy --region us-east-2 hotshot hotshot_libp2p -i libp2p ghcr.io/espressosystems/hotshot/validator-push-cdn:ed
 ecs deploy --region us-east-2 hotshot hotshot_libp2p -c libp2p ${orchestrator_url}
 
 # runstart keydb
