4b6d333dd77413a71ec761533c3b148aa3974d90
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2024-08-01 21:04:02 -0400

    s

--- crates/orchestrator/src/lib.rs
+++ crates/orchestrator/src/lib.rs
@@ -480,9 +480,11 @@ where
 
     // Aggregates results of the run from all nodes
     fn post_run_results(&mut self, metrics: BenchResults) -> Result<(), ServerError> {
+
         self.nodes_post_results += 1;
 
         if metrics.total_transactions_committed != 0 {
+
             // Deal with the bench results
             if self.bench_results.total_transactions_committed == 0 {
                 self.bench_results = metrics;
