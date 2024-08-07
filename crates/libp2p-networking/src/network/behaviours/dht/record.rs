a5e59f308e43e6b2ea386c637797a0ab432ea47e
Rob <rob@espressosys.com>
2024-08-07 16:02:34 -0400

    add some more tests

--- crates/libp2p-networking/src/network/behaviours/dht/record.rs
+++ crates/libp2p-networking/src/network/behaviours/dht/record.rs
@@ -18,10 +18,14 @@ pub enum RecordValue<K: SignatureKey + 'static> {
 /// The namespace of a record. This is included with the key
 /// and allows for multiple types of records to be stored in the DHT.
 #[repr(u8)]
-#[derive(Serialize, Deserialize, Clone, Copy)]
+#[derive(Serialize, Deserialize, Clone, Copy, Eq, PartialEq)]
 pub enum Namespace {
     /// A namespace for looking up P2P identities
     Lookup = 0,
+
+    #[cfg(test)]
+    /// A secondary record type for testing purposes
+    Testing = u8::MAX,
 }
 
 /// Allow fallible conversion from a byte to a namespace
@@ -31,6 +35,8 @@ impl TryFrom<u8> for Namespace {
     fn try_from(value: u8) -> Result<Self> {
         match value {
             0 => Ok(Self::Lookup),
+            #[cfg(test)]
+            1 => Ok(Self::Testing),
             _ => bail!("Unknown namespace"),
         }
     }
@@ -151,5 +157,140 @@ impl<K: SignatureKey + 'static> TryFrom<Record> for RecordValue<K> {
 
         // Return the record
         Ok(record)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use hotshot_types::signature_key::BLSPubKey;
+
+    use super::*;
+
+    /// Test that namespace serialization and deserialization is consistent
+    #[test]
+    fn test_namespace_serialization_parity() {
+        // Serialize the namespace
+        let namespace = Namespace::Lookup;
+        let bytes = namespace as u8;
+
+        // Deserialize the namespace
+        let namespace = Namespace::try_from(bytes).expect("Failed to deserialize namespace");
+        assert!(namespace == Namespace::Lookup, "Wrong namespace");
+    }
+
+    /// Test that record key serialization and deserialization is consistent
+    #[test]
+    fn test_record_key_serialization_parity() {
+        // Create a new record key
+        let namespace = Namespace::Lookup;
+        let key = vec![1, 2, 3, 4];
+        let record_key = RecordKey::new(namespace, key.clone());
+
+        // Serialize it
+        let bytes = record_key.to_bytes();
+
+        // Deserialize it
+        let record_key =
+            RecordKey::try_from_bytes(&bytes).expect("Failed to deserialize record key");
+
+        // Make sure the deserialized record key is the same as the original
+        assert!(record_key.namespace == namespace, "Namespace mismatch");
+        assert!(record_key.key == key, "Key mismatch");
+    }
+
+    /// Test that the validity of a valid, signed record is correct
+    #[test]
+    fn test_valid_signature() {
+        // Generate a staking keypair
+        let (public_key, private_key) = BLSPubKey::generated_from_seed_indexed([1; 32], 1337);
+
+        // Create a value. The key is the public key
+        let value = vec![5, 6, 7, 8];
+
+        // Create a record key (as we need to sign both the key and the value)
+        let record_key = RecordKey::new(Namespace::Lookup, public_key.to_bytes());
+
+        // Sign the record and value with the private key
+        let record_value: RecordValue<BLSPubKey> =
+            RecordValue::new_signed(&record_key, value.clone(), &private_key).unwrap();
+
+        // Validate the signed record
+        assert!(
+            record_value.validate(&record_key),
+            "Failed to validate signed record"
+        );
+    }
+
+    /// Test that altering the namespace byte causes a validation failure
+    #[test]
+    fn test_invalid_namespace() {
+        // Generate a staking keypair
+        let (public_key, private_key) = BLSPubKey::generated_from_seed_indexed([1; 32], 1337);
+
+        // Create a value. The key is the public key
+        let value = vec![5, 6, 7, 8];
+
+        // Create a record key (as we need to sign both the key and the value)
+        let mut record_key = RecordKey::new(Namespace::Lookup, public_key.to_bytes());
+
+        // Sign the record and value with the private key
+        let record_value: RecordValue<BLSPubKey> =
+            RecordValue::new_signed(&record_key, value.clone(), &private_key).unwrap();
+
+        // Alter the namespace
+        record_key.namespace = Namespace::Testing;
+
+        // Validate the signed record
+        assert!(
+            !record_value.validate(&record_key),
+            "Failed to detect invalid namespace"
+        );
+    }
+
+    /// Test that altering the contents of the record key causes a validation failure
+    #[test]
+    fn test_invalid_key() {
+        // Generate a staking keypair
+        let (public_key, private_key) = BLSPubKey::generated_from_seed_indexed([1; 32], 1337);
+
+        // Create a value. The key is the public key
+        let value = vec![5, 6, 7, 8];
+
+        // Create a record key (as we need to sign both the key and the value)
+        let mut record_key = RecordKey::new(Namespace::Lookup, public_key.to_bytes());
+
+        // Sign the record and value with the private key
+        let record_value: RecordValue<BLSPubKey> =
+            RecordValue::new_signed(&record_key, value.clone(), &private_key).unwrap();
+
+        // Set the key to a different one
+        record_key.key = BLSPubKey::generated_from_seed_indexed([1; 32], 1338)
+            .0
+            .to_bytes();
+
+        // Validate the signed record
+        assert!(
+            !record_value.validate(&record_key),
+            "Failed to detect invalid record key"
+        );
+    }
+
+    /// Test that unsigned records are always valid
+    #[test]
+    fn test_unsigned_record_is_valid() {
+        // Create a value
+        let value = vec![5, 6, 7, 8];
+
+        // Create a record key
+        let record_key = RecordKey::new(Namespace::Lookup, vec![1, 2, 3, 4]);
+
+        // Create an unsigned record
+        let record_value: RecordValue<BLSPubKey> = RecordValue::new(value.clone());
+
+        // Validate the unsigned record
+        assert!(
+            record_value.validate(&record_key),
+            "Failed to validate unsigned record"
+        );
     }
 }
