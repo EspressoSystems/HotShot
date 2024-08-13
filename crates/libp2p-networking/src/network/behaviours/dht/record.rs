82bc9bf08276c2b961930f1593f041b0da9fa1e6
Rob <rob@espressosys.com>
2024-08-12 21:27:37 -0400

    Revert "more logging"
    This reverts commit 7c84ed5c80fdbfe747b17804c3095530efab49a8.

--- crates/libp2p-networking/src/network/behaviours/dht/record.rs
+++ crates/libp2p-networking/src/network/behaviours/dht/record.rs
@@ -134,12 +134,7 @@ impl<K: SignatureKey + 'static> RecordValue<K> {
             signed_value.extend_from_slice(value);
 
             // Check the entire value
-            if public_key.validate(signature, &signed_value){
-                true
-            } else {
-                error!("Failed to validate signed record");
-                false
-            }
+            public_key.validate(signature, &signed_value)
         } else {
             true
         }
