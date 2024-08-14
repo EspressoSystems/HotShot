56e3f2c663aad796c3d831c77ca47e5648a209be
rob-maron <132852777+rob-maron@users.noreply.github.com>
2024-08-14 15:09:36 -0400

    [Libp2p] DHT overhaul (#3548)

--- crates/hotshot/src/traits/networking/libp2p_network.rs
+++ crates/hotshot/src/traits/networking/libp2p_network.rs
@@ -65,6 +65,7 @@ use libp2p_identity::{
 };
 use libp2p_networking::{
     network::{
+        behaviours::dht::record::{Namespace, RecordKey, RecordValue},
         spawn_network_node,
         transport::construct_auth_message,
         MeshParams,
@@ -76,7 +77,7 @@ use libp2p_networking::{
 };
 use rand::{rngs::StdRng, seq::IteratorRandom, SeedableRng};
 use serde::Serialize;
-use tracing::{debug, error, info, instrument, warn};
+use tracing::{debug, error, info, instrument, trace, warn};
 
 use crate::BroadcastDelay;
 
@@ -255,6 +256,19 @@ impl<TYPES: NodeType> TestableNetworkingImplementation<TYPES>
                 let privkey =
                     TYPES::SignatureKey::generated_from_seed_indexed([0u8; 32], node_id).1;
                 let pubkey = TYPES::SignatureKey::from_private(&privkey);
+
+                // Derive the Libp2p keypair from the private key
+                let libp2p_keypair = derive_libp2p_keypair::<TYPES::SignatureKey>(&privkey)
+                    .expect("Failed to derive libp2p keypair");
+
+                // Sign the lookup record
+                let lookup_record_value = RecordValue::new_signed(
+                    &RecordKey::new(Namespace::Lookup, pubkey.to_bytes()),
+                    libp2p_keypair.public().to_peer_id().to_bytes(),
+                    &privkey,
+                )
+                .expect("Failed to sign DHT lookup record");
+
                 // we want the majority of peers to have this lying around.
                 let replication_factor = NonZeroUsize::new(2 * expected_node_count / 3).unwrap();
                 let config = if node_id < num_bootstrap as u64 {
@@ -269,6 +283,7 @@ impl<TYPES: NodeType> TestableNetworkingImplementation<TYPES>
                             mesh_n: (expected_node_count / 2 + 3),
                         }))
                         .server_mode(true)
+                        .identity(libp2p_keypair)
                         .replication_factor(replication_factor)
                         .node_type(NetworkNodeType::Bootstrap)
                         .bound_addr(Some(addr))
@@ -290,6 +305,7 @@ impl<TYPES: NodeType> TestableNetworkingImplementation<TYPES>
                             mesh_n: 8,
                         }))
                         .server_mode(true)
+                        .identity(libp2p_keypair)
                         .replication_factor(replication_factor)
                         .node_type(NetworkNodeType::Regular)
                         .bound_addr(Some(addr))
@@ -304,6 +320,7 @@ impl<TYPES: NodeType> TestableNetworkingImplementation<TYPES>
                 let node_ids_ref = Arc::clone(&node_ids);
                 let da = da_keys.clone();
                 let reliability_config_dup = reliability_config.clone();
+
                 Box::pin(async move {
                     // If it's the second time we are starting this network, clear the bootstrap info
                     let mut write_ids = node_ids_ref.write().await;
@@ -318,6 +335,7 @@ impl<TYPES: NodeType> TestableNetworkingImplementation<TYPES>
                             Libp2pMetricsValue::default(),
                             config,
                             pubkey.clone(),
+                            lookup_record_value,
                             bootstrap_addrs_ref,
                             usize::try_from(node_id).unwrap(),
                             #[cfg(feature = "hotshot-testing")]
@@ -437,6 +455,15 @@ impl<K: SignatureKey + 'static> Libp2pNetwork<K> {
         ))
         .with_context(|| "Failed to calculate replication factor")?;
 
+        // Sign our DHT lookup record
+        let lookup_record_value = RecordValue::new_signed(
+            &RecordKey::new(Namespace::Lookup, pub_key.to_bytes()),
+            // The value is our Libp2p Peer ID
+            keypair.public().to_peer_id().to_bytes(),
+            priv_key,
+        )
+        .with_context(|| "Failed to sign DHT lookup record")?;
+
         config_builder
             .server_mode(libp2p_config.server_mode)
             .identity(keypair)
@@ -477,6 +504,7 @@ impl<K: SignatureKey + 'static> Libp2pNetwork<K> {
             metrics,
             node_config,
             pub_key.clone(),
+            lookup_record_value,
             Arc::new(RwLock::new(bootstrap_nodes)),
             usize::try_from(config.node_index)?,
             #[cfg(feature = "hotshot-testing")]
@@ -519,12 +547,13 @@ impl<K: SignatureKey + 'static> Libp2pNetwork<K> {
         metrics: Libp2pMetricsValue,
         config: NetworkNodeConfig<K>,
         pk: K,
+        lookup_record_value: RecordValue<K>,
         bootstrap_addrs: BootstrapAddrs,
         id: usize,
         #[cfg(feature = "hotshot-testing")] reliability_config: Option<Box<dyn NetworkReliability>>,
         is_da: bool,
     ) -> Result<Libp2pNetwork<K>, NetworkError> {
-        let (mut rx, network_handle) = spawn_network_node(config.clone(), id)
+        let (mut rx, network_handle) = spawn_network_node::<K>(config.clone(), id)
             .await
             .map_err(Into::<NetworkError>::into)?;
         // Make bootstrap mappings known
@@ -588,7 +617,7 @@ impl<K: SignatureKey + 'static> Libp2pNetwork<K> {
 
         result.handle_event_generator(sender, requests_tx, rx);
         result.spawn_node_lookup(node_lookup_recv);
-        result.spawn_connect(id);
+        result.spawn_connect(id, lookup_record_value);
 
         Ok(result)
     }
@@ -608,19 +637,12 @@ impl<K: SignatureKey + 'static> Libp2pNetwork<K> {
                 #[allow(clippy::cast_possible_truncation)]
                 const THRESHOLD: u64 = (LOOK_AHEAD as f64 * 0.8) as u64;
 
-                debug!("Performing lookup for peer {:?}", pk);
+                trace!("Performing lookup for peer {:?}", pk);
 
                 // only run if we are not too close to the next view number
                 if latest_seen_view.load(Ordering::Relaxed) + THRESHOLD <= *view_number {
-                    let pk_bytes = match bincode::serialize(&pk) {
-                        Ok(serialized) => serialized,
-                        Err(e) => {
-                            tracing::error!("Failed to serialize public key; this should never happen. Error: {e}");
-                            return;
-                        }
-                    };
                     // look up
-                    if let Err(err) = handle.lookup_node(&pk_bytes, dht_timeout).await {
+                    if let Err(err) = handle.lookup_node(&pk.to_bytes(), dht_timeout).await {
                         warn!("Failed to perform lookup for key {:?}: {}", pk, err);
                     };
                 }
@@ -629,7 +651,7 @@ impl<K: SignatureKey + 'static> Libp2pNetwork<K> {
     }
 
     /// Initiates connection to the outside world
-    fn spawn_connect(&mut self, id: usize) {
+    fn spawn_connect(&mut self, id: usize, lookup_record_value: RecordValue<K>) {
         let pk = self.inner.pk.clone();
         let bootstrap_ref = Arc::clone(&self.inner.bootstrap_addrs);
         let handle = Arc::clone(&self.inner.handle);
@@ -660,34 +682,18 @@ impl<K: SignatureKey + 'static> Libp2pNetwork<K> {
                     handle.subscribe("DA".to_string()).await.unwrap();
                 }
 
-                // we want our records published before
-                // we begin participating in consensus
-                //
-                // Note: this serialization should never fail,
-                // and if it does the error is unrecoverable.
+                // Map our staking key to our Libp2p Peer ID so we can properly
+                // route direct messages
                 while handle
                     .put_record(
-                        &bincode::serialize(&pk).unwrap(),
-                        &bincode::serialize(&handle.peer_id()).unwrap(),
+                        RecordKey::new(Namespace::Lookup, pk.to_bytes()),
+                        lookup_record_value.clone(),
                     )
                     .await
                     .is_err()
                 {
                     async_sleep(Duration::from_secs(1)).await;
                 }
-
-                while handle
-                    .put_record(
-                        &bincode::serialize(&handle.peer_id()).unwrap(),
-                        &bincode::serialize(&pk).unwrap(),
-                    )
-                    .await
-                    .is_err()
-                {
-                    async_sleep(Duration::from_secs(1)).await;
-                }
-
-                info!("Finished putting Kademlia records");
 
                 // Wait for the network to connect to the required number of peers
                 if let Err(e) = handle.wait_to_connect(4, id).await {
@@ -826,11 +832,7 @@ impl<K: SignatureKey + 'static> ConnectedNetwork<K> for Libp2pNetwork<K> {
         let pid = match self
             .inner
             .handle
-            .lookup_node(
-                &bincode::serialize(&recipient)
-                    .map_err(|e| NetworkError::Libp2p { source: e.into() })?,
-                self.inner.dht_timeout,
-            )
+            .lookup_node(&recipient.to_bytes(), self.inner.dht_timeout)
             .await
         {
             Ok(pid) => pid,
@@ -1043,11 +1045,7 @@ impl<K: SignatureKey + 'static> ConnectedNetwork<K> for Libp2pNetwork<K> {
         let pid = match self
             .inner
             .handle
-            .lookup_node(
-                &bincode::serialize(&recipient)
-                    .map_err(|e| NetworkError::Libp2p { source: e.into() })?,
-                self.inner.dht_timeout,
-            )
+            .lookup_node(&recipient.to_bytes(), self.inner.dht_timeout)
             .await
         {
             Ok(pid) => pid,
