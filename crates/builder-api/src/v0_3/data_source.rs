bf9566fd261348a1d4b9980a97db698140c72a98
Artemii Gerasimovich <artemii@espressosys.com>
2024-08-15 15:46:14 +0300

    Fix BuilderDataSource for marketplace builder (#3576)
    * Fix BuilderDataSource for marketplace builder

--- crates/builder-api/src/v0_3/data_source.rs
+++ crates/builder-api/src/v0_3/data_source.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use hotshot_types::{bundle::Bundle, traits::node_implementation::NodeType};
+use hotshot_types::{bundle::Bundle, traits::node_implementation::NodeType, vid::VidCommitment};
 
 use super::builder::BuildError;
 /// No changes to these types
@@ -8,7 +8,12 @@ pub use crate::v0_1::data_source::AcceptsTxnSubmits;
 #[async_trait]
 pub trait BuilderDataSource<TYPES: NodeType> {
     /// To get the list of available blocks
-    async fn bundle(&self, view_number: u64) -> Result<Bundle<TYPES>, BuildError>;
+    async fn bundle(
+        &self,
+        parent_view: u64,
+        parent_hash: &VidCommitment,
+        view_number: u64,
+    ) -> Result<Bundle<TYPES>, BuildError>;
 
     /// To get the builder's address
     async fn builder_address(&self) -> Result<TYPES::BuilderSignatureKey, BuildError>;
