6826229847631204dc1e84ecfdfa9837db0d344a
ss-es <ss-es@users.noreply.github.com>
2024-08-16 16:00:34 +0000

    deploy: e87fc540b0b6c5379a13f6913a7c4f08515085e1

--- hotshot_testing/consistency_task/sidebar-items.js
+++ hotshot_testing/consistency_task/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["sanitize_network_map","sanitize_node_map"],"struct":["ConsistencyTask"],"trait":["Validatable"],"type":["NetworkMap","NetworkMapSanitized","NodeMap","NodeMapSanitized"]};
+window.SIDEBAR_ITEMS = {"fn":["invert_network_map","sanitize_network_map","sanitize_node_map","sanitize_view_map","validate_node_map"],"struct":["ConsistencyTask"],"type":["NetworkMap","NetworkMapSanitized","NodeMap","NodeMapSanitized","ViewMap","ViewMapSanitized"]};
