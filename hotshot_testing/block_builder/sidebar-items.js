dac05f3114c1d84323c77dc6ecb18f03de64fd3d
ss-es <ss-es@users.noreply.github.com>
2024-09-04 16:29:33 +0000

    deploy: 3403dc2e53257e742e60c6d5bf5b88b749fb7a0e

--- hotshot_testing/block_builder/sidebar-items.js
+++ hotshot_testing/block_builder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["build_block","run_builder_source"],"mod":["random","simple"],"struct":["BlockEntry"],"trait":["BuilderTask","TestBuilderImplementation"]};
+window.SIDEBAR_ITEMS = {"fn":["build_block","run_builder_source","run_builder_source_0_1"],"mod":["random","simple"],"struct":["BlockEntry"],"trait":["BuilderTask","TestBuilderImplementation"]};
