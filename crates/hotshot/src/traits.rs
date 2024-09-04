aaa99234430a28ae039ba42bf5907b0bf812a92c
Rob <rob@espressosys.com>
2024-09-04 09:58:54 -0400

    reexport `GossipConfig`

--- crates/hotshot/src/traits.rs
+++ crates/hotshot/src/traits.rs
@@ -19,8 +19,8 @@ pub mod implementations {
     pub use super::networking::{
         combined_network::{CombinedNetworks, UnderlyingCombinedNetworks},
         libp2p_network::{
-            derive_libp2p_keypair, derive_libp2p_peer_id, Libp2pMetricsValue, Libp2pNetwork,
-            PeerInfoVec,
+            derive_libp2p_keypair, derive_libp2p_peer_id, GossipConfig, Libp2pMetricsValue,
+            Libp2pNetwork, PeerInfoVec,
         },
         memory_network::{MasterMap, MemoryNetwork},
         push_cdn_network::{
