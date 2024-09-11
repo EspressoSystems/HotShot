45f70a4c52614d79a207e05e21b1c6b8e75fc8f1
Brendon Fish <bfish713@gmail.com>
2024-09-11 13:34:14 -0400

    Bf/restart tune (#3668)
    * actually restart all nodes in restart tests
    
    * saturating sub
    
    * lint

--- crates/testing/tests/tests_2/catchup.rs
+++ crates/testing/tests/tests_2/catchup.rs
@@ -322,7 +322,7 @@ async fn test_all_restart() {
     let mut metadata: TestDescription<TestTypes, CombinedImpl, TestVersions> =
         TestDescription::default();
     let mut catchup_nodes = vec![];
-    for i in 1..20 {
+    for i in 0..20 {
         catchup_nodes.push(ChangeNode {
             idx: i,
             updown: UpDown::Restart,
@@ -384,7 +384,7 @@ async fn test_all_restart_cdn() {
     let mut metadata: TestDescription<TestTypes, PushCdnImpl, TestVersions> =
         TestDescription::default();
     let mut catchup_nodes = vec![];
-    for i in 1..20 {
+    for i in 0..20 {
         catchup_nodes.push(ChangeNode {
             idx: i,
             updown: UpDown::Restart,
@@ -450,12 +450,8 @@ async fn test_all_restart_one_da() {
     let mut metadata: TestDescription<TestTypes, CombinedImpl, TestVersions> =
         TestDescription::default();
 
-    let node_0_down = vec![ChangeNode {
-        idx: 0,
-        updown: UpDown::Restart,
-    }];
     let mut catchup_nodes = vec![];
-    for i in 1..20 {
+    for i in 0..20 {
         catchup_nodes.push(ChangeNode {
             idx: i,
             updown: UpDown::Restart,
@@ -471,7 +467,7 @@ async fn test_all_restart_one_da() {
 
     metadata.spinning_properties = SpinningTaskDescription {
         // Restart all the nodes in view 13
-        node_changes: vec![(12, node_0_down), (13, catchup_nodes)],
+        node_changes: vec![(13, catchup_nodes)],
     };
     metadata.view_sync_properties =
         hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
