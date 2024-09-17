f06ff79112d682c7b86959528185665eadc1d56a
ss-es <ss-es@users.noreply.github.com>
2024-09-17 16:09:37 +0000

    deploy: 2567b1adf7c2693bb565208b96c89e9e1e06a4e3

--- hotshot_testing/test_builder/sidebar-items.js
+++ hotshot_testing/test_builder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Behaviour","BuilderChange"],"fn":["create_test_handle"],"struct":["BuilderDescription","FakeSolverApiDescription","TestDescription","TimingData"]};
+window.SIDEBAR_ITEMS = {"enum":["Behaviour","BuilderChange"],"fn":["create_test_handle","nonempty_block_limit","nonempty_block_threshold"],"struct":["BuilderDescription","FakeSolverApiDescription","TestDescription","TimingData"],"type":["TransactionValidator"]};
