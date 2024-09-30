179e76497ccba9d5ea05a074b0ea975db5221e12
ss-es <155648797+ss-es@users.noreply.github.com>
2024-09-30 12:01:16 -0400

    update

--- crates/testing/tests/tests_2/catchup.rs
+++ crates/testing/tests/tests_2/catchup.rs
@@ -4,6 +4,24 @@
 // You should have received a copy of the MIT License
 // along with the HotShot repository. If not, see <https://mit-license.org/>.
 
+use std::time::Duration;
+
+use hotshot_example_types::node_types::{
+    CombinedImpl, PushCdnImpl, TestTypes, TestTypesRandomizedLeader, TestVersions,
+};
+#[cfg(feature = "dependency-tasks")]
+use hotshot_example_types::testable_delay::{
+    DelayConfig, DelayOptions, DelaySettings, SupportedTraitTypesForAsyncDelay,
+};
+use hotshot_macros::cross_tests;
+use hotshot_testing::{
+    block_builder::SimpleBuilderImplementation,
+    completion_task::{CompletionTaskDescription, TimeBasedCompletionTaskDescription},
+    overall_safety_task::OverallSafetyPropertiesDescription,
+    spinning_task::{ChangeNode, NodeAction, SpinningTaskDescription},
+    test_builder::{TestDescription, TimingData},
+};
+
 #[cfg(test)]
 #[cfg_attr(async_executor_impl = "tokio", tokio::test(flavor = "multi_thread"))]
 #[cfg_attr(async_executor_impl = "async-std", async_std::test)]
@@ -301,274 +319,172 @@ async fn test_catchup_reload() {
         .await;
 }
 
-#[cfg(test)]
-#[cfg_attr(async_executor_impl = "tokio", tokio::test(flavor = "multi_thread"))]
-#[cfg_attr(async_executor_impl = "async-std", async_std::test)]
-async fn test_all_restart() {
-    use std::time::Duration;
+cross_tests!(
+    TestName: test_all_restart,
+    Impls: [CombinedImpl, PushCdnImpl],
+    Types: [TestTypes, TestTypesRandomizedLeader],
+    Versions: [TestVersions],
+    Ignore: false,
+    Metadata: {
+      let timing_data = TimingData {
+          next_view_timeout: 2000,
+          round_start_delay: 500,
+          ..Default::default()
+      };
+      let mut metadata = TestDescription::default();
+      let mut catchup_nodes = vec![];
 
-    use hotshot_example_types::node_types::{CombinedImpl, TestTypes, TestVersions};
-    use hotshot_testing::{
-        block_builder::SimpleBuilderImplementation,
-        completion_task::{CompletionTaskDescription, TimeBasedCompletionTaskDescription},
-        overall_safety_task::OverallSafetyPropertiesDescription,
-        spinning_task::{ChangeNode, NodeAction, SpinningTaskDescription},
-        test_builder::{TestDescription, TimingData},
-    };
+      for i in 0..20 {
+          catchup_nodes.push(ChangeNode {
+              idx: i,
+              updown: NodeAction::RestartDown(0),
+          })
+      }
 
-    async_compatibility_layer::logging::setup_logging();
-    async_compatibility_layer::logging::setup_backtrace();
-    let timing_data = TimingData {
-        next_view_timeout: 2000,
-        round_start_delay: 500,
-        ..Default::default()
-    };
-    let mut metadata: TestDescription<TestTypes, CombinedImpl, TestVersions> =
-        TestDescription::default();
-    let mut catchup_nodes = vec![];
-    for i in 0..20 {
-        catchup_nodes.push(ChangeNode {
-            idx: i,
-            updown: NodeAction::RestartDown(0),
-        })
-    }
+      metadata.timing_data = timing_data;
+      metadata.start_nodes = 20;
+      metadata.num_nodes_with_stake = 20;
 
-    metadata.timing_data = timing_data;
-    metadata.start_nodes = 20;
-    metadata.num_nodes_with_stake = 20;
+      metadata.spinning_properties = SpinningTaskDescription {
+          // Restart all the nodes in view 13
+          node_changes: vec![(13, catchup_nodes)],
+      };
+      metadata.view_sync_properties =
+          hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
 
-    metadata.spinning_properties = SpinningTaskDescription {
-        // Restart all the nodes in view 13
-        node_changes: vec![(13, catchup_nodes)],
-    };
-    metadata.view_sync_properties =
-        hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
+      metadata.completion_task_description =
+          CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
+              TimeBasedCompletionTaskDescription {
+                  duration: Duration::from_secs(60),
+              },
+          );
+      metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
+          // Make sure we keep committing rounds after the catchup, but not the full 50.
+          num_successful_views: 22,
+          num_failed_views: 15,
+          ..Default::default()
+      };
 
-    metadata.completion_task_description =
-        CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
-            TimeBasedCompletionTaskDescription {
-                duration: Duration::from_secs(60),
-            },
-        );
-    metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
-        // Make sure we keep committing rounds after the catchup, but not the full 50.
-        num_successful_views: 22,
-        num_failed_views: 15,
-        ..Default::default()
-    };
+      metadata
+    },
+);
 
-    metadata
-        .gen_launcher(0)
-        .launch()
-        .run_test::<SimpleBuilderImplementation>()
-        .await;
-}
+// This test case ensures that proposals persist off of a restart. We demonstrate this by
+// artificially removing node 0 (the only DA committee member) from the candidate pool,
+// meaning that the entire DA also does not have the proposal, but we're still able to
+// move on because the *leader* does have the proposal.
+cross_tests!(
+    TestName: test_all_restart_one_da,
+    Impls: [CombinedImpl],
+    Types: [TestTypes],
+    Versions: [TestVersions],
+    Ignore: false,
+    Metadata: {
+      let timing_data = TimingData {
+          next_view_timeout: 2000,
+          round_start_delay: 500,
+          ..Default::default()
+      };
+      let mut metadata: TestDescription<TestTypes, CombinedImpl, TestVersions> =
+          TestDescription::default();
 
-#[cfg(test)]
-#[cfg_attr(async_executor_impl = "tokio", tokio::test(flavor = "multi_thread"))]
-#[cfg_attr(async_executor_impl = "async-std", async_std::test)]
-async fn test_all_restart_cdn() {
-    use std::time::Duration;
+      let mut catchup_nodes = vec![];
+      for i in 0..20 {
+          catchup_nodes.push(ChangeNode {
+              idx: i,
+              updown: NodeAction::RestartDown(0),
+          })
+      }
 
-    use hotshot_example_types::node_types::{PushCdnImpl, TestTypes, TestVersions};
-    use hotshot_testing::{
-        block_builder::SimpleBuilderImplementation,
-        completion_task::{CompletionTaskDescription, TimeBasedCompletionTaskDescription},
-        overall_safety_task::OverallSafetyPropertiesDescription,
-        spinning_task::{ChangeNode, NodeAction, SpinningTaskDescription},
-        test_builder::{TestDescription, TimingData},
-    };
+      metadata.timing_data = timing_data;
+      metadata.start_nodes = 20;
+      metadata.num_nodes_with_stake = 20;
 
-    async_compatibility_layer::logging::setup_logging();
-    async_compatibility_layer::logging::setup_backtrace();
-    let timing_data = TimingData {
-        next_view_timeout: 2000,
-        round_start_delay: 500,
-        ..Default::default()
-    };
-    let mut metadata: TestDescription<TestTypes, PushCdnImpl, TestVersions> =
-        TestDescription::default();
-    let mut catchup_nodes = vec![];
-    for i in 0..20 {
-        catchup_nodes.push(ChangeNode {
-            idx: i,
-            updown: NodeAction::RestartDown(0),
-        })
-    }
+      // Explicitly make the DA tiny to exaggerate a missing proposal.
+      metadata.da_staked_committee_size = 1;
 
-    metadata.timing_data = timing_data;
-    metadata.start_nodes = 20;
-    metadata.num_nodes_with_stake = 20;
+      metadata.spinning_properties = SpinningTaskDescription {
+          // Restart all the nodes in view 13
+          node_changes: vec![(13, catchup_nodes)],
+      };
+      metadata.view_sync_properties =
+          hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
 
-    metadata.spinning_properties = SpinningTaskDescription {
-        // Restart all the nodes in view 13
-        node_changes: vec![(13, catchup_nodes)],
-    };
-    metadata.view_sync_properties =
-        hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
+      metadata.completion_task_description =
+          CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
+              TimeBasedCompletionTaskDescription {
+                  duration: Duration::from_secs(60),
+              },
+          );
+      metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
+          // Make sure we keep committing rounds after the catchup, but not the full 50.
+          num_successful_views: 22,
+          num_failed_views: 15,
+          ..Default::default()
+      };
 
-    metadata.completion_task_description =
-        CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
-            TimeBasedCompletionTaskDescription {
-                duration: Duration::from_secs(60),
-            },
-        );
-    metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
-        // Make sure we keep committing rounds after the catchup, but not the full 50.
-        num_successful_views: 22,
-        num_failed_views: 15,
-        ..Default::default()
-    };
 
-    metadata
-        .gen_launcher(0)
-        .launch()
-        .run_test::<SimpleBuilderImplementation>()
-        .await;
-}
+      metadata
+    },
+);
 
-/// This test case ensures that proposals persist off of a restart. We demonstrate this by
-/// artificially removing node 0 (the only DA committee member) from the candidate pool,
-/// meaning that the entire DA also does not have the proposal, but we're still able to
-/// move on because the *leader* does have the proposal.
-#[cfg(test)]
-#[cfg_attr(async_executor_impl = "tokio", tokio::test(flavor = "multi_thread"))]
-#[cfg_attr(async_executor_impl = "async-std", async_std::test)]
-async fn test_all_restart_one_da() {
-    use std::time::Duration;
+cross_tests!(
+    TestName: test_staggered_restart,
+    Impls: [CombinedImpl],
+    Types: [TestTypes],
+    Versions: [TestVersions],
+    Ignore: false,
+    Metadata: {
+      let mut metadata = TestDescription::default();
 
-    use hotshot_example_types::node_types::{CombinedImpl, TestTypes, TestVersions};
-    use hotshot_testing::{
-        block_builder::SimpleBuilderImplementation,
-        completion_task::{CompletionTaskDescription, TimeBasedCompletionTaskDescription},
-        overall_safety_task::OverallSafetyPropertiesDescription,
-        spinning_task::{ChangeNode, NodeAction, SpinningTaskDescription},
-        test_builder::{TestDescription, TimingData},
-    };
+      let mut down_da_nodes = vec![];
+      for i in 1..4 {
+          down_da_nodes.push(ChangeNode {
+              idx: i,
+              updown: NodeAction::RestartDown(20),
+          });
+      }
 
-    async_compatibility_layer::logging::setup_logging();
-    async_compatibility_layer::logging::setup_backtrace();
-    let timing_data = TimingData {
-        next_view_timeout: 2000,
-        round_start_delay: 500,
-        ..Default::default()
-    };
-    let mut metadata: TestDescription<TestTypes, CombinedImpl, TestVersions> =
-        TestDescription::default();
+      let mut down_regular_nodes = vec![];
+      for i in 4..10 {
+          down_regular_nodes.push(ChangeNode {
+              idx: i,
+              updown: NodeAction::RestartDown(0),
+          });
+      }
+      // restart the last da so it gets the new libp2p routing table
+      down_regular_nodes.push(ChangeNode {
+          idx: 0,
+          updown: NodeAction::RestartDown(0),
+      });
 
-    let mut catchup_nodes = vec![];
-    for i in 0..20 {
-        catchup_nodes.push(ChangeNode {
-            idx: i,
-            updown: NodeAction::RestartDown(0),
-        })
-    }
+      metadata.start_nodes = 10;
+      metadata.num_nodes_with_stake = 10;
 
-    metadata.timing_data = timing_data;
-    metadata.start_nodes = 20;
-    metadata.num_nodes_with_stake = 20;
+      // Explicitly make the DA small to simulate real network.
+      metadata.da_staked_committee_size = 4;
 
-    // Explicitly make the DA tiny to exaggerate a missing proposal.
-    metadata.da_staked_committee_size = 1;
+      metadata.spinning_properties = SpinningTaskDescription {
+          // Restart all the nodes in view 13
+          node_changes: vec![(13, down_da_nodes), (33, down_regular_nodes)],
+      };
+      metadata.view_sync_properties =
+          hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 50);
 
-    metadata.spinning_properties = SpinningTaskDescription {
-        // Restart all the nodes in view 13
-        node_changes: vec![(13, catchup_nodes)],
-    };
-    metadata.view_sync_properties =
-        hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
+      // Give the test some extra time because we are purposely timing out views
+      metadata.completion_task_description =
+          CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
+              TimeBasedCompletionTaskDescription {
+                  duration: Duration::from_secs(240),
+              },
+          );
+      metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
+          // Make sure we keep committing rounds after the catchup, but not the full 50.
+          num_successful_views: 22,
+          num_failed_views: 30,
+          ..Default::default()
+      };
 
-    metadata.completion_task_description =
-        CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
-            TimeBasedCompletionTaskDescription {
-                duration: Duration::from_secs(60),
-            },
-        );
-    metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
-        // Make sure we keep committing rounds after the catchup, but not the full 50.
-        num_successful_views: 22,
-        num_failed_views: 15,
-        ..Default::default()
-    };
-
-    metadata
-        .gen_launcher(0)
-        .launch()
-        .run_test::<SimpleBuilderImplementation>()
-        .await;
-}
-
-#[cfg(test)]
-#[cfg_attr(async_executor_impl = "tokio", tokio::test(flavor = "multi_thread"))]
-#[cfg_attr(async_executor_impl = "async-std", async_std::test)]
-async fn test_staggered_restart() {
-    use std::time::Duration;
-
-    use hotshot_example_types::node_types::{CombinedImpl, TestTypes, TestVersions};
-    use hotshot_testing::{
-        block_builder::SimpleBuilderImplementation,
-        completion_task::{CompletionTaskDescription, TimeBasedCompletionTaskDescription},
-        overall_safety_task::OverallSafetyPropertiesDescription,
-        spinning_task::{ChangeNode, NodeAction, SpinningTaskDescription},
-        test_builder::TestDescription,
-    };
-
-    async_compatibility_layer::logging::setup_logging();
-    async_compatibility_layer::logging::setup_backtrace();
-    let mut metadata: TestDescription<TestTypes, CombinedImpl, TestVersions> =
-        TestDescription::default();
-
-    let mut down_da_nodes = vec![];
-    for i in 1..4 {
-        down_da_nodes.push(ChangeNode {
-            idx: i,
-            updown: NodeAction::RestartDown(20),
-        });
-    }
-
-    let mut down_regular_nodes = vec![];
-    for i in 4..10 {
-        down_regular_nodes.push(ChangeNode {
-            idx: i,
-            updown: NodeAction::RestartDown(0),
-        });
-    }
-    // restart the last da so it gets the new libp2p routing table
-    down_regular_nodes.push(ChangeNode {
-        idx: 0,
-        updown: NodeAction::RestartDown(0),
-    });
-
-    metadata.start_nodes = 10;
-    metadata.num_nodes_with_stake = 10;
-
-    // Explicitly make the DA small to simulate real network.
-    metadata.da_staked_committee_size = 4;
-
-    metadata.spinning_properties = SpinningTaskDescription {
-        // Restart all the nodes in view 13
-        node_changes: vec![(13, down_da_nodes), (33, down_regular_nodes)],
-    };
-    metadata.view_sync_properties =
-        hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 50);
-
-    // Give the test some extra time because we are purposely timing out views
-    metadata.completion_task_description =
-        CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
-            TimeBasedCompletionTaskDescription {
-                duration: Duration::from_secs(240),
-            },
-        );
-    metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
-        // Make sure we keep committing rounds after the catchup, but not the full 50.
-        num_successful_views: 22,
-        num_failed_views: 30,
-        ..Default::default()
-    };
-
-    metadata
-        .gen_launcher(0)
-        .launch()
-        .run_test::<SimpleBuilderImplementation>()
-        .await;
-}
+      metadata
+    },
+);
