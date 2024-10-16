91495b8980a97e95b3325105c110da0241e1c79c
QuentinI <QuentinI@users.noreply.github.com>
2024-10-16 15:28:02 +0000

    deploy: 1d48deec38979e2a7fd6b625c9d62554d4caae56

--- hotshot_testing/test_task/sidebar-items.js
+++ hotshot_testing/test_task/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["TestEvent","TestResult"],"fn":["add_network_message_test_task"],"struct":["TestTask"],"trait":["TestTaskState"]};
+window.SIDEBAR_ITEMS = {"enum":["TestEvent","TestResult"],"fn":["add_network_message_test_task"],"struct":["TestTask"],"trait":["TestTaskState","TestTaskStateSeed"],"type":["AnyTestTaskState"]};
