b480cb482131b0e383798202e9cdce31e834962f
Brendon Fish <bfish713@gmail.com>
2024-10-18 16:15:52 -0400

    more

--- crates/testing/tests/tests_3/byzantine_tests.rs
+++ crates/testing/tests/tests_3/byzantine_tests.rs
@@ -80,7 +80,7 @@ cross_tests!(
             ..TestDescription::default()
         };
 
-        metadata.overall_safety_properties.num_failed_views = 10;
+        metadata.overall_safety_properties.num_failed_views = 15;
 
         metadata
     },
