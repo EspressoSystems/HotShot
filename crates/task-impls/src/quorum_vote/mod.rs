033513c8cd08c7a260d70ef9c948625a3c8e89e6
pls148 <184445976+pls148@users.noreply.github.com>
2024-10-24 13:31:37 -0700

    3215 dont use high qc for handle_dep_result

--- crates/task-impls/src/quorum_vote/mod.rs
+++ crates/task-impls/src/quorum_vote/mod.rs
@@ -12,13 +12,13 @@ use async_lock::RwLock;
 use async_std::task::JoinHandle;
 use async_trait::async_trait;
 use hotshot_task::{
-    dependency::{AndDependency, Dependency, EventDependency},
+    dependency::{AndDependency, EventDependency},
     dependency_task::{DependencyTask, HandleDepOutput},
     task::TaskState,
 };
 use hotshot_types::{
     consensus::OuterConsensus,
-    data::{Leaf, QuorumProposal, ViewNumber},
+    data::{Leaf, QuorumProposal},
     event::Event,
     message::{Proposal, UpgradeLock},
     traits::{
@@ -94,36 +94,10 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
     #[allow(clippy::too_many_lines)]
     #[instrument(skip_all, fields(id = self.id, view = *self.view_number))]
     async fn handle_dep_result(self, res: Self::Output) {
-        let high_qc_view_number = self.consensus.read().await.high_qc().view_number;
-
-        // The validated state of a non-genesis high QC should exist in the state map.
-        if *high_qc_view_number != *ViewNumber::genesis()
-            && !self
-                .consensus
-                .read()
-                .await
-                .validated_state_map()
-                .contains_key(&high_qc_view_number)
-        {
-            // Block on receiving the event from the event stream.
-            EventDependency::new(
-                self.receiver.activate_cloned(),
-                Box::new(move |event| {
-                    let event = event.as_ref();
-                    if let HotShotEvent::ValidatedStateUpdated(view_number, _) = event {
-                        *view_number == high_qc_view_number
-                    } else {
-                        false
-                    }
-                }),
-            )
-            .completed()
-            .await;
-        }
-
         let mut payload_commitment = None;
         let mut leaf = None;
         let mut vid_share = None;
+        let mut parent_view_number = None;
         for event in res {
             match event.as_ref() {
                 #[allow(unused_assignments)]
@@ -168,6 +142,7 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
                         return;
                     }
                     leaf = Some(proposed_leaf);
+                    parent_view_number = Some(parent_leaf.view_number());
                 }
                 HotShotEvent::DaCertificateValidated(cert) => {
                     let cert_payload_comm = &cert.data().payload_commit;
@@ -195,6 +170,7 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
                 _ => {}
             }
         }
+
         broadcast_event(
             Arc::new(HotShotEvent::QuorumVoteDependenciesValidated(
                 self.view_number,
@@ -233,6 +209,7 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
             Arc::clone(&self.storage),
             &leaf,
             &vid_share,
+            parent_view_number,
         )
         .await
         {
@@ -694,6 +671,7 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES>, V: Versions> QuorumVoteTaskS
             Arc::clone(&self.storage),
             &proposed_leaf,
             &updated_vid,
+            Some(parent_leaf.view_number()),
         )
         .await
         {
