59fed932c1e82658183939ff2edd04b1b398b5be
bfish713 <bfish713@gmail.com>
2024-10-29 23:35:03 -0400

    lint

--- crates/testing/src/test_task.rs
+++ crates/testing/src/test_task.rs
@@ -4,7 +4,7 @@
 // You should have received a copy of the MIT License
 // along with the HotShot repository. If not, see <https://mit-license.org/>.
 
-use std::{num::NonZeroUsize, sync::Arc, time::Duration};
+use std::{sync::Arc, time::Duration};
 
 use anyhow::Result;
 use async_broadcast::{Receiver, Sender};
