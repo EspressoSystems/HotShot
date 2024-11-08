fdbe9207916ff3e16937ed507c4adde15b6a8229
Sishan Long <dlsubjam@gmail.com>
2024-11-07 18:17:16 -0800

    type

--- crates/builder-api/src/v0_1/builder.rs
+++ crates/builder-api/src/v0_1/builder.rs
@@ -50,7 +50,7 @@ pub enum BuildError {
     Error(String),
 }
 
-/// Enum to keep track on status of a transactions
+/// Enum to keep track on status of a transaction
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub enum TransactionStatus {
     Pending,
