4bf5595e8151e0940ce514573cb9341eb6144caa
ss-es <ss-es@users.noreply.github.com>
2024-11-19 20:33:12 +0000

    deploy: 7abc4ba4d615f2042df74d73e82a1861ee28a0e8

--- hotshot_types/message/sidebar-items.js
+++ hotshot_types/message/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["DaConsensusMessage","DataMessage","GeneralConsensusMessage","MessageKind","MessagePurpose","RecipientList","SequencingMessage"],"struct":["Message","Messages","Proposal","UpgradeLock"]};
+window.SIDEBAR_ITEMS = {"enum":["DaConsensusMessage","DataMessage","GeneralConsensusMessage","MessageKind","MessagePurpose","RecipientList","SequencingMessage"],"fn":["convert_proposal"],"struct":["Message","Messages","Proposal","UpgradeLock"]};
