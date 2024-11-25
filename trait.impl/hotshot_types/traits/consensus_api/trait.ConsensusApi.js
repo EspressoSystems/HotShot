3311912c6b5ccde1748add2c9e5db8a4012b9566
DieracDelta <DieracDelta@users.noreply.github.com>
2024-11-25 00:29:49 +0000

    deploy: 1e54cc31aad7a0eba90dc69b10fc5e9dcfc3895f

--- trait.impl/hotshot_types/traits/consensus_api/trait.ConsensusApi.js
+++ trait.impl/hotshot_types/traits/consensus_api/trait.ConsensusApi.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hotshot",[["impl&lt;TYPES: NodeType, I: <a class=\"trait\" href=\"hotshot/traits/trait.NodeImplementation.html\" title=\"trait hotshot::traits::NodeImplementation\">NodeImplementation</a>&lt;TYPES&gt;, V: Versions&gt; ConsensusApi&lt;TYPES, I&gt; for <a class=\"struct\" href=\"hotshot/types/handle/struct.SystemContextHandle.html\" title=\"struct hotshot::types::handle::SystemContextHandle\">SystemContextHandle</a>&lt;TYPES, I, V&gt;"]]]]);
+    var implementors = Object.fromEntries([["hotshot",[["impl&lt;TYPES: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeType.html\" title=\"trait hotshot_types::traits::node_implementation::NodeType\">NodeType</a>, I: <a class=\"trait\" href=\"hotshot/traits/trait.NodeImplementation.html\" title=\"trait hotshot::traits::NodeImplementation\">NodeImplementation</a>&lt;TYPES&gt;, V: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.Versions.html\" title=\"trait hotshot_types::traits::node_implementation::Versions\">Versions</a>&gt; <a class=\"trait\" href=\"hotshot_types/traits/consensus_api/trait.ConsensusApi.html\" title=\"trait hotshot_types::traits::consensus_api::ConsensusApi\">ConsensusApi</a>&lt;TYPES, I&gt; for <a class=\"struct\" href=\"hotshot/types/handle/struct.SystemContextHandle.html\" title=\"struct hotshot::types::handle::SystemContextHandle\">SystemContextHandle</a>&lt;TYPES, I, V&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[442]}
+//{"start":57,"fragment_lengths":[924]}
