d4fa5208aa02c2c49989807064e369dbdbc8305e
pls148 <pls148@users.noreply.github.com>
2025-01-18 00:05:44 +0000

    deploy: fa68e1beb6736e8be43625faaf7a2286afef9e36

--- trait.impl/futures_core/stream/trait.Stream.js
+++ trait.impl/futures_core/stream/trait.Stream.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hotshot_testing",[["impl Stream for <a class=\"struct\" href=\"hotshot_testing/view_generator/struct.TestViewGenerator.html\" title=\"struct hotshot_testing::view_generator::TestViewGenerator\">TestViewGenerator</a>"]]]]);
+    var implementors = Object.fromEntries([["hotshot_testing",[["impl&lt;V: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.Versions.html\" title=\"trait hotshot_types::traits::node_implementation::Versions\">Versions</a>&gt; Stream for <a class=\"struct\" href=\"hotshot_testing/view_generator/struct.TestViewGenerator.html\" title=\"struct hotshot_testing::view_generator::TestViewGenerator\">TestViewGenerator</a>&lt;V&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[221]}
+//{"start":57,"fragment_lengths":[411]}
