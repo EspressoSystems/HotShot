cf51472a06025a05bb261517d27cd5dd1d772b84
mrain <mrain@users.noreply.github.com>
2025-01-21 17:45:30 +0000

    deploy: 02aa36a5e2e6d61c01289aafc5a44996a36c3a64

--- trait.impl/jf_vid/precomputable/trait.Precomputable.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["hotshot_types",[["impl Precomputable for <a class=\"struct\" href=\"hotshot_types/vid/struct.VidSchemeType.html\" title=\"struct hotshot_types::vid::VidSchemeType\">VidSchemeType</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[188]}
