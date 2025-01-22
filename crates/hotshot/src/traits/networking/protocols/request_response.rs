2878f2dc71b59def54ece766f33bef31e73d7bb1
Rob <rob@espressosys.com>
2025-01-22 07:23:48 -0500

    `Serializable`

--- crates/hotshot/src/traits/networking/protocols/request_response.rs
+++ crates/hotshot/src/traits/networking/protocols/request_response.rs
@@ -1,5 +1,6 @@
 use std::{marker::PhantomData, time::Duration};
 
+use anyhow::{Context, Error, Result};
 use async_trait::async_trait;
 use hotshot_types::traits::{network::ConnectedNetwork, signature_key::SignatureKey};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
@@ -12,17 +13,68 @@ pub trait Validatable {
     fn is_valid(&self) -> bool;
 }
 
-/// A trait alias to help with readability
-pub trait Request: Serialize + DeserializeOwned + Send + Sync + Validatable {}
-pub trait Response: Serialize + DeserializeOwned + Send + Sync + Validatable {}
+pub trait Serializable: Sized {
+    /// Serialize the type to a byte array
+    fn to_bytes(&self) -> Result<Vec<u8>>;
+
+    /// Deserialize the type from a byte array
+    fn from_bytes(bytes: &[u8]) -> Result<Self>;
+}
+
+/// A trait for a request. Associates itself with a response type.
+pub trait Request: Send + Sync + Serializable + Validatable {
+    /// The response type associated with this request
+    type Response: Response;
+}
+
+/// A trait that a response needs to implement
+pub trait Response: Send + Sync + Serializable + Validatable {}
 
 /// The message type for the request-response protocol
-#[derive(Serialize, Deserialize)]
-pub enum Message<Q, A> {
+pub enum Message<R: Request> {
     /// A request message
-    Request(Q),
+    Request(R),
     /// A response message
-    Response(A),
+    Response(R::Response),
+}
+
+/// A blanket implementation of the [`Serializable`] trait for any [`Message`]
+impl<R: Request> Serializable for Message<R> {
+    /// Convert any [`Message`] to bytes
+    fn to_bytes(&self) -> Result<Vec<u8>> {
+        // Convert the message to bytes based on the type. By default it is just type-prefixed
+        let bytes = match self {
+            Message::Request(request) => [&[0], request.to_bytes()?.as_slice()].concat(),
+            Message::Response(response) => [&[1], response.to_bytes()?.as_slice()].concat(),
+        };
+
+        Ok(bytes)
+    }
+
+    /// Convert bytes to a [`Message`]
+    fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        // Get the message type
+        let message_type = bytes
+            .get(0)
+            .ok_or(anyhow::anyhow!("message type not found"))?;
+
+        // Deserialize the message based on the type
+        match message_type {
+            0 => {
+                let request_bytes = bytes
+                    .get(1..)
+                    .ok_or(anyhow::anyhow!("request bytes not found"))?;
+                Ok(Message::Request(R::from_bytes(request_bytes)?))
+            }
+            1 => {
+                let response_bytes = bytes
+                    .get(1..)
+                    .ok_or(anyhow::anyhow!("response bytes not found"))?;
+                Ok(Message::Response(R::Response::from_bytes(response_bytes)?))
+            }
+            _ => Err(anyhow::anyhow!("invalid message type")),
+        }
+    }
 }
 
 /// The [`Sender`] trait is used to allow the [`RequestResponseProtocol`] to send messages to a specific recipient
@@ -30,11 +82,7 @@ pub enum Message<Q, A> {
 #[async_trait]
 pub trait Sender<K: SignatureKey + 'static> {
     /// Send a message to a specified recipient
-    async fn send_message<Q: Request, A: Response>(
-        &self,
-        message: &Message<Q, A>,
-        recipient: K,
-    ) -> anyhow::Result<()>;
+    async fn send_message<R: Request>(&self, message: &Message<R>, recipient: K) -> Result<()>;
 }
 
 /// The [`Receiver`] trait is used to allow the [`RequestResponseProtocol`] to receive messages from a network
@@ -42,31 +90,29 @@ pub trait Sender<K: SignatureKey + 'static> {
 #[async_trait]
 pub trait Receiver {
     /// Receive a message
-    async fn receive_message<Q: Request, A: Response>(&mut self) -> anyhow::Result<Message<Q, A>>;
+    async fn receive_message<R: Request>(&mut self) -> Result<Message<R>>;
 }
 
 /// A trait that allows the [`RequestResponseProtocol`] to get the recipients that a specific message should
 /// expect responses from
 pub trait RecipientSource<K: SignatureKey + 'static> {
     /// Get all the recipients that the specific message should expect responses from
-    fn get_recipients_for<Q: Request, A: Response>(&self, message: &Message<Q, A>) -> Vec<K>;
+    fn get_recipients_for<R: Request>(&self, message: &Message<R>) -> Vec<K>;
 }
 
 /// A blanket implementation of the [`Sender`] trait for all types that implement [`ConnectedNetwork`]
 #[async_trait]
 impl<N: ConnectedNetwork<K>, K: SignatureKey + 'static> Sender<K> for N {
-    async fn send_message<Q: Request, A: Response>(
-        &self,
-        message: &Message<Q, A>,
-        recipient: K,
-    ) -> anyhow::Result<()> {
+    async fn send_message<R: Request>(&self, message: &Message<R>, recipient: K) -> Result<()> {
         // Serialize the message
-        let serialized_message = bincode::serialize(message)?;
+        let serialized_message = message
+            .to_bytes()
+            .with_context(|| "failed to serialize message")?;
 
         // Just send the message to the recipient
         self.direct_message(serialized_message, recipient)
             .await
-            .map_err(anyhow::Error::from)
+            .with_context(|| "failed to send message")
     }
 }
 
@@ -74,12 +120,12 @@ impl<N: ConnectedNetwork<K>, K: SignatureKey + 'static> Sender<K> for N {
 /// to a channel and have the protocol receive them.
 #[async_trait]
 impl Receiver for mpsc::Receiver<Vec<u8>> {
-    async fn receive_message<Q: Request, A: Response>(&mut self) -> anyhow::Result<Message<Q, A>> {
+    async fn receive_message<R: Request>(&mut self) -> Result<Message<R>> {
         // Receive a message from the channel
         let message = self.recv().await.ok_or(anyhow::anyhow!("channel closed"))?;
 
-        // Deserialize the message
-        bincode::deserialize(&message).map_err(anyhow::Error::from)
+        // Convert the message to a [`Message`]
+        Message::from_bytes(&message)
     }
 }
 
@@ -133,13 +179,13 @@ impl<S: Sender<K>, R: Receiver, RS: RecipientSource<K>, K: SignatureKey + 'stati
     }
 
     /// Request something from the protocol and wait for the response
-    pub async fn request<Q: Request, A: Response>(
+    pub async fn request<Req: Request>(
         &self,
-        request: Q,
+        request: Req,
         timeout: Duration,
-    ) -> anyhow::Result<A> {
+    ) -> Result<Req::Response> {
         // Create a request message
-        let message: Message<Q, A> = Message::Request(request);
+        let message: Message<Req> = Message::Request(request);
 
         // Get the recipients that the message should expect responses from
         let recipients = self.recipient_source.get_recipients_for(&message);
