a8a8e7ea8d061661237f3fd12bd517eeaaea7c94
Rob <rob@espressosys.com>
2025-01-23 12:37:53 -0500

    add `VecDeque` test

--- crates/request-response/src/util/bounded_vec_deque.rs
+++ crates/request-response/src/util/bounded_vec_deque.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-/// A bounded [`VecDeque`]
+/// A [`VecDeque`] with a maximum size
 pub struct BoundedVecDeque<T> {
     /// The inner [`VecDeque`]
     inner: VecDeque<T>,
@@ -24,5 +24,22 @@ impl<T> BoundedVecDeque<T> {
             self.inner.pop_front();
         }
         self.inner.push_back(item);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bounded_vec_deque() {
+        let mut deque = BoundedVecDeque::new(3);
+        deque.push(1);
+        deque.push(2);
+        deque.push(3);
+        deque.push(4);
+        deque.push(5);
+        assert_eq!(deque.inner.len(), 3);
+        assert_eq!(deque.inner, vec![3, 4, 5]);
     }
 }
--- crates/request-response/src/util/mod.rs
+++ crates/request-response/src/util/mod.rs
@@ -1,4 +1,4 @@
 /// A [`tokio::task::JoinHandle`] that will automatically abort the task if dropped
 pub mod abort_on_drop_handle;
-/// A bounded [`VecDeque`]
+/// A [`VecDeque`] with a maximum size
 pub mod bounded_vec_deque;
