d8035489be996a149b2c3e8c6c6fbfa8dfae5f76
rob-maron <rob-maron@users.noreply.github.com>
2025-01-24 20:33:16 +0000

    deploy: 4fd2389f28df4d5b5b5a6383399b88343066378e

--- trait.impl/libp2p_kad/record/store/trait.RecordStore.js
+++ trait.impl/libp2p_kad/record/store/trait.RecordStore.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["libp2p_networking",[["impl&lt;R: RecordStore, D: <a class=\"trait\" href=\"libp2p_networking/network/behaviours/dht/store/persistent/trait.DhtPersistentStorage.html\" title=\"trait libp2p_networking::network::behaviours::dht::store::persistent::DhtPersistentStorage\">DhtPersistentStorage</a>&gt; RecordStore for <a class=\"struct\" href=\"libp2p_networking/network/behaviours/dht/store/persistent/struct.PersistentStore.html\" title=\"struct libp2p_networking::network::behaviours::dht::store::persistent::PersistentStore\">PersistentStore</a>&lt;R, D&gt;"],["impl&lt;R: RecordStore, K&gt; RecordStore for <a class=\"struct\" href=\"libp2p_networking/network/behaviours/dht/store/validated/struct.ValidatedStore.html\" title=\"struct libp2p_networking::network::behaviours::dht::store::validated::ValidatedStore\">ValidatedStore</a>&lt;R, K&gt;<div class=\"where\">where\n    K: 'static + <a class=\"trait\" href=\"hotshot_types/traits/signature_key/trait.SignatureKey.html\" title=\"trait hotshot_types::traits::signature_key::SignatureKey\">SignatureKey</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["libp2p_networking",[["impl&lt;R: RecordStore, D: <a class=\"trait\" href=\"libp2p_networking/network/behaviours/dht/store/persistent/trait.DhtPersistentStorage.html\" title=\"trait libp2p_networking::network::behaviours::dht::store::persistent::DhtPersistentStorage\">DhtPersistentStorage</a>&gt; RecordStore for <a class=\"struct\" href=\"libp2p_networking/network/behaviours/dht/store/persistent/struct.PersistentStore.html\" title=\"struct libp2p_networking::network::behaviours::dht::store::persistent::PersistentStore\">PersistentStore</a>&lt;R, D&gt;"],["impl&lt;R: RecordStore, K&gt; RecordStore for <a class=\"struct\" href=\"libp2p_networking/network/behaviours/dht/store/validated/struct.ValidatedStore.html\" title=\"struct libp2p_networking::network::behaviours::dht::store::validated::ValidatedStore\">ValidatedStore</a>&lt;R, K&gt;<div class=\"where\">where\n    K: 'static + SignatureKey,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[1073]}
+//{"start":57,"fragment_lengths":[915]}
